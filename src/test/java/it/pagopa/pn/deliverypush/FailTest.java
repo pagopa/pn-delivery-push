b0544143f81ad8c2505a8c4e37e9dcdbff35f021
marco-vit-pagopa <marco.vit@pagopa.it>
2021-08-03 15:44:18 +0200

    Aggiungi test che fallisce, ma disabilitato

--- src/test/java/it/pagopa/pn/deliverypush/FailTest.java
+++ src/test/java/it/pagopa/pn/deliverypush/FailTest.java
@@ -2,11 +2,12 @@ package it.pagopa.pn.deliverypush
 
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class FailTest {
 
-    @Test
+    @Test @Disabled
     void fail() {
         Assertions.assertEquals( 1, 2);
     }
