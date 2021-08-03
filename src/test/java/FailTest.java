58f603d0f3e3d655ef65e32396433ccd76274fe5
marco-vit-pagopa <marco.vit@pagopa.it>
2021-08-03 15:22:00 +0200

    Fail test

---
+++ src/test/java/FailTest.java
@@ -0,0 +1,13 @@
+package it.pagopa.pn.deliverypush
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FailTest {
+
+    @Test
+    void fail() {
+        Assertions.assertEquals( 1, 2);
+    }
+}
