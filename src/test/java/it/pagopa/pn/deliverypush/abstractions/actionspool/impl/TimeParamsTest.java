3704ab2a059d2b933810c3f81b890091da68393f
Simone <s.lurbrano@itdm.it>
2021-09-09 09:56:28 +0200

    test timeParamsTest di minuti, ore, giorni.

--- src/test/java/it/pagopa/pn/deliverypush/abstractions/actionspool/impl/TimeParamsTest.java
+++ src/test/java/it/pagopa/pn/deliverypush/abstractions/actionspool/impl/TimeParamsTest.java
@@ -19,9 +19,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @TestPropertySource(properties = {"pn.delivery-push.time-params.second-attempt-waiting-time=1s",
         "pn.delivery-push.time-params.processing-time-to-recipient=1s",
         "pn.delivery-push.time-params.waiting-response-from-first-address=1s",
-        "pn.delivery-push.time-params.waiting-for-next-action=1s",
-        "pn.delivery-push.time-params.time-between-ext-ch-reception-and-message-processed=1s",
-        "pn.delivery-push.time-params.interval-between-notification-and-message-received=1s"})
+        "pn.delivery-push.time-params.waiting-for-next-action=1m",
+        "pn.delivery-push.time-params.time-between-ext-ch-reception-and-message-processed=1h",
+        "pn.delivery-push.time-params.interval-between-notification-and-message-received=1d"})
 public class TimeParamsTest {
 
     @Autowired
@@ -29,13 +29,13 @@ public class TimeParamsTest {
 
     @Test
     public void assertConfigurationIsLoaded(){
-
+        
         assertEquals( Duration.ofSeconds(1), pnDeliveryPushConfigs.getTimeParams().getSecondAttemptWaitingTime() );
         assertEquals( Duration.ofSeconds(1), pnDeliveryPushConfigs.getTimeParams().getProcessingTimeToRecipient() );
         assertEquals( Duration.ofSeconds(1), pnDeliveryPushConfigs.getTimeParams().getWaitingResponseFromFirstAddress() );
-        assertEquals( Duration.ofSeconds(1), pnDeliveryPushConfigs.getTimeParams().getWaitingForNextAction() );
-        assertEquals( Duration.ofSeconds(1), pnDeliveryPushConfigs.getTimeParams().getTimeBetweenExtChReceptionAndMessageProcessed() );
-        assertEquals( Duration.ofSeconds(1), pnDeliveryPushConfigs.getTimeParams().getIntervalBetweenNotificationAndMessageReceived() );
+        assertEquals( Duration.ofMinutes(1), pnDeliveryPushConfigs.getTimeParams().getWaitingForNextAction() );
+        assertEquals( Duration.ofHours(1), pnDeliveryPushConfigs.getTimeParams().getTimeBetweenExtChReceptionAndMessageProcessed() );
+        assertEquals( Duration.ofDays(1), pnDeliveryPushConfigs.getTimeParams().getIntervalBetweenNotificationAndMessageReceived() );
 
     }
 
