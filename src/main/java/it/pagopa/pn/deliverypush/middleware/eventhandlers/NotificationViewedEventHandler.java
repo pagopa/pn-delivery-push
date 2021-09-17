e0ac944fc99e436db34dbe8a180e9c6d436d8512
marco-vit-pagopa <marco.vit@pagopa.it>
2021-09-17 13:27:48 +0200

    PN-270 fix

--- src/main/java/it/pagopa/pn/deliverypush/middleware/eventhandlers/NotificationViewedEventHandler.java
+++ src/main/java/it/pagopa/pn/deliverypush/middleware/eventhandlers/NotificationViewedEventHandler.java
@@ -1,31 +1,31 @@
 package it.pagopa.pn.deliverypush.middleware.eventhandlers;
 
+import it.pagopa.pn.api.dto.events.PnDeliveryNotificationViewedEvent;
+import it.pagopa.pn.deliverypush.temp.mom.consumer.AbstractEventHandler;
 import org.springframework.stereotype.Service;
 
-import it.pagopa.pn.api.dto.events.PnDeliveryNotificationAcknowledgementEvent;
 import it.pagopa.pn.api.dto.events.StandardEventHeader;
 import it.pagopa.pn.deliverypush.PnDeliveryPushConfigs;
 import it.pagopa.pn.deliverypush.abstractions.actionspool.Action;
 import it.pagopa.pn.deliverypush.abstractions.actionspool.ActionType;
 import it.pagopa.pn.deliverypush.abstractions.actionspool.ActionsPool;
-import it.pagopa.pn.deliverypush.temp.mom.consumer.AbstractEventHandler;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
-public class NotificationViewedEventHandler extends AbstractEventHandler<PnDeliveryNotificationAcknowledgementEvent> {
+public class NotificationViewedEventHandler extends AbstractEventHandler<PnDeliveryNotificationViewedEvent> {
 
     private final ActionsPool actionsPool;
     private final PnDeliveryPushConfigs pnDeliveryPushConfigs;
 
     public NotificationViewedEventHandler( ActionsPool actionsPool , PnDeliveryPushConfigs pnDeliveryPushConfigs) {
-        super( PnDeliveryNotificationAcknowledgementEvent.class );
+        super( PnDeliveryNotificationViewedEvent.class );
         this.actionsPool = actionsPool;
         this.pnDeliveryPushConfigs = pnDeliveryPushConfigs;
     }
 
     @Override
-    public void handleEvent(PnDeliveryNotificationAcknowledgementEvent evt ) {
+    public void handleEvent(PnDeliveryNotificationViewedEvent evt ) {
         StandardEventHeader header = evt.getHeader();
         log.info( "NOTIFICATION VIEWED  iun={} eventId={}", header.getIun(), header.getEventId() );
 
