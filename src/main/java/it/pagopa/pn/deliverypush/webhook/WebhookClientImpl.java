ae8501ef70650a0ff15f0f9a070ca79596ea7904
Alessandro Masci <alessandro.masci@pagopa.it>
2021-09-24 15:08:16 +0200

    modificata implementazione webhook client

--- src/main/java/it/pagopa/pn/deliverypush/webhook/WebhookClientImpl.java
+++ src/main/java/it/pagopa/pn/deliverypush/webhook/WebhookClientImpl.java
@@ -1,22 +1,28 @@
 package it.pagopa.pn.deliverypush.webhook;
 
 import it.pagopa.pn.commons.exceptions.PnInternalException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
-import java.util.Map;
 
+@Slf4j
 @Component
 public class WebhookClientImpl implements WebhookClient {
 
     private final RestTemplate rest = new RestTemplate();
 
-    public void sendInfo( String url, List<WebhookBufferRowDto> data ) {
-        ResponseEntity<Map> resp = rest.postForEntity( url, data, Map.class);
-        if( ! resp.getStatusCode().is2xxSuccessful() ) {
-            throw new PnInternalException("Http error " + resp.getStatusCodeValue() + " calling webhook " + url );
+    public void sendInfo(String url, List<WebhookBufferRowDto> data) {
+        log.info("Send info webhook with url: " + url);
+        HttpEntity<List<WebhookBufferRowDto>> entity = new HttpEntity<>(data, null);
+        ResponseEntity<Void> resp = rest.exchange(url, HttpMethod.POST, entity, Void.class);
+        log.info("Response: " + resp);
+        if (!resp.getStatusCode().is2xxSuccessful()) {
+            throw new PnInternalException("Http error " + resp.getStatusCodeValue() + " calling webhook " + url);
         }
     }
 
