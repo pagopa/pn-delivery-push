e1f882aab08362d87e73b0defcf33d7c2a3cd082
marco-vit-pagopa <marco.vit@pagopa.it>
2022-03-02 21:02:19 +0100

    PN-751 fix logs

--- src/main/java/it/pagopa/pn/deliverypush/pnclient/delivery/PnDeliveryClientImpl.java
+++ src/main/java/it/pagopa/pn/deliverypush/pnclient/delivery/PnDeliveryClientImpl.java
@@ -14,20 +14,24 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 @Component
 public class PnDeliveryClientImpl implements PnDeliveryClient {
+
+    private static final String UPDATE_STATUS_URL ="/notifications/update-status";
+
     private final RestTemplate restTemplate;
     private final PnDeliveryPushConfigs cfg;
-    
-    private static final String UPDATE_STATUS_URL ="/notifications/update-status";
+    private final String baseUrl;
+
 
     public PnDeliveryClientImpl( @Qualifier("withTracing") RestTemplate restTemplate, PnDeliveryPushConfigs cfg) {
         this.restTemplate = restTemplate;
         this.cfg = cfg;
+        this.baseUrl = cfg.getDeliveryBaseUrl() + UPDATE_STATUS_URL;
     }
 
     public ResponseEntity<ResponseUpdateStatusDto> updateState(RequestUpdateStatusDto dto) {
-        log.debug("Start update status call for iun {}", dto.getIun());
 
-        final String baseUrl = cfg.getDeliveryBaseUrl() + UPDATE_STATUS_URL;
+        log.info("Start update status call for iun {}, url: {}", dto.getIun(), baseUrl);
+
         HttpEntity<RequestUpdateStatusDto> entity = new HttpEntity<>(dto, null);
 
         ResponseEntity<ResponseUpdateStatusDto> resp = restTemplate.exchange(baseUrl, HttpMethod.POST, entity, ResponseUpdateStatusDto.class);
