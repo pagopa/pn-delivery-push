b643d835c84da34053a96e0da06291945e670e7d
Simone Rondinella <simone.rondinella@pagopa.it>
2022-03-24 11:14:19 +0100

    Modifiche spostare controller in pn-delivery-push

---
+++ src/main/java/it/pagopa/pn/deliverypush/rest/PnPaperNotificationFailedController.java
@@ -0,0 +1,30 @@
+package it.pagopa.pn.deliverypush.rest;
+
+import it.pagopa.pn.api.dto.notification.failednotification.PaperNotificationFailed;
+import it.pagopa.pn.api.rest.PnDeliveryPushRestConstants;
+import it.pagopa.pn.api.rest.PnDeliveryRestApi_methodSearchPaperNotificationFailed;
+import it.pagopa.pn.deliverypush.service.PaperNotificationFailedService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PnPaperNotificationFailedController implements PnDeliveryRestApi_methodSearchPaperNotificationFailed {
+
+    private PaperNotificationFailedService service;
+
+    public PnPaperNotificationFailedController(PaperNotificationFailedService service) {
+        this.service = service;
+    }
+
+    @Override
+    @GetMapping(PnDeliveryPushRestConstants.NOTIFICATIONS_PAPER_FAILED_PATH)
+    public ResponseEntity<List<PaperNotificationFailed>> searchPaperNotificationsFailed(
+            @RequestParam(name = "recipientId") String recipientId) {
+        return ResponseEntity.ok().body(service.getPaperNotificationsFailed(recipientId));
+    }
+
+}
---
+++ src/main/java/it/pagopa/pn/deliverypush/service/impl/PaperNotificationFailedServiceImpl.java
@@ -0,0 +1,26 @@
+package it.pagopa.pn.deliverypush.service.impl;
+
+import it.pagopa.pn.api.dto.notification.failednotification.PaperNotificationFailed;
+import it.pagopa.pn.deliverypush.middleware.failednotificationdao.PaperNotificationFailedDao;
+import it.pagopa.pn.deliverypush.service.PaperNotificationFailedService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+public class PaperNotificationFailedServiceImpl implements PaperNotificationFailedService {
+    private PaperNotificationFailedDao paperNotificationFailedDao;
+
+    public PaperNotificationFailedServiceImpl(PaperNotificationFailedDao paperNotificationFailedDao) {
+        this.paperNotificationFailedDao = paperNotificationFailedDao;
+    }
+
+    public List<PaperNotificationFailed> getPaperNotificationsFailed(String recipientId) {
+        log.info( "Retrieve paper notifications failed for recipientId={}", recipientId);
+        return new ArrayList<>(paperNotificationFailedDao.getNotificationByRecipientId(recipientId));
+    }
+
+}
