c29cc6215592238843787cffd4765cdcebe0d893
Simone Rondinella <simone.rondinella@pagopa.it>
2022-05-27 16:21:40 +0200

    Modifiche per test visualizzazione messaggio

--- src/test/java/it/pagopa/pn/deliverypush/action2/it/mockbean/TimelineDaoMock.java
+++ src/test/java/it/pagopa/pn/deliverypush/action2/it/mockbean/TimelineDaoMock.java
@@ -1,7 +1,13 @@
 package it.pagopa.pn.deliverypush.action2.it.mockbean;
 
+import it.pagopa.pn.deliverypush.action2.NotificationViewedHandler;
+import it.pagopa.pn.deliverypush.action2.utils.NotificationUtils;
+import it.pagopa.pn.deliverypush.dto.ext.delivery.notification.NotificationInt;
+import it.pagopa.pn.deliverypush.dto.ext.delivery.notification.NotificationRecipientInt;
 import it.pagopa.pn.deliverypush.dto.timeline.TimelineElementInternal;
 import it.pagopa.pn.deliverypush.middleware.dao.timelinedao.TimelineDao;
+import it.pagopa.pn.deliverypush.service.NotificationService;
+import org.springframework.context.annotation.Lazy;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -10,10 +16,19 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class TimelineDaoMock implements TimelineDao {
-    private Collection<TimelineElementInternal> timelineList;
+    public static final String SIMULATE_VIEW_NOTIFICATION= "simulate-view-notification";
 
-    public TimelineDaoMock() {
+    private Collection<TimelineElementInternal> timelineList;
+    private final NotificationViewedHandler notificationViewedHandler;
+    private final NotificationService notificationService;
+    private final NotificationUtils notificationUtils;
+
+    public TimelineDaoMock(@Lazy NotificationViewedHandler notificationViewedHandler, @Lazy NotificationService notificationService,
+                           @Lazy NotificationUtils notificationUtils) {
         timelineList = new ArrayList<>();
+        this.notificationViewedHandler = notificationViewedHandler;
+        this.notificationService = notificationService;
+        this.notificationUtils = notificationUtils;
     }
 
     public void clear() {
@@ -22,7 +37,21 @@ public class TimelineDaoMock implements TimelineDao {
     
     @Override
     public void addTimelineElement(TimelineElementInternal row) {
+        NotificationRecipientInt notificationRecipientInt = getRecipientInt(row);
+
+        String viewNotificationString = SIMULATE_VIEW_NOTIFICATION + row.getElementId();
+        
+        if(notificationRecipientInt.getTaxId().startsWith(viewNotificationString)){
+            //Viene simulata la visualizzazione della notifica prima di uno specifico inserimento in timeline
+            notificationViewedHandler.handleViewNotification( row.getIun(), row.getDetails().getRecIndex() );
+        }
+        
         timelineList.add(row);
+    }
+
+    private NotificationRecipientInt getRecipientInt(TimelineElementInternal row) {
+        NotificationInt notificationInt = this.notificationService.getNotificationByIun(row.getIun());
+        return notificationUtils.getRecipientFromIndex(notificationInt, row.getDetails().getRecIndex());
     }
 
     @Override
