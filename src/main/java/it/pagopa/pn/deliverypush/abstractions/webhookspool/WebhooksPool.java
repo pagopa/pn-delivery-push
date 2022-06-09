99f30a70f0c65b0758cde6e87d48b4a9473c91cd
fabriziolana <fabrizio.lana@necsy.it>
2022-06-09 14:41:00 +0200

    fix varie conf stream

--- src/main/java/it/pagopa/pn/deliverypush/abstractions/webhookspool/WebhooksPool.java
+++ src/main/java/it/pagopa/pn/deliverypush/abstractions/webhookspool/WebhooksPool.java
@@ -1,5 +1,5 @@
 package it.pagopa.pn.deliverypush.abstractions.webhookspool;
 
 public interface WebhooksPool {
-     void scheduleFutureAction(WebhookAction action, WebhookEventType webhookEventType);
+     void scheduleFutureAction(WebhookAction action);
 }
