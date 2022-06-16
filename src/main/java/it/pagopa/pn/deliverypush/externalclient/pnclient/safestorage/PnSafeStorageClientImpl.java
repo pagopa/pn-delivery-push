fa61bc5bab348b8b6aa926c53790d433c40c3b1f
fabriziolana <100559407+fabriziolana@users.noreply.github.com>
2022-06-16 16:19:52 +0200

    Bug/pn 1607 (#88)
    * fix codifica in base64 dello sha
    
    * fix codifica in base64 dello sha
    
    * fix codifica in base64 dello sha

--- src/main/java/it/pagopa/pn/deliverypush/externalclient/pnclient/safestorage/PnSafeStorageClientImpl.java
+++ src/main/java/it/pagopa/pn/deliverypush/externalclient/pnclient/safestorage/PnSafeStorageClientImpl.java
@@ -1,6 +1,5 @@
 package it.pagopa.pn.deliverypush.externalclient.pnclient.safestorage;
 
-import com.amazonaws.util.Base64;
 import it.pagopa.pn.commons.exceptions.PnInternalException;
 import it.pagopa.pn.delivery.generated.openapi.clients.safestorage.api.FileDownloadApi;
 import it.pagopa.pn.delivery.generated.openapi.clients.safestorage.api.FileUploadApi;
@@ -8,7 +7,6 @@ import it.pagopa.pn.delivery.generated.openapi.clients.safestorage.model.FileCre
 import it.pagopa.pn.delivery.generated.openapi.clients.safestorage.model.FileDownloadResponse;
 import it.pagopa.pn.deliverypush.PnDeliveryPushConfigs;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
@@ -16,12 +14,14 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Base64Utils;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
-import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @Slf4j
 @Component
@@ -44,7 +44,7 @@ public class PnSafeStorageClientImpl implements PnSafeStorageClient {
 
 
     public FileDownloadResponse getFile(String fileKey, Boolean metadataOnly) {
-        log.debug("Start call getFile - fileKey {} metadataOnly {}", fileKey, metadataOnly);
+        log.debug("Start call getFile - fileKey={} metadataOnly={}", fileKey, metadataOnly);
         // elimino eventuale prefisso di safestorage
         fileKey = fileKey.replace(SAFE_STORAGE_URL_PREFIX, "");
         return fileDownloadApi.getFile( fileKey, this.cfg.getSafeStorageCxId(), metadataOnly );
@@ -52,27 +52,28 @@ public class PnSafeStorageClientImpl implements PnSafeStorageClient {
 
     @Override
     public FileCreationResponse createAndUploadContent(FileCreationWithContentRequest fileCreationRequest) {
-            log.debug("Start call createAndUploadFile - documentType {} filesize:{}", fileCreationRequest.getDocumentType(), fileCreationRequest.getContent().length);
+            log.debug("Start call createAndUploadFile - documentType={} filesize={}", fileCreationRequest.getDocumentType(), fileCreationRequest.getContent().length);
 
-            String sha256 = DigestUtils.sha256Hex(fileCreationRequest.getContent());
+            String sha256 = computeSha256(fileCreationRequest.getContent());
 
             FileCreationResponse fileCreationResponse = fileUploadApi.createFile( this.cfg.getSafeStorageCxId(),  "SHA-256", sha256,  fileCreationRequest );
 
-            log.debug("createAndUploadContent create file preloaded sha256:{}", sha256);
+            log.debug("createAndUploadContent create file preloaded sha256={}", sha256);
 
             this.uploadContent(fileCreationRequest, fileCreationResponse, sha256);
 
-            log.info("createAndUploadContent file uploaded successfully key:{}", fileCreationResponse.getKey());
+            log.info("createAndUploadContent file uploaded successfully key={} sha256={}", fileCreationResponse.getKey(), sha256);
 
             return fileCreationResponse;
     }
+
 
     private void uploadContent(FileCreationWithContentRequest fileCreationRequest, FileCreationResponse fileCreationResponse, String sha256){
 
         try {
             MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
             headers.add("Content-type", fileCreationRequest.getContentType());
-            headers.add("x-amz-checksum-sha256", Base64.encodeAsString(sha256.getBytes(StandardCharsets.UTF_8)));
+            headers.add("x-amz-checksum-sha256", sha256);
             headers.add("x-amz-meta-secret", fileCreationResponse.getSecret());
 
             HttpEntity<Resource> req = new HttpEntity<>(new ByteArrayResource(fileCreationRequest.getContent()), headers);
@@ -92,5 +93,22 @@ public class PnSafeStorageClientImpl implements PnSafeStorageClient {
             log.error("uploadContent Exception uploading file", ee);
             throw new PnInternalException("Exception uploading file", ee);
         }
+    }
+
+
+
+    private String computeSha256( byte[] content ) {
+
+        try{
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodedhash = digest.digest( content );
+            return bytesToBase64( encodedhash );
+        } catch (NoSuchAlgorithmException exc) {
+            throw new PnInternalException("cannot compute sha256", exc );
+        }
+    }
+
+    private static String bytesToBase64(byte[] hash) {
+        return Base64Utils.encodeToString( hash );
     }
 }
