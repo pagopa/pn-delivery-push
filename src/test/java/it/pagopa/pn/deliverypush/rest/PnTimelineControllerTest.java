36ebb6a33c8101ec2d3d54f8d6258cb137422738
Simone Rondinella <simone.rondinella@pagopa.it>
2022-07-12 17:29:57 +0200

    PN-1544 Gestione casistiche di errore nei controller

--- src/test/java/it/pagopa/pn/deliverypush/rest/PnTimelineControllerTest.java
+++ src/test/java/it/pagopa/pn/deliverypush/rest/PnTimelineControllerTest.java
@@ -1,16 +1,15 @@
 package it.pagopa.pn.deliverypush.rest;
 
-import it.pagopa.pn.deliverypush.generated.openapi.server.v1.dto.NotificationHistoryResponse;
-import it.pagopa.pn.deliverypush.generated.openapi.server.v1.dto.TimelineElement;
-import it.pagopa.pn.deliverypush.generated.openapi.server.v1.dto.TimelineElementCategory;
-import it.pagopa.pn.deliverypush.generated.openapi.server.v1.dto.TimelineElementDetails;
+import it.pagopa.pn.deliverypush.generated.openapi.server.v1.dto.*;
 import it.pagopa.pn.deliverypush.service.TimelineService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
@@ -64,5 +63,67 @@ class PnTimelineControllerTest {
 
         Mockito.verify(service).getTimelineAndStatusHistory(Mockito.anyString(), Mockito.anyInt(), Mockito.any());
         
+    }
+
+    @Test
+    void getTimelineKoRuntimeEx() {
+        Mockito.when(service.getTimelineAndStatusHistory(Mockito.anyString(), Mockito.anyInt(), Mockito.any()))
+                .thenThrow( new NullPointerException() );
+        
+        Instant createdAt = Instant.now();
+        int numberOfRecipients = 1;
+
+        webTestClient.get()
+                .uri(
+                        uriBuilder ->
+                                uriBuilder
+                                        .path( "/delivery-push-private/" + IUN + "/history" )
+                                        .queryParam("createdAt", createdAt)
+                                        .queryParam("numberOfRecipients", numberOfRecipients).build()
+                )
+                .accept(MediaType.ALL)
+                .header(HttpHeaders.ACCEPT, "application/json")
+                .exchange()
+                .expectStatus()
+                .is5xxServerError()
+                .expectBody(Problem.class).consumeWith(
+                        elem -> {
+                            Problem problem = elem.getResponseBody();
+                            assert problem != null;
+                            Assertions.assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), problem.getStatus());
+                        }
+                );
+
+        Mockito.verify(service).getTimelineAndStatusHistory(Mockito.anyString(), Mockito.anyInt(), Mockito.any());
+    }
+
+    @Test
+    void getTimelineKoBadRequest() {
+        Mockito.when(service.getTimelineAndStatusHistory(Mockito.anyString(), Mockito.anyInt(), Mockito.any()))
+                .thenThrow( new NullPointerException() );
+
+        Instant createdAt = Instant.now();
+
+        webTestClient.get()
+                .uri(
+                        uriBuilder ->
+                                uriBuilder
+                                        .path( "/delivery-push-private/" + IUN + "/history" )
+                                        .queryParam("createdAt", createdAt)
+                                        .build()
+                )
+                .accept(MediaType.ALL)
+                .header(HttpHeaders.ACCEPT, "application/json")
+                .exchange()
+                .expectStatus()
+                .isBadRequest()
+                .expectBody(Problem.class).consumeWith(
+                        elem -> {
+                            Problem problem = elem.getResponseBody();
+                            assert problem != null;
+                            Assertions.assertEquals(HttpStatus.BAD_REQUEST.value(), problem.getStatus());
+                            Assertions.assertNotNull(problem.getDetail());
+                        }
+                );
     }
 }
