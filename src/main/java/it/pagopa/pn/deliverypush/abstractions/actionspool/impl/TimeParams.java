52f051d266e9c994b170304d576264f09eb49c04
Simone Rondinella <simone.rondinella@pagopa.it>
2022-07-13 14:47:56 +0200

    PN-1684 Eliminazione properties non necessarie

--- src/main/java/it/pagopa/pn/deliverypush/abstractions/actionspool/impl/TimeParams.java
+++ src/main/java/it/pagopa/pn/deliverypush/abstractions/actionspool/impl/TimeParams.java
@@ -6,24 +6,10 @@ import java.time.Duration;
 
 @Data
 public class TimeParams {
-    //Timing workflow LEGACY
-    private Duration waitingResponseFromFirstAddress;
-    private Duration secondAttemptWaitingTime;
-
-    private Duration recipientViewMaxTimeForDigital;
-    private Duration recipientViewMaxTimeForAnalog;
-    private Duration refinementTimeForCompletelyUnreachable;
-
-    private Duration waitingForNextAction;
-    private Duration timeBetweenExtChReceptionAndMessageProcessed;
-    private Duration intervalBetweenNotificationAndMessageReceived;
-    
-    //Timing workflow NEW
     private Duration waitingForReadCourtesyMessage;
     private Duration schedulingDaysSuccessDigitalRefinement;
     private Duration schedulingDaysFailureDigitalRefinement;
     private Duration schedulingDaysSuccessAnalogRefinement;
     private Duration schedulingDaysFailureAnalogRefinement;
     private Duration secondNotificationWorkflowWaitingTime;
-
 }
