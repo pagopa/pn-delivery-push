50794c7383cf10d88dbab91b2a9565bf1457bf6d
Simone Rondinella <simone.rondinella@pagopa.it>
2022-07-27 15:10:51 +0200

    PN-1889 Eliminato DownstreamId non necessario

--- src/test/java/it/pagopa/pn/deliverypush/service/mapper/SmartMapperTest.java
+++ src/test/java/it/pagopa/pn/deliverypush/service/mapper/SmartMapperTest.java
@@ -6,7 +6,6 @@ import it.pagopa.pn.deliverypush.dto.timeline.details.DownstreamIdInt;
 import it.pagopa.pn.deliverypush.dto.timeline.details.SendDigitalDetailsInt;
 import it.pagopa.pn.deliverypush.generated.openapi.server.v1.dto.DigitalAddress;
 import it.pagopa.pn.deliverypush.generated.openapi.server.v1.dto.DigitalAddressSource;
-import it.pagopa.pn.deliverypush.generated.openapi.server.v1.dto.DownstreamId;
 import it.pagopa.pn.deliverypush.generated.openapi.server.v1.dto.TimelineElementDetails;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -44,10 +43,6 @@ class SmartMapperTest {
                         .address("testAddress@gmail.com")
                         .build())
                 .retryNumber(0)
-                .downstreamId(DownstreamId.builder()
-                        .messageId("messageId")
-                        .systemId("systemId")
-                        .build())
                 .investigation(true)
                 .build();
 
