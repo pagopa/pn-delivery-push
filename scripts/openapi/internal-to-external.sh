f2abd7f7664c4339caa9d20184e765d43871469c
Alessandro Masci <alessandro.masci@pagopa.it>
2022-09-12 16:20:19 +0200

    PN-2140: rename # property

--- scripts/openapi/internal-to-external.sh
+++ scripts/openapi/internal-to-external.sh
@@ -1,6 +1,6 @@
 
 cat docs/openapi/api-internal-b2b-webhook-v1.yaml \
     | grep -v "# NO EXTERNAL" \
-    | grep -v "# TO BE IMPLEMENTED" \
+    | grep -v "# NOT YET IMPLEMENTED" \
     | sed -e '/# ONLY EXTERNAL/s/^#//' \
     > docs/openapi/api-external-b2b-webhook-v1.yaml
