b72a4894e5f47a15072e2b0869746b11a62b2ee2
Alessandro Masci <alessandro.masci@pagopa.it>
2022-09-12 15:30:27 +0200

    PN-2140: add TO BE IMPLEMENTED external APIs mngt

--- scripts/openapi/internal-to-external.sh
+++ scripts/openapi/internal-to-external.sh
@@ -1,5 +1,6 @@
 
 cat docs/openapi/api-internal-b2b-webhook-v1.yaml \
     | grep -v "# NO EXTERNAL" \
+    | grep -v "# TO BE IMPLEMENTED" \
     | sed -e '/# ONLY EXTERNAL/s/^#//' \
     > docs/openapi/api-external-b2b-webhook-v1.yaml
