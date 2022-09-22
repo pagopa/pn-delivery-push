3a2668d469e1a8710f934a973cb2dcf62cd6d372
Simone Rondinella <simone.rondinella@pagopa.it>
2022-09-22 17:43:46 +0200

    PN-2076 Completate modifiche per paperNotificationFailed

--- src/main/java/it/pagopa/pn/deliverypush/rest/PnPaperNotificationFailedController.java
+++ src/main/java/it/pagopa/pn/deliverypush/rest/PnPaperNotificationFailedController.java
@@ -1,16 +1,18 @@
 package it.pagopa.pn.deliverypush.rest;
 
-import it.pagopa.pn.deliverypush.dto.papernotificationfailed.PaperNotificationFailed;
+import it.pagopa.pn.deliverypush.generated.openapi.server.v1.api.PaperNotificationFailedApi;
+import it.pagopa.pn.deliverypush.generated.openapi.server.v1.dto.ResponsePaperNotificationFailedDto;
 import it.pagopa.pn.deliverypush.service.PaperNotificationFailedService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 @RestController
-public class PnPaperNotificationFailedController {
+public class PnPaperNotificationFailedController implements PaperNotificationFailedApi {
 
     private final PaperNotificationFailedService service;
 
@@ -18,10 +20,17 @@ public class PnPaperNotificationFailedController {
         this.service = service;
     }
 
-    @GetMapping(PnDeliveryPushRestConstants.NOTIFICATIONS_PAPER_FAILED_PATH)
-    public ResponseEntity<List<PaperNotificationFailed>> searchPaperNotificationsFailed(
-            @RequestParam(name = "recipientId") String recipientId) {
-        return ResponseEntity.ok().body(service.getPaperNotificationByRecipientId(recipientId));
+    @Override
+    public Mono<ResponseEntity<Flux<ResponsePaperNotificationFailedDto>>> paperNotificationFailed(
+            String recipientInternalId,
+            Boolean getAAR,
+            final ServerWebExchange exchange
+    ) {
+        return Mono.fromSupplier(() -> {
+            List<ResponsePaperNotificationFailedDto> responses = service.getPaperNotificationByRecipientId(recipientInternalId, getAAR);
+            Flux<ResponsePaperNotificationFailedDto> fluxFacts = Flux.fromIterable(responses);
+            return ResponseEntity.ok(fluxFacts);
+        });
     }
 
 }
