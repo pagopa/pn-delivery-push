c4fec07af88927b51edd466210a26c7df9662d20
Simone Rondinella <simone.rondinella@pagopa.it>
2022-09-26 12:29:51 +0200

    PN-2232 Modificata logica recipientMapper + Modificato test case

--- src/test/java/it/pagopa/pn/deliverypush/service/mapper/RecipientMapperTest.java
+++ src/test/java/it/pagopa/pn/deliverypush/service/mapper/RecipientMapperTest.java
@@ -9,21 +9,19 @@ import it.pagopa.pn.deliverypush.dto.ext.delivery.notification.NotificationRecip
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 class RecipientMapperTest {
 
     @Test
     void externalToInternal() {
-
+        //GIVEN
         NotificationRecipient given = buildNotificationRecipient();
-
+        
+        //WHEN
         NotificationRecipientInt actual = RecipientMapper.externalToInternal(given);
-
+        
+        //THEN
         NotificationRecipientInt expected = buildNotificationRecipientInt();
-
-        Assertions.assertEquals(expected.getTaxId(), actual.getTaxId());
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
@@ -35,7 +33,7 @@ class RecipientMapperTest {
 
         NotificationRecipient expected = buildNotificationRecipient();
 
-        Assertions.assertEquals(expected.getTaxId(), actual.getTaxId());
+        Assertions.assertEquals(expected, actual);
     }
 
     private NotificationRecipientInt buildNotificationRecipientInt() {
@@ -68,6 +66,20 @@ class RecipientMapperTest {
                                         .versionToken("002")
                                         .build())
                                 .build())
+                        .f24flatRate(NotificationDocumentInt.builder()
+                                .digests(NotificationDocumentInt.Digests.builder().sha256("001").build())
+                                .ref(NotificationDocumentInt.Ref.builder()
+                                        .key("001")
+                                        .versionToken("002")
+                                        .build())
+                                .build())
+                        .f24standard(NotificationDocumentInt.builder()
+                                .digests(NotificationDocumentInt.Digests.builder().sha256("001").build())
+                                .ref(NotificationDocumentInt.Ref.builder()
+                                        .key("001")
+                                        .versionToken("002")
+                                        .build())
+                                .build())
                         .build())
                 .build();
     }
@@ -83,69 +95,49 @@ class RecipientMapperTest {
         physicalAddress.setZip("007");
 
         NotificationPaymentAttachment pagoPaForm = new NotificationPaymentAttachment();
-
         NotificationAttachmentDigests digests = new NotificationAttachmentDigests();
         digests.setSha256("001");
         pagoPaForm.setDigests(digests);
-
         NotificationAttachmentBodyRef ref = new NotificationAttachmentBodyRef();
         ref.setKey("001");
         ref.setVersionToken("002");
         pagoPaForm.setRef(ref);
 
+        NotificationPaymentAttachment f24FlatRate = new NotificationPaymentAttachment();
+        NotificationAttachmentDigests digestsF24FlatRate = new NotificationAttachmentDigests();
+        digestsF24FlatRate.setSha256("001");
+        f24FlatRate.setDigests(digestsF24FlatRate);
+        NotificationAttachmentBodyRef refF24FlatRate = new NotificationAttachmentBodyRef();
+        refF24FlatRate.setKey("001");
+        refF24FlatRate.setVersionToken("002");
+        f24FlatRate.setRef(refF24FlatRate);
+
+        NotificationPaymentAttachment f24Standard = new NotificationPaymentAttachment();
+        NotificationAttachmentDigests digestsF24Standard = new NotificationAttachmentDigests();
+        digestsF24Standard.setSha256("001");
+        f24Standard.setDigests(digestsF24Standard);
+        NotificationAttachmentBodyRef refF24Standard = new NotificationAttachmentBodyRef();
+        refF24Standard.setKey("001");
+        refF24Standard.setVersionToken("002");
+        f24Standard.setRef(refF24Standard);
+        
         NotificationPaymentInfo paymentInfo = new NotificationPaymentInfo();
         paymentInfo.setNoticeCode("001");
         paymentInfo.setCreditorTaxId("002");
         paymentInfo.setPagoPaForm(pagoPaForm);
-
+        paymentInfo.setF24flatRate(f24FlatRate);
+        paymentInfo.setF24standard(f24Standard);
+        
         NotificationRecipient recipient = new NotificationRecipient();
         recipient.setTaxId("001");
         recipient.setInternalId("002");
         recipient.setDenomination("003");
         recipient.setDigitalDomicile(new NotificationDigitalAddress()
-                .address("address")
+                .address("account@dominio.it")
                 .type(NotificationDigitalAddress.TypeEnum.PEC));
         recipient.setPhysicalAddress(physicalAddress);
         recipient.setPayment(paymentInfo);
         return recipient;
     }
-
-    private SentNotification getExternalNotification() {
-        return new SentNotification()
-                .iun("IUN_01")
-                .paProtocolNumber("protocol_01")
-                .subject("Subject 01")
-                .senderPaId("pa_02")
-                .physicalCommunicationType(SentNotification.PhysicalCommunicationTypeEnum.REGISTERED_LETTER_890)
-                .amount(18)
-                .paymentExpirationDate("2022-10-22")
-                .recipients(Collections.singletonList(
-                        new NotificationRecipient()
-                                .taxId("Codice Fiscale 01")
-                                .denomination("Nome Cognome/Ragione Sociale")
-                                .digitalDomicile(
-                                        new NotificationDigitalAddress()
-                                                .address("address")
-                                                .type(NotificationDigitalAddress.TypeEnum.PEC)
-                                )
-                ))
-                .documents(Arrays.asList(
-                        new NotificationDocument()
-                                .ref(new NotificationAttachmentBodyRef()
-                                        .key("doc00")
-                                        .versionToken("v01_doc00")
-                                )
-                                .digests(new NotificationAttachmentDigests()
-                                        .sha256("sha256_doc00")
-                                ),
-                        new NotificationDocument()
-                                .ref(new NotificationAttachmentBodyRef()
-                                        .key("doc01")
-                                        .versionToken("v01_doc01")
-                                )
-                                .digests(new NotificationAttachmentDigests()
-                                        .sha256("sha256_doc01")
-                                )
-                ));
-    }
+    
 }
