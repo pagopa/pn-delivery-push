51a38cd9ced0ac5448823854884d3d7a9bdc9b2c
Christian Fiorillo <c.fiorillo@mediaticaspa.it>
2022-09-30 09:06:07 +0200

    Improve testing coverage PN-522

--- src/test/java/it/pagopa/pn/deliverypush/middleware/queue/producer/action/sqs/SqsActionProducerTest.java
+++ src/test/java/it/pagopa/pn/deliverypush/middleware/queue/producer/action/sqs/SqsActionProducerTest.java
@@ -2,6 +2,7 @@ package it.pagopa.pn.deliverypush.middleware.queue.producer.action.sqs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import software.amazon.awssdk.services.sqs.SqsClient;
@@ -19,8 +20,6 @@ class SqsActionProducerTest {
     void setUp() {
         sqsClient = Mockito.mock(SqsClient.class);
         objectMapper = Mockito.mock(ObjectMapper.class);
-
-        producer = new SqsActionProducer(sqsClient, "Test", objectMapper);
     }
-    
+
 }
