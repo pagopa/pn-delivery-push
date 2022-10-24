aa837ec08e278b77d86b41387b84e3d509b3e9fd
fabriziolana <fabrizio.lana@necsy.it>
2022-10-24 17:08:00 +0200

    PN-2264: fix test

--- src/test/java/it/pagopa/pn/deliverypush/middleware/dao/actiondao/dynamo/FutureActionEntityDaoDynamoTest.java
+++ src/test/java/it/pagopa/pn/deliverypush/middleware/dao/actiondao/dynamo/FutureActionEntityDaoDynamoTest.java
@@ -1,16 +1,37 @@
 package it.pagopa.pn.deliverypush.middleware.dao.actiondao.dynamo;
 
+import it.pagopa.pn.deliverypush.PnDeliveryPushConfigs;
+import it.pagopa.pn.deliverypush.middleware.dao.actiondao.dynamo.entity.FutureActionEntity;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.Mockito;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 
 class FutureActionEntityDaoDynamoTest {
 
-    @Test
-    void putIfAbsent() {
+    private FutureActionEntityDaoDynamo actionEntityDaoDynamo;
+    private DynamoDbEnhancedClient dynamoDbEnhancedClient;
+    private PnDeliveryPushConfigs cfg;
+
+    @BeforeEach
+    public void setup(){
+        cfg = Mockito.mock(PnDeliveryPushConfigs.class);
+        dynamoDbEnhancedClient = Mockito.mock(DynamoDbEnhancedClient.class);
+
+        Mockito.when(cfg.getFutureActionDao()).thenReturn(Mockito.mock(PnDeliveryPushConfigs.FutureActionDao.class));
+
+        actionEntityDaoDynamo = new FutureActionEntityDaoDynamo(dynamoDbEnhancedClient, cfg);
     }
 
     @Test
-    void findByTimeSlot() {
+    void putIfAbsent() {
+        Assertions.assertThrows(UnsupportedOperationException.class, () -> actionEntityDaoDynamo.putIfAbsent(new FutureActionEntity()));
+    }
+
+    @Test
+    void testPreparePut() {
+        Assertions.assertDoesNotThrow(() -> actionEntityDaoDynamo.preparePut(new FutureActionEntity()));
+
     }
 }
