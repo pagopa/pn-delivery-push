245575816e78e5e0dde8a3678e7909d80185e8fc
Alessandro Masci <alessandro.masci@pagopa.it>
2022-11-02 16:48:58 +0100

    PN-2511: add script for AGID tool

--- scripts/openapi/internal-to-external.sh
+++ scripts/openapi/internal-to-external.sh
@@ -4,3 +4,7 @@ cat docs/openapi/api-internal-b2b-webhook-v1.yaml \
     | grep -v "# NOT YET IMPLEMENTED" \
     | sed -e '/# ONLY EXTERNAL/s/^#//' \
     > docs/openapi/api-external-b2b-webhook-v1.yaml
+
+swagger-cli bundle -o docs/openapi/api-external-b2b-webhook-bundle.yaml -t yaml docs/openapi/api-external-b2b-webhook-v1.yaml
+
+spectral lint -r https://italia.github.io/api-oas-checker/spectral.yml docs/openapi/api-external-b2b-webhook-bundle.yaml
