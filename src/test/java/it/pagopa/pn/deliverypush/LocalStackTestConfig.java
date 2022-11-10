e001d5b132ef84fe479d0736a85e5f1093860c05
fabriziolana <fabrizio.lana@necsy.it>
2022-11-10 12:36:25 +0100

    PN-2530: IT con testcontainer

---
+++ src/test/java/it/pagopa/pn/deliverypush/LocalStackTestConfig.java
@@ -0,0 +1,49 @@
+package it.pagopa.pn.deliverypush;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.core.io.ClassPathResource;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.DockerImageName;
+
+import java.io.IOException;
+
+import static org.testcontainers.containers.localstack.LocalStackContainer.Service.DYNAMODB;
+
+/**
+ * Classe che permette di creare un container Docker di LocalStack.
+ * Il container (e quindi la classe) può essere condivisa tra più classi di test.
+ * Per utilizzare questa classe, le classi di test dovranno essere annotate con
+ * @Import(LocalStackTestConfig.class)
+ */
+@TestConfiguration
+public class LocalStackTestConfig {
+
+    static LocalStackContainer localStack =
+            new LocalStackContainer(DockerImageName.parse(getImageName()).asCompatibleSubstituteFor("localstack/localstack"))
+                    .withServices(DYNAMODB)
+                    .withClasspathResourceMapping("testcontainers/init.sh",
+                            "/docker-entrypoint-initaws.d/make-storages.sh", BindMode.READ_ONLY)
+                    .withClasspathResourceMapping("testcontainers/credentials",
+                            "/root/.aws/credentials", BindMode.READ_ONLY)
+                    .withNetworkAliases("localstack")
+                    .withNetwork(Network.builder().createNetworkCmdModifier(cmd -> cmd.withName("delivery-push-net")).build())
+                    .waitingFor(Wait.forLogMessage(".*Initialization terminated.*", 1));
+
+    static {
+        localStack.start();
+        System.setProperty("aws.endpoint-url", localStack.getEndpointOverride(DYNAMODB).toString());
+        try {
+            System.setProperty("aws.sharedCredentialsFile", new ClassPathResource("testcontainers/credentials").getFile().getAbsolutePath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static String getImageName() {
+        return System.getProperty("localstack_image_name", "public.ecr.aws/localstack/localstack:1.0.4");
+    }
+}
---
+++ src/test/resources/testcontainers/init.sh
@@ -0,0 +1,104 @@
+echo "### CREATE QUEUES ###"
+queues="local-delivery-push-safestorage-inputs local-delivery-push-actions local-ext-channels-inputs local-ext-channels-outputs local-delivery-push-actions-done local-ext-channels-elab-res"
+for qn in  $( echo $queues | tr " " "\n" ) ; do
+    echo creating queue $qn ...
+    aws --profile default --region us-east-1 --endpoint-url http://localstack:4566 \
+        sqs create-queue \
+        --attributes '{"DelaySeconds":"2"}' \
+        --queue-name $qn
+done
+
+echo " - Create pn-delivery-push TABLES"
+aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
+    dynamodb create-table \
+    --table-name Timelines \
+    --attribute-definitions \
+        AttributeName=iun,AttributeType=S \
+        AttributeName=timelineElementId,AttributeType=S \
+    --key-schema \
+        AttributeName=iun,KeyType=HASH \
+        AttributeName=timelineElementId,KeyType=RANGE \
+    --provisioned-throughput \
+        ReadCapacityUnits=10,WriteCapacityUnits=5
+
+aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
+    dynamodb create-table \
+    --table-name PaperNotificationFailed \
+    --attribute-definitions \
+        AttributeName=recipientId,AttributeType=S \
+        AttributeName=iun,AttributeType=S \
+    --key-schema \
+        AttributeName=recipientId,KeyType=HASH \
+        AttributeName=iun,KeyType=RANGE \
+    --provisioned-throughput \
+        ReadCapacityUnits=10,WriteCapacityUnits=5
+
+aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
+    dynamodb create-table \
+    --table-name Action \
+    --attribute-definitions \
+        AttributeName=actionId,AttributeType=S \
+    --key-schema \
+        AttributeName=actionId,KeyType=HASH \
+    --provisioned-throughput \
+        ReadCapacityUnits=10,WriteCapacityUnits=5
+
+aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
+    dynamodb create-table \
+    --table-name FutureAction \
+    --attribute-definitions \
+        AttributeName=timeSlot,AttributeType=S \
+        AttributeName=actionId,AttributeType=S \
+    --key-schema \
+        AttributeName=timeSlot,KeyType=HASH \
+        AttributeName=actionId,KeyType=RANGE \
+    --provisioned-throughput \
+        ReadCapacityUnits=10,WriteCapacityUnits=5
+
+aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
+    dynamodb create-table \
+    --table-name LastPollForFutureAction \
+    --attribute-definitions \
+        AttributeName=lastPoolKey,AttributeType=N \
+    --key-schema \
+        AttributeName=lastPoolKey,KeyType=HASH \
+    --provisioned-throughput \
+        ReadCapacityUnits=10,WriteCapacityUnits=5
+
+aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
+    dynamodb create-table \
+    --table-name PnDeliveryPushShedLock \
+    --attribute-definitions \
+        AttributeName=_id,AttributeType=S \
+    --key-schema \
+        AttributeName=_id,KeyType=HASH \
+    --provisioned-throughput \
+        ReadCapacityUnits=10,WriteCapacityUnits=5
+
+
+aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
+    dynamodb create-table \
+    --table-name WebhookStreams  \
+    --attribute-definitions \
+        AttributeName=hashKey,AttributeType=S \
+        AttributeName=sortKey,AttributeType=S \
+    --key-schema \
+        AttributeName=hashKey,KeyType=HASH \
+        AttributeName=sortKey,KeyType=RANGE \
+    --provisioned-throughput \
+        ReadCapacityUnits=10,WriteCapacityUnits=5
+
+aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
+    dynamodb create-table \
+    --table-name WebhookEvents  \
+    --attribute-definitions \
+        AttributeName=hashKey,AttributeType=S \
+        AttributeName=sortKey,AttributeType=S \
+    --key-schema \
+        AttributeName=hashKey,KeyType=HASH \
+        AttributeName=sortKey,KeyType=RANGE \
+    --provisioned-throughput \
+        ReadCapacityUnits=10,WriteCapacityUnits=5
+
+
+echo "Initialization terminated"
