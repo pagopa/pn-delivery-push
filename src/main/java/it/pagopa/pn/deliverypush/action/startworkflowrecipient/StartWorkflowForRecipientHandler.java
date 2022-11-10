014e0d8b1a64441429271770ccdf8330719ff7bb
Antonio Pezzuto <antonio.pezzuto@pagopa.it>
2022-11-10 18:07:48 +0100

    PN-1860

--- src/main/java/it/pagopa/pn/deliverypush/action/startworkflowrecipient/StartWorkflowForRecipientHandler.java
+++ src/main/java/it/pagopa/pn/deliverypush/action/startworkflowrecipient/StartWorkflowForRecipientHandler.java
@@ -8,6 +8,7 @@ import it.pagopa.pn.deliverypush.action.details.RecipientsWorkflowDetails;
 import it.pagopa.pn.deliverypush.action.utils.AarUtils;
 import it.pagopa.pn.deliverypush.action.utils.CourtesyMessageUtils;
 import it.pagopa.pn.deliverypush.dto.ext.delivery.notification.NotificationInt;
+import it.pagopa.pn.deliverypush.dto.ext.delivery.notification.NotificationRecipientInt;
 import it.pagopa.pn.deliverypush.service.NotificationService;
 import it.pagopa.pn.deliverypush.service.SchedulerService;
 import lombok.extern.slf4j.Slf4j;
@@ -40,7 +41,7 @@ public class StartWorkflowForRecipientHandler {
         log.info("Start notification workflow for recipient - iun {} id {}", iun, recIndex);
         
         NotificationInt notification = notificationService.getNotificationByIun(iun);
-        notification.getRecipients().get(recIndex).toBuilder().quickAccessLinkToken(details.getQuickAccessLinkToken());
+        unrichNotificationWithQuickAccessLinkToken(notification, recIndex, details);
         generateAAR(notification, recIndex);
 
         //... Invio messaggio di cortesia ... 
@@ -50,6 +51,11 @@ public class StartWorkflowForRecipientHandler {
         scheduleChooseDeliveryMode(iun, recIndex);
     }
 
+    private void unrichNotificationWithQuickAccessLinkToken(NotificationInt notification, int recIndex, RecipientsWorkflowDetails details ) {
+      NotificationRecipientInt recipient = notification.getRecipients().get(recIndex);
+      recipient.toBuilder().quickAccessLinkToken(details.getQuickAccessLinkToken());
+      
+    }
     private void generateAAR(NotificationInt notification, Integer recIndex) {
         // ... genero il pdf dell'AAR, salvo su Safestorage e genero elemento in timeline AAR_GENERATION, potrebbe servirmi dopo ...
         PnAuditLogEvent logEvent = auditLogBuilder
