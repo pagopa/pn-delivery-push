5020b36f8bb506370fff0cd7df7f029018f8fb3b
fabriziolana <fabrizio.lana@necsy.it>
2022-11-11 10:44:14 +0100

    PN-2546: cambio libreria aws

--- src/test/java/it/pagopa/pn/deliverypush/LocalStackTestConfig.java
+++ src/test/java/it/pagopa/pn/deliverypush/LocalStackTestConfig.java
@@ -29,7 +29,7 @@ public class LocalStackTestConfig {
                     .withClasspathResourceMapping("testcontainers/credentials",
                             "/root/.aws/credentials", BindMode.READ_ONLY)
                     .withNetworkAliases("localstack")
-                    .withNetwork(Network.builder().createNetworkCmdModifier(cmd -> cmd.withName("delivery-push-net" + System.currentTimeMillis())).build())
+                    .withNetwork(Network.builder().createNetworkCmdModifier(cmd -> cmd.withName("delivery-push-net")).build())
                     .waitingFor(Wait.forLogMessage(".*Initialization terminated.*", 1));
 
     static {
