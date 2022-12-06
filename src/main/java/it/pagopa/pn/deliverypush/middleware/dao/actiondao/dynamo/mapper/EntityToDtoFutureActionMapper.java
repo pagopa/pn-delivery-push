1d796febfd0d300745ceb9b32f80227512d0ed03
MTurraPpa <94441477+MTurraPpa@users.noreply.github.com>
2022-12-06 11:08:29 +0100

    PN-1860: hot fix for empty detail in q

--- src/main/java/it/pagopa/pn/deliverypush/middleware/dao/actiondao/dynamo/mapper/EntityToDtoFutureActionMapper.java
+++ src/main/java/it/pagopa/pn/deliverypush/middleware/dao/actiondao/dynamo/mapper/EntityToDtoFutureActionMapper.java
@@ -1,7 +1,11 @@
 package it.pagopa.pn.deliverypush.middleware.dao.actiondao.dynamo.mapper;
 
+import it.pagopa.pn.deliverypush.middleware.dao.actiondao.dynamo.entity.ActionDetailsEntity;
 import it.pagopa.pn.deliverypush.middleware.dao.actiondao.dynamo.entity.FutureActionEntity;
 import it.pagopa.pn.deliverypush.middleware.queue.producer.abstractions.actionspool.Action;
+import it.pagopa.pn.deliverypush.middleware.queue.producer.abstractions.actionspool.ActionDetails;
+import it.pagopa.pn.deliverypush.middleware.queue.producer.abstractions.actionspool.ActionType;
+import it.pagopa.pn.deliverypush.service.mapper.SmartMapper;
 
 public class EntityToDtoFutureActionMapper {
     private EntityToDtoFutureActionMapper(){}
@@ -13,7 +17,12 @@ public class EntityToDtoFutureActionMapper {
                 .recipientIndex(entity.getRecipientIndex())
                 .type(entity.getType())
                 .timelineId(entity.getTimelineId())
+                .details(parseDetailsFromEntity(entity.getDetails(),entity.getType()))
                 .iun(entity.getIun());
         return builder.build();
+    }
+
+    private static ActionDetails parseDetailsFromEntity(ActionDetailsEntity entity, ActionType type) {
+      return SmartMapper.mapToClass(entity, type.getDetailsJavaClass());
     }
 }
