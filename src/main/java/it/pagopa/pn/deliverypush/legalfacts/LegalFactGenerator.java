fade59832457f88d54df6363c43b24ef4fa1bf62
Lorenzo <l.cocciarelli@appsonrails.it>
2022-12-12 16:25:56 +0100

    fix: change to layout of NotificationAAR and set qrcode size to 130

--- src/main/java/it/pagopa/pn/deliverypush/legalfacts/LegalFactGenerator.java
+++ src/main/java/it/pagopa/pn/deliverypush/legalfacts/LegalFactGenerator.java
@@ -341,7 +341,7 @@ public class LegalFactGenerator {
     
       String url = String.format(templateUrl, recipient.getQuickAccessLinkToken());
       // Definire altezza e larghezza del qrcode
-      return "data:image/png;base64, ".concat(Base64Utils.encodeToString(QrCodeUtils.generateQRCodeImage(url, 0, 0)));
+      return "data:image/png;base64, ".concat(Base64Utils.encodeToString(QrCodeUtils.generateQRCodeImage(url, 130, 130)));
     }
 
     
