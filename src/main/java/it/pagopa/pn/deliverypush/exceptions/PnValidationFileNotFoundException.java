dfa5cb547b8295908ac179352169f565eb062f1c
simonerondinella <92715398+simonerondinella@users.noreply.github.com>
2022-12-14 10:29:21 +0100

    PN-2927 Modifica testo inviato a notifica rifiutata (#313)
    * PN-2927 Modifica testo inviato a notifica rifiutata
    
    * PN-2927 Aggiunti test

--- src/main/java/it/pagopa/pn/deliverypush/exceptions/PnValidationFileNotFoundException.java
+++ src/main/java/it/pagopa/pn/deliverypush/exceptions/PnValidationFileNotFoundException.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class PnValidationFileNotFoundException extends PnValidationException {
 
     public PnValidationFileNotFoundException(String errorCode, String detail, Throwable ex) {
-        super("Validazione fallita, file non trovato su safe-storage", List.of(ProblemError.builder()
+        super( detail , List.of(ProblemError.builder()
                 .code(errorCode)
                 .detail(detail)
                 .build()), ex );
