d34858678c0df9622369d4b3fa2ddf1ae96993cf
vincenzoracca <vincenzo.racca@pagopa.it>
2022-12-29 11:31:41 +0100

    PN-2896 spostato l'handler di AWS in pn-commons

--- src/main/java/it/pagopa/pn/deliverypush/springbootcfg/SQSConfig.java
+++ src/main/java/it/pagopa/pn/deliverypush/springbootcfg/SQSConfig.java
@@ -1,14 +1,11 @@
 package it.pagopa.pn.deliverypush.springbootcfg;
 
 
-import com.amazonaws.Request;
-import com.amazonaws.Response;
 import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.handlers.RequestHandler2;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import it.pagopa.pn.commons.configs.aws.AwsConfigs;
-import lombok.extern.slf4j.Slf4j;
+import it.pagopa.pn.commons.utils.CustomAWSRequestHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
@@ -33,7 +30,7 @@ public class SQSConfig {
     public AmazonSQSAsync amazonSQS() {
         if (StringUtils.hasText(awsConfigs.getEndpointUrl()))
             return AmazonSQSAsyncClientBuilder.standard()
-                    .withRequestHandlers(new CustomRequestHandler())
+                    .withRequestHandlers(new CustomAWSRequestHandler())
                     .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(awsConfigs.getEndpointUrl(), awsConfigs.getRegionCode()))
                     .build();
         else
@@ -41,19 +38,4 @@ public class SQSConfig {
                     .withRegion(awsConfigs.getRegionCode())
                     .build();
     }
-
-
-    /**
-     * Questa Handler cattura tutte le richieste, risposte ed errori di oggetti di AWS come {@link AmazonSQSAsync}.
-     * È possibile catturare gli eventi sovrascrivendo i metodi di {@link RequestHandler2}.
-     */
-    @Slf4j
-    static class CustomRequestHandler extends RequestHandler2 {
-
-        @Override
-        public void afterError(Request<?> request, Response<?> response, Exception e) {
-            log.error("Exception for: " + request.getOriginalRequestObject().toString(), e);
-        }
-    }
-
 }
