0f344b3453996b638f7125790098aa4bd8e94487
Simone Rondinella <simone.rondinella@pagopa.it>
2023-01-03 15:05:36 +0100

    pn-3304 modificare creazione queue

--- src/test/resources/testcontainers/init.sh
+++ src/test/resources/testcontainers/init.sh
@@ -1,5 +1,5 @@
 echo "### CREATE QUEUES ###"
-queues="local-delivery-push-inputs.fifo local-delivery-push-safestorage-inputs local-delivery-push-actions local-ext-channels-inputs local-ext-channels-outputs local-delivery-push-actions-done local-ext-channels-elab-res"
+queues="local-delivery-push-safestorage-inputs local-delivery-push-actions local-ext-channels-inputs local-ext-channels-outputs local-delivery-push-actions-done local-ext-channels-elab-res"
 for qn in  $( echo $queues | tr " " "\n" ) ; do
     echo creating queue $qn ...
 
@@ -7,7 +7,16 @@ for qn in  $( echo $queues | tr " " "\n" ) ; do
         sqs create-queue \
         --attributes '{"DelaySeconds":"2"}' \
         --queue-name $qn
+done
 
+echo "### CREATE QUEUES FIFO ###"
+queues_fifo="local-delivery-push-inputs.fifo"
+for qn in  $( echo $queues_fifo | tr " " "\n" ) ; do
+    echo creating queue fifo $qn ...
+    aws --profile default --region us-east-1 --endpoint-url http://localstack:4566 \
+        sqs create-queue \
+        --attributes '{"DelaySeconds":"2","FifoQueue": "true","ContentBasedDeduplication": "true"}' \
+        --queue-name $qn
 done
 
 echo " - Create pn-delivery-push TABLES"
