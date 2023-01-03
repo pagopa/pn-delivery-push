fc28eb4fc26f85b94de253fc9842af2c8f5053a8
Simone Rondinella <simone.rondinella@pagopa.it>
2023-01-03 15:55:30 +0100

    PN-3304 Revertate modifiche ad init.sh

--- src/test/resources/testcontainers/init.sh
+++ src/test/resources/testcontainers/init.sh
@@ -7,16 +7,7 @@ for qn in  $( echo $queues | tr " " "\n" ) ; do
         sqs create-queue \
         --attributes '{"DelaySeconds":"2"}' \
         --queue-name $qn
-done
 
-echo "### CREATE QUEUES FIFO ###"
-queues_fifo="local-delivery-push-inputs.fifo"
-for qn in  $( echo $queues_fifo | tr " " "\n" ) ; do
-    echo creating queue fifo $qn ...
-    aws --profile default --region us-east-1 --endpoint-url http://localstack:4566 \
-        sqs create-queue \
-        --attributes '{"DelaySeconds":"2","FifoQueue": "true","ContentBasedDeduplication": "true"}' \
-        --queue-name $qn
 done
 
 echo " - Create pn-delivery-push TABLES"
