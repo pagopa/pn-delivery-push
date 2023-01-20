c6ca6d4fe8268dd1f7515ac9410aa8f353e81282
cFiorilloMediatica <106066100+cFiorilloMediatica@users.noreply.github.com>
2023-01-20 09:53:33 +0100

    feature/PN-2961 (#349)
    * PN-2961 delegate audit log
    
    * PN-2961 audit log delegate

--- src/main/java/it/pagopa/pn/deliverypush/utils/AuditLogUtils.java
+++ src/main/java/it/pagopa/pn/deliverypush/utils/AuditLogUtils.java
@@ -18,8 +18,8 @@ public class AuditLogUtils {
 
     public static PnAuditLogEventType getAuditLogEventType(NotificationInt notification, String senderRecipientId, String mandateId) {
         if (StringUtils.hasText(mandateId)) {
-            //La request è stata ricevuta da un delagato, generazione audit log per destinatario
-            return PnAuditLogEventType.AUD_NT_LEGALOPEN_RCP;
+            //La richiesta è stata ricevuta da un delagato, generazione audit log per destinatario
+            return PnAuditLogEventType.AUD_NT_LEGALOPEN_DEL;
         } else {
             String paId = notification.getSender().getPaId();
             boolean isRequestFromSender = senderRecipientId.equals(paId);
