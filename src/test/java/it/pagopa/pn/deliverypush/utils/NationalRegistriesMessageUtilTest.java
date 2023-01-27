55613c15d4e41374719a38481b37d0cf2a9f9bcd
vincenzoracca <vincenzo.racca@pagopa.it>
2023-01-27 11:36:42 +0100

    PN-2782 commit per build

--- src/test/java/it/pagopa/pn/deliverypush/utils/NationalRegistriesMessageUtilTest.java
+++ src/test/java/it/pagopa/pn/deliverypush/utils/NationalRegistriesMessageUtilTest.java
@@ -9,6 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class NationalRegistriesMessageUtilTest {
 
+
     @Test
     void buildPublicRegistryResponseTest() {
         String correlationId = "corrId1";
