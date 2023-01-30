7990464a3940971cac77b1093d64caf8fb91a8d4
Simone Rondinella <simone.rondinella@pagopa.it>
2023-01-30 14:49:01 +0100

    PN-3834 Modificato commit id per test container

--- src/test/resources/testcontainers/initsh-for-testcontainer.sh
+++ src/test/resources/testcontainers/initsh-for-testcontainer.sh
@@ -1,5 +1,5 @@
 echo "### CREATE DELIVERY-PUSH COMPONENT ###"
-bash <(curl -s https://raw.githubusercontent.com/pagopa/pn-delivery-push/aa15c57523cc9732a3728f1b581c3e620a0ba17a/src/test/resources/testcontainers/init.sh)
+bash <(curl -s https://raw.githubusercontent.com/pagopa/pn-delivery-push/89a676dc5948d65508c626dfdd687ecac55aade1/src/test/resources/testcontainers/init.sh)
 
 ## La creazione delle queue local-delivery-push-inputs.fifo è già presente nel file init.sh di delivery. Viene duplicata su deliveryPush per solo per test-container
 
