2f45fc5d4c17aa2417d9806ba81d1aa1abbf56e5
fabriziolana <fabrizio.lana@necsy.it>
2023-02-08 11:06:00 +0100

    PN-3963: tst cov

---
+++ src/test/java/it/pagopa/pn/deliverypush/middleware/queue/consumer/handler/IOSentMessageEventHandlerTest.java
@@ -0,0 +1,48 @@
+package it.pagopa.pn.deliverypush.middleware.queue.consumer.handler;
+
+import it.pagopa.pn.api.dto.events.PnExtRegistryIOSentMessageEvent;
+import it.pagopa.pn.deliverypush.LocalStackTestConfig;
+import it.pagopa.pn.deliverypush.action.iosentmessage.IOSentMessageHandler;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.cloud.function.context.FunctionCatalog;
+import org.springframework.cloud.function.context.test.FunctionalSpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+
+import java.time.Instant;
+import java.util.function.Consumer;
+
+
+@FunctionalSpringBootTest
+@Import(LocalStackTestConfig.class)
+class IOSentMessageEventHandlerTest {
+
+    @Autowired
+    private FunctionCatalog functionCatalog;
+
+    @MockBean
+    private IOSentMessageHandler handler;
+
+    @Test
+    void pnExtRegistryIOSentMessageConsumer() {
+
+        Instant instant = Instant.now();
+
+        Consumer<Message<PnExtRegistryIOSentMessageEvent.Payload>> pnExtRegistryIOSentMessageConsumer =
+                functionCatalog.lookup(Consumer.class, "pnExtRegistryIOSentMessageConsumer");
+        PnExtRegistryIOSentMessageEvent.Payload body =PnExtRegistryIOSentMessageEvent.Payload.builder()
+                .iun("IUN")
+                .recIndex(1)
+                .sendDate(instant)
+                .build();
+
+        Message<PnExtRegistryIOSentMessageEvent.Payload> message = MessageBuilder.withPayload(body).build();
+        pnExtRegistryIOSentMessageConsumer.accept(message);
+        Mockito.verify(handler).handleIOSentMessage("IUN", 1, instant);
+    }
+
+}
