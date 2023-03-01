36b2fa71fbe64076affe88e79c79a23f33718e80
Marco Iannaccone <marco.iannaccone@pagopa.it>
2023-03-01 17:19:17 +0100

    PN-4300-4310: not yet implemented parts

--- scripts/openapi/internal-to-external.sh
+++ scripts/openapi/internal-to-external.sh
@@ -9,3 +9,7 @@ redocly bundle docs/openapi/api-external-b2b-webhook.yaml --output docs/openapi/
 
 #spectral lint -r https://italia.github.io/api-oas-checker/spectral-security.yml docs/openapi/api-external-b2b-webhook-bundle.yaml
 spectral lint -r https://italia.github.io/api-oas-checker/spectral.yml docs/openapi/api-external-b2b-webhook-bundle.yaml
+
+# single files checks
+#spectral lint -r https://italia.github.io/api-oas-checker/spectral-security.yml docs/openapi/api-internal-b2b-webhook.yaml
+#spectral lint -r https://italia.github.io/api-oas-checker/spectral-security.yml docs/openapi/api-internal-pn-delivery-push.yaml
