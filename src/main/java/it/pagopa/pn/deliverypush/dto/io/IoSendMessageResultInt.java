7c8bffbcfe4c9ef89b7fc59722fe7732d6a32f93
Simone Rondinella <simone.rondinella@pagopa.it>
2023-03-03 14:16:27 +0100

    PN-4437 Modificato enum

--- src/main/java/it/pagopa/pn/deliverypush/dto/io/IoSendMessageResultInt.java
+++ src/main/java/it/pagopa/pn/deliverypush/dto/io/IoSendMessageResultInt.java
@@ -1,8 +1,10 @@
 package it.pagopa.pn.deliverypush.dto.io;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+import lombok.ToString;
 
+@Getter
+@ToString
 public enum IoSendMessageResultInt {
     NOT_SENT_OPTIN_ALREADY_SENT("NOT_SENT_OPTIN_ALREADY_SENT"),
     
@@ -15,25 +17,4 @@ public enum IoSendMessageResultInt {
     IoSendMessageResultInt(String value) {
         this.value = value;
     }
-
-    @JsonValue
-    public String getValue() {
-        return value;
-    }
-
-    @Override
-    public String toString() {
-        return String.valueOf(value);
-    }
-
-    @JsonCreator
-    public static IoSendMessageResultInt fromValue(String value) {
-        for (IoSendMessageResultInt b : IoSendMessageResultInt.values()) {
-            if (b.value.equals(value)) {
-                return b;
-            }
-        }
-        throw new IllegalArgumentException("Unexpected value '" + value + "'");
-    }
-    
 }
