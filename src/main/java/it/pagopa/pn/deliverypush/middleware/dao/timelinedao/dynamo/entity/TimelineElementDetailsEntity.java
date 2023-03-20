1d124608971ecc52e5a7007400f1681ece718f9a
simonerondinella <92715398+simonerondinella@users.noreply.github.com>
2023-03-20 09:46:52 +0100

    Feature/pn 4566 (#452)
    * PN-4566 Modifiche per risoluzione task
    
    * PN-4566 Modifica test
    
    * PN-4566 Ulteriori modifiche
    
    * Pn-4566 modifica attachment details

--- src/main/java/it/pagopa/pn/deliverypush/middleware/dao/timelinedao/dynamo/entity/TimelineElementDetailsEntity.java
+++ src/main/java/it/pagopa/pn/deliverypush/middleware/dao/timelinedao/dynamo/entity/TimelineElementDetailsEntity.java
@@ -16,6 +16,7 @@ import java.util.List;
 @ToString
 @DynamoDbBean
 public class TimelineElementDetailsEntity {
+
     @Getter(onMethod=@__({@DynamoDbAttribute("recIndex")}))  private Integer recIndex;
     @Getter(onMethod=@__({@DynamoDbAttribute("physicalAddress")}))  private PhysicalAddressEntity physicalAddress;
     @Getter(onMethod=@__({@DynamoDbAttribute("digitalAddress")}))  private DigitalAddressEntity digitalAddress;
@@ -66,5 +67,5 @@ public class TimelineElementDetailsEntity {
     @Getter(onMethod=@__({@DynamoDbAttribute("envelopeWeight")})) private Integer envelopeWeight;
     @Getter(onMethod=@__({@DynamoDbAttribute("deliveryDetailCode")})) private String deliveryDetailCode;
     @Getter(onMethod=@__({@DynamoDbAttribute("deliveryFailureCause")})) private String deliveryFailureCause;
-    @Getter(onMethod=@__({@DynamoDbAttribute("attachments")})) private AttachmentDetailsEntity attachments;
+    @Getter(onMethod=@__({@DynamoDbAttribute("attachments")})) private List<AttachmentDetailsEntity> attachments;
 }
