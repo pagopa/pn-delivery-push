4abc90cd42ff5c69b14c91e783e58f945d759014
EmanueleBonofiglio <emanuele.bonofiglio@grupposcai.it>
2023-03-21 14:26:05 +0100

    PN-4803 fix smartMapper

--- src/main/java/it/pagopa/pn/deliverypush/service/mapper/SmartMapper.java
+++ src/main/java/it/pagopa/pn/deliverypush/service/mapper/SmartMapper.java
@@ -4,15 +4,24 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 
 public class SmartMapper {
+    private static ModelMapper modelMapper;
+
     private SmartMapper (){}
-    
+
     public static  <S,T> T mapToClass(S source, Class<T> destinationClass ){
         T result = null;
         if( source != null) {
-            ModelMapper modelMapper = new ModelMapper();
-            modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+            ModelMapper modelMapper = getModelMapper();
             result = modelMapper.map(source, destinationClass );
         }
         return result;
+    }
+
+    private static ModelMapper getModelMapper(){
+        if(modelMapper == null){
+            modelMapper = new ModelMapper();
+            modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        }
+        return modelMapper;
     }
 }
