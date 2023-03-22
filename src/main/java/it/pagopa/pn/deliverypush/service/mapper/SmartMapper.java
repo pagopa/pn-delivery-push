edd90bf4654cc46ebccc3d09f3f194e669a33444
EmanueleBonofiglio <emanuele.bonofiglio@grupposcai.it>
2023-03-22 17:23:49 +0100

    PN-4803 fix threadSafety

--- src/main/java/it/pagopa/pn/deliverypush/service/mapper/SmartMapper.java
+++ src/main/java/it/pagopa/pn/deliverypush/service/mapper/SmartMapper.java
@@ -8,20 +8,18 @@ public class SmartMapper {
 
     private SmartMapper (){}
 
+    static{
+        modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+    }
+
     public static  <S,T> T mapToClass(S source, Class<T> destinationClass ){
         T result = null;
         if( source != null) {
-            ModelMapper modelMapper = getModelMapper();
             result = modelMapper.map(source, destinationClass );
         }
         return result;
     }
 
-    private static ModelMapper getModelMapper(){
-        if(modelMapper == null){
-            modelMapper = new ModelMapper();
-            modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-        }
-        return modelMapper;
-    }
+
 }
