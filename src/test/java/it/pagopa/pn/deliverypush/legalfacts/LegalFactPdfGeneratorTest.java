fe6187980f5d8d5792b4c9faffe2bf06ddfede7b
Marco Iannaccone <marco.iannaccone@pagopa.it>
2023-04-05 11:34:30 +0200

    PN-5262: PEC delivery workflow digital address source

--- src/test/java/it/pagopa/pn/deliverypush/legalfacts/LegalFactPdfGeneratorTest.java
+++ src/test/java/it/pagopa/pn/deliverypush/legalfacts/LegalFactPdfGeneratorTest.java
@@ -11,6 +11,7 @@ import it.pagopa.pn.commons.configs.MVPParameterConsumer;
 import it.pagopa.pn.deliverypush.PnDeliveryPushConfigs;
 import it.pagopa.pn.deliverypush.action.utils.EndWorkflowStatus;
 import it.pagopa.pn.deliverypush.action.utils.InstantNowSupplier;
+import it.pagopa.pn.deliverypush.dto.address.DigitalAddressSourceInt;
 import it.pagopa.pn.deliverypush.dto.address.LegalDigitalAddressInt;
 import it.pagopa.pn.deliverypush.dto.address.PhysicalAddressInt;
 import it.pagopa.pn.deliverypush.dto.ext.datavault.RecipientTypeInt;
@@ -310,6 +311,7 @@ class LegalFactPdfGeneratorTest {
 						.type(LegalDigitalAddressInt.LEGAL_DIGITAL_ADDRESS_TYPE.PEC)
 						.address("prova@test.com")
 						.build())
+				.digitalAddressSource(DigitalAddressSourceInt.PLATFORM)
 				.responseStatus(ResponseStatusInt.KO)
 				.notificationDate(Instant.now().minus(10, ChronoUnit.MINUTES))
 				.build();
@@ -320,6 +322,18 @@ class LegalFactPdfGeneratorTest {
 						.type(LegalDigitalAddressInt.LEGAL_DIGITAL_ADDRESS_TYPE.PEC)
 						.address("pçroà2@test.com")
 						.build())
+				.digitalAddressSource(DigitalAddressSourceInt.SPECIAL)
+				.responseStatus(status)
+				.notificationDate(Instant.now().minus(5, ChronoUnit.MINUTES))
+				.build();
+
+		SendDigitalFeedbackDetailsInt sdf3 = SendDigitalFeedbackDetailsInt.builder()
+				.recIndex( 0 )
+				.digitalAddress(LegalDigitalAddressInt.builder()
+						.type(LegalDigitalAddressInt.LEGAL_DIGITAL_ADDRESS_TYPE.PEC)
+						.address("pçroà3@test.com")
+						.build())
+				.digitalAddressSource(DigitalAddressSourceInt.GENERAL)
 				.responseStatus(status)
 				.notificationDate(Instant.now().minus(5, ChronoUnit.MINUTES))
 				.build();
@@ -327,6 +341,7 @@ class LegalFactPdfGeneratorTest {
 		List<SendDigitalFeedbackDetailsInt> result = new ArrayList<SendDigitalFeedbackDetailsInt>();
 		result.add(sdf);
 		result.add(sdf2);
+		result.add(sdf3);
 		return result;
 	}
 
