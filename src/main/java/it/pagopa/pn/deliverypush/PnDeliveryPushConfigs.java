d2277c5bfd0002a6d89723811c6ebf72a87e11c9
Christian Fiorillo <c.fiorillo@mediaticaspa.it>
2023-04-11 20:18:47 +0200

    PN-3718 improved faq links in templates

--- src/main/java/it/pagopa/pn/deliverypush/PnDeliveryPushConfigs.java
+++ src/main/java/it/pagopa/pn/deliverypush/PnDeliveryPushConfigs.java
@@ -165,6 +165,7 @@ public class PnDeliveryPushConfigs {
         private String directAccessUrlTemplateLegal;
         private String faqUrlTemplateSuffix;
         private String faqCompletionMomentHash;
+        private String faqSendHash;
         private String quickAccessUrlAarDetailSuffix;
         private String landingUrl;
    }
--- src/main/java/it/pagopa/pn/deliverypush/legalfacts/LegalFactGenerator.java
+++ src/main/java/it/pagopa/pn/deliverypush/legalfacts/LegalFactGenerator.java
@@ -56,6 +56,7 @@ public class LegalFactGenerator {
     public static final String FIELD_PIATTAFORMA_NOTIFICHE_URL_LABEL = "piattaformaNotificheURLLabel";
     public static final String FIELD_PN_FAQ_URL = "PNFaqURL";
     public static final String FIELD_PN_FAQ_COMPLETION_MOMENT_URL = "PNFaqCompletionMomentURL";
+    public static final String FIELD_SEND_URL = "PNFaqSendURL";
     public static final String FIELD_END_WORKFLOW_STATUS = "endWorkflowStatus";
     public static final String FIELD_END_WORKFLOW_DATE = "endWorkflowDate";
     public static final String FIELD_LEGALFACT_CREATION_DATE = "legalFactCreationDate";
@@ -64,6 +65,8 @@ public class LegalFactGenerator {
     public static final String FIELD_RECIPIENT_TYPE = "recipientType";
     public static final String FIELD_DELEGATE = "delegate";
     public static final String FIELD_PERFEZIONAMENTO = "perfezionamentoURL";
+
+    public static final String FIELD_SENDURL = "sendURL";
     public static final String FIELD_LOGO = "logoBase64";
 
     private final DocumentComposition documentComposition;
@@ -336,6 +339,7 @@ public class LegalFactGenerator {
         templateModel.put(FIELD_PIATTAFORMA_NOTIFICHE_URL_LABEL, this.getAccessUrlLabel(recipient) );
         templateModel.put(FIELD_PN_FAQ_URL, this.getFAQAccessLink());
         templateModel.put(FIELD_PN_FAQ_COMPLETION_MOMENT_URL, this.getFAQCompletionMomentAccessLink());
+        templateModel.put(FIELD_SEND_URL, this.getFAQSendURL());
         templateModel.put(FIELD_QUICK_ACCESS_LINK, this.getQuickAccessLink(recipient, quickAccesstoken) );
         templateModel.put(FIELD_RECIPIENT_TYPE, this.getRecipientTypeForHTMLTemplate(recipient));
 
@@ -380,6 +384,10 @@ public class LegalFactGenerator {
 
     private String getFAQCompletionMomentAccessLink() {
         return this.getFAQAccessLink() + "#" + pnDeliveryPushConfigs.getWebapp().getFaqCompletionMomentHash();
+    }
+
+    private String getFAQSendURL() {
+        return this.getFAQAccessLink() + "#" + pnDeliveryPushConfigs.getWebapp().getFaqSendHash();
     }
 
     private String getRecipientTypeForHTMLTemplate(NotificationRecipientInt recipientInt) {
