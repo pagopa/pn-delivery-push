c037231635518279d3df0eb28ff27bb6d5a9d89c
Simone Rondinella <simone.rondinella@pagopa.it>
2023-04-26 17:23:25 +0200

    PN-5493 Fix richieste a valle review

--- src/main/java/it/pagopa/pn/deliverypush/middleware/responsehandler/AddressManagerResponseHandler.java
+++ src/main/java/it/pagopa/pn/deliverypush/middleware/responsehandler/AddressManagerResponseHandler.java
@@ -4,7 +4,6 @@ import it.pagopa.pn.addressmanager.generated.openapi.clients.addressmanager.mode
 import it.pagopa.pn.deliverypush.action.startworkflow.notificationvalidation.NotificationValidationActionHandler;
 import it.pagopa.pn.deliverypush.action.utils.TimelineUtils;
 import it.pagopa.pn.deliverypush.dto.ext.addressmanager.NormalizeItemsResultInt;
-import it.pagopa.pn.deliverypush.dto.timeline.TimelineEventIdBuilder;
 import it.pagopa.pn.deliverypush.service.mapper.AddressManagerMapper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,11 +24,5 @@ public class AddressManagerResponseHandler {
         NormalizeItemsResultInt normalizeItemsResult = AddressManagerMapper.externalToInternal(response);
 
         notificationValidationActionHandler.handleValidateAndNormalizeAddressResponse(iun, normalizeItemsResult);
-    }
-
-    public String getIunFromCorrelationId(String correlationId)
-    {
-        //<timelineId = CATEGORY_VALUE>;IUN_<IUN_VALUE>;RECINDEX_<RECINDEX_VALUE>...
-        return correlationId.split("\\" + TimelineEventIdBuilder.DELIMITER)[1].replace("IUN_", "");
     }
 }
