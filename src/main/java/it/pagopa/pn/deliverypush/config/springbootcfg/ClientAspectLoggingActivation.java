e52c3d58c32f4596d3205476eacda80ecb9f181c
Simone Rondinella <simone.rondinella@pagopa.it>
2023-05-24 18:29:50 +0200

    PN-5476 Attivazione aspect

---
+++ src/main/java/it/pagopa/pn/deliverypush/config/springbootcfg/ClientAspectLoggingActivation.java
@@ -0,0 +1,6 @@
+package it.pagopa.pn.deliverypush.config.springbootcfg;
+
+import it.pagopa.pn.commons.utils.ClientAspectLogging;
+
+public class ClientAspectLoggingActivation extends ClientAspectLogging {
+}
---
+++ src/main/java/it/pagopa/pn/deliverypush/config/springbootcfg/ServerAspectLoggingActivation.java
@@ -0,0 +1,6 @@
+package it.pagopa.pn.deliverypush.config.springbootcfg;
+
+import it.pagopa.pn.commons.utils.ServerAspectLogging;
+
+public class ServerAspectLoggingActivation extends ServerAspectLogging {
+}
