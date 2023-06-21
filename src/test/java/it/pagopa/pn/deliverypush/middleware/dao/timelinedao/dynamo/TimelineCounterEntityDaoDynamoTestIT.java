ee2981e180d7aff12f3b23eb5cdeadeea618fa63
fabriziolana <fabrizio.lana@necsy.it>
2023-06-21 08:57:48 +0200

    PN-6796: fix mancato salvataggio eventi progress ravvicinati

---
+++ src/test/java/it/pagopa/pn/deliverypush/middleware/dao/timelinedao/dynamo/TimelineCounterEntityDaoDynamoTestIT.java
@@ -0,0 +1,52 @@
+package it.pagopa.pn.deliverypush.middleware.dao.timelinedao.dynamo;
+
+import it.pagopa.pn.commons.abstractions.impl.MiddlewareTypes;
+import it.pagopa.pn.deliverypush.LocalStackTestConfig;
+import it.pagopa.pn.deliverypush.middleware.dao.actiondao.ActionDao;
+import it.pagopa.pn.deliverypush.middleware.dao.failednotificationdao.PaperNotificationFailedDao;
+import it.pagopa.pn.deliverypush.middleware.dao.timelinedao.TimelineCounterEntityDao;
+import it.pagopa.pn.deliverypush.middleware.dao.timelinedao.TimelineDao;
+import it.pagopa.pn.deliverypush.middleware.dao.timelinedao.dynamo.entity.TimelineCounterEntity;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(SpringExtension.class)
+@TestPropertySource(properties = {
+        TimelineDao.IMPLEMENTATION_TYPE_PROPERTY_NAME + "=" + MiddlewareTypes.DYNAMO,
+        TimelineCounterEntityDao.IMPLEMENTATION_TYPE_PROPERTY_NAME + "=" + MiddlewareTypes.DYNAMO,
+        PaperNotificationFailedDao.IMPLEMENTATION_TYPE_PROPERTY_NAME + "=" + MiddlewareTypes.DYNAMO,
+        ActionDao.IMPLEMENTATION_TYPE_PROPERTY_NAME + "=" + MiddlewareTypes.DYNAMO
+})
+@SpringBootTest
+@Import(LocalStackTestConfig.class)
+class TimelineCounterEntityDaoDynamoTestIT {
+
+    @Autowired
+    private TimelineCounterEntityDao dao;
+
+    @Test
+    void getCounter() {
+
+        String timeline = System.currentTimeMillis()+"";
+        TimelineCounterEntity r = dao.getCounter(timeline);
+
+        Assertions.assertNotNull(r);
+        Assertions.assertEquals(1L, r.getCounter());
+
+
+        TimelineCounterEntity r1 = dao.getCounter(timeline);
+
+        Assertions.assertNotNull(r1);
+        Assertions.assertEquals(2L, r1.getCounter());
+
+    }
+}
