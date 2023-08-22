d7e0e02c40f1b3cdf65db1a3335eadfc8c8292ef
simonerondinella <92715398+simonerondinella@users.noreply.github.com>
2023-08-22 09:17:29 +0200

    feature/PN-7179 (#609)
    * PN-7179 Init modifiche
    
    * PN-7179 Modifica html
    
    * PN-7179 Modifica html
    
    * PN-7179 Modifica html e metodo di failure

--- src/main/java/it/pagopa/pn/deliverypush/action/completionworkflow/FailureWorkflowHandler.java
+++ src/main/java/it/pagopa/pn/deliverypush/action/completionworkflow/FailureWorkflowHandler.java
@@ -1,12 +1,10 @@
 package it.pagopa.pn.deliverypush.action.completionworkflow;
 
-import it.pagopa.pn.commons.configs.MVPParameterConsumer;
 import it.pagopa.pn.commons.log.PnAuditLogEvent;
 import it.pagopa.pn.deliverypush.action.utils.EndWorkflowStatus;
 import it.pagopa.pn.deliverypush.action.utils.TimelineUtils;
 import it.pagopa.pn.deliverypush.dto.ext.delivery.notification.NotificationInt;
 import it.pagopa.pn.deliverypush.dto.timeline.details.DigitalDeliveryCreationRequestDetailsInt;
-import it.pagopa.pn.deliverypush.dto.timeline.details.NotHandledDetailsInt;
 import it.pagopa.pn.deliverypush.service.TimelineService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,53 +16,29 @@ import java.time.Instant;
 @AllArgsConstructor
 @Slf4j
 public class FailureWorkflowHandler {
-    private final MVPParameterConsumer mvpParameterConsumer;
     private final RefinementScheduler refinementScheduler;
     private final RegisteredLetterSender registeredLetterSender;
     private final TimelineUtils timelineUtils;
     private final TimelineService timelineService;
 
+    public void scheduleRefinementAndSendRegisteredLetter(NotificationInt notification, int recIndex, Instant completionWorkflowDate){
+        refinementScheduler.scheduleDigitalRefinement(notification, recIndex, completionWorkflowDate, EndWorkflowStatus.FAILURE);
+        registeredLetterSender.prepareSimpleRegisteredLetter(notification, recIndex);
+    }
+    
     public void handleFailureWorkflow(NotificationInt notification, int recIndex, PnAuditLogEvent logEvent, DigitalDeliveryCreationRequestDetailsInt timelineDetails) {
-        String senderTaxId = notification.getSender().getPaTaxId();
-        Instant legalFactCreationDate = Instant.now();
-        EndWorkflowStatus status = timelineDetails.getEndWorkflowStatus();
         String legalFactId = timelineDetails.getLegalFactId();
                 
-        addFailureWorkflowTimelineElement(recIndex, notification, logEvent, legalFactId, legalFactCreationDate);
-
-        if (Boolean.FALSE.equals(mvpParameterConsumer.isMvp(senderTaxId))) {
-            refinementScheduler.scheduleDigitalRefinement(notification, recIndex, legalFactCreationDate, status);
-            registeredLetterSender.prepareSimpleRegisteredLetter(notification, recIndex);
-        } else {
-            boolean isNotificationAlreadyViewed = timelineUtils.checkIsNotificationViewed(notification.getIun(), recIndex);
-            if (!isNotificationAlreadyViewed) {
-                log.info("Paper message is not handled, registered Letter will not be sent to externalChannel - iun={} recipientIndex={}", notification.getIun(), recIndex);
-                addPaperNotificationNotHandledToTimeline(notification, recIndex);
-            } else {
-                log.info("Notification is already viewed, it will not go into the cancelled state - iun={} recipientIndex={}", notification.getIun(), recIndex);
-            }
-        }
+        addFailureWorkflowTimelineElement(recIndex, notification, logEvent, legalFactId);
     }
 
-    private void addFailureWorkflowTimelineElement(int recIndex, NotificationInt notification, PnAuditLogEvent logEvent, String legalFactId, Instant legalFactCreationDate) {
+    private void addFailureWorkflowTimelineElement(int recIndex, NotificationInt notification, PnAuditLogEvent logEvent, String legalFactId) {
         try {
-            timelineService.addTimelineElement(timelineUtils.buildFailureDigitalWorkflowTimelineElement(notification, recIndex, legalFactId, legalFactCreationDate), notification);
+            timelineService.addTimelineElement(timelineUtils.buildFailureDigitalWorkflowTimelineElement(notification, recIndex, legalFactId), notification);
             logEvent.generateSuccess().log();
         }catch (Exception ex){
             logEvent.generateFailure("Error in buildFailureDigitalWorkflowTimelineElement legalFactId={} - iun={} recIndex={}", legalFactId, notification.getIun(), recIndex, ex).log();
             throw ex;
         }
-    }
-
-    private void addPaperNotificationNotHandledToTimeline(NotificationInt notification, Integer recIndex) {
-        timelineService.addTimelineElement(
-                timelineUtils.buildNotHandledTimelineElement(
-                        notification,
-                        recIndex,
-                        NotHandledDetailsInt.PAPER_MESSAGE_NOT_HANDLED_CODE,
-                        NotHandledDetailsInt.PAPER_MESSAGE_NOT_HANDLED_REASON
-                ),
-                notification
-        );
     }
 }
