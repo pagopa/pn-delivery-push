c56f201a26a7a1d9cd9f9705d463d9cda9874d6d
Ivan Rosina <ivan.rosina@gmail.com>
2023-08-24 13:09:35 +0200

    wip PN-7540

--- src/main/java/it/pagopa/pn/deliverypush/dto/timeline/TimelineEventId.java
+++ src/main/java/it/pagopa/pn/deliverypush/dto/timeline/TimelineEventId.java
@@ -481,6 +481,30 @@ public enum TimelineEventId {
             return paymentCode;
         }
     }
+    ,
+
+    NOTIFICATION_CANCELLATION_REQUEST("NOTIFICATION_CANCELLATION_REQUEST") {
+        @Override
+        public String buildEventId(EventId eventId) {
+            return new TimelineEventIdBuilder()
+                .withCategory(this.getValue())
+                .withIun(eventId.getIun())
+                .build();
+        }
+
+    }
+    ,
+
+    NOTIFICATION_CANCELLED("NOTIFICATION_CANCELLED") {
+        @Override
+        public String buildEventId(EventId eventId) {
+            return new TimelineEventIdBuilder()
+                .withCategory(this.getValue())
+                .withIun(eventId.getIun())
+                .build();
+        }
+
+    }
     ;
 
     public String buildEventId(EventId eventId) {
