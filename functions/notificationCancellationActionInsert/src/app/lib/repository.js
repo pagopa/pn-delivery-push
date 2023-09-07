79aca49a2c984764ac58de63c6bddb885350d58c
Marco Iannaccone <marco.iannaccone@pagopa.it>
2023-09-07 09:36:06 +0200

    feature/PN-7548: preparation for persistence

--- functions/notificationCancellationActionInsert/src/app/lib/repository.js
+++ functions/notificationCancellationActionInsert/src/app/lib/repository.js
@@ -1,7 +1,7 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const {
   DynamoDBDocumentClient,
-  BatchExecuteStatementCommand,
+  TransactWriteItemsCommand,
 } = require("@aws-sdk/lib-dynamodb");
 
 const TABLES = {
@@ -9,6 +9,10 @@ const TABLES = {
   ACTION: "pn-Action",
   FUTUREACTION: "pn-FutureAction",
 };
+
+const client = new DynamoDBClient({
+  region: process.env.REGION,
+});
 
 exports.TABLES = TABLES;
 
@@ -21,7 +25,41 @@ exports.persistEvents = async (events) => {
   const dynamoDB = DynamoDBDocumentClient.from(client);
 
   for (let persistEvent of events) {
-    // ...
+    const params = {
+      TransactItems: [
+        {
+          Put: {
+            TableName: TABLES.ACTION,
+            Item: {
+              iun: { S: "exampleIun" },
+              id: { S: "exampleId1" },
+              user: { S: "exampleUser" },
+              // ...
+            },
+            // condition for put if absent
+            ConditionExpression:
+              "attribute_not_exists(iun) AND attribute_not_exists(id)",
+          },
+        },
+        {
+          Put: {
+            TableName: TABLES.FUTUREACTION,
+            Item: {
+              id: { S: "exampleId2" },
+              // ...
+            },
+          },
+        },
+      ],
+    };
+
+    try {
+      const result = await dynamoDB.send(new TransactWriteItemsCommand(params));
+      console.log("Action-FutureAction transaction succeeded:", result);
+    } catch (error) {
+      console.error("Error performing Action-FutureAction transaction:", error);
+      summary.errors.push(persistEvent);
+    }
   }
 
   return summary;
