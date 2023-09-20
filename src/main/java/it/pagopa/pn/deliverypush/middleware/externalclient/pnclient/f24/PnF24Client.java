f3778c10e5bdbc9e3e30c2ea7c2ff99f5df3e0fc
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2023-09-20 09:19:29 +0200

    PN-7398 Add todo for PnLogger in PnF24Client

--- src/main/java/it/pagopa/pn/deliverypush/middleware/externalclient/pnclient/f24/PnF24Client.java
+++ src/main/java/it/pagopa/pn/deliverypush/middleware/externalclient/pnclient/f24/PnF24Client.java
@@ -1,12 +1,14 @@
 package it.pagopa.pn.deliverypush.middleware.externalclient.pnclient.f24;
 
-import it.pagopa.pn.commons.log.PnLogger;
 import it.pagopa.pn.deliverypush.generated.openapi.msclient.f24.model.RequestAccepted;
 import it.pagopa.pn.deliverypush.generated.openapi.msclient.f24.model.ValidateF24Request;
 import reactor.core.publisher.Mono;
 
 public interface PnF24Client {
-    String CLIENT_NAME = PnLogger.EXTERNAL_SERVICES.PN_F24;
+
+    //TODO: DECOMMENTARE QUANDO SARA' DISPONIBILE COMMONS
+    //String CLIENT_NAME = PnLogger.EXTERNAL_SERVICES.PN_F24;
+    String CLIENT_NAME = "pn-f24";
     String VALIDATE_F24_PROCESS_NAME = "VALIDATE F24";
     String X_PAGOPA_CX_ID = "pn-delivery";
 
