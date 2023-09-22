e27e95b610565ad528d28ec7d3c010a52a002c2e
ivanrosinapa <127737322+ivanrosinapa@users.noreply.github.com>
2023-09-22 10:44:14 +0200

    7972 (#692)
    * 7972
    
    * generate-cod-laf
    
    * Delete scripts/generate-code-laf.sh
    
    * PN-7972:introdotto postMappingTransformer
    
    * rimosso commento su yaml
    
    * aggiunto unit test
    
    ---------
    
    Co-authored-by: fabriziolana <100559407+fabriziolana@users.noreply.github.com>

--- src/main/java/it/pagopa/pn/deliverypush/service/mapper/SmartMapper.java
+++ src/main/java/it/pagopa/pn/deliverypush/service/mapper/SmartMapper.java
@@ -1,14 +1,21 @@
 package it.pagopa.pn.deliverypush.service.mapper;
 
 import it.pagopa.pn.deliverypush.dto.timeline.details.NormalizedAddressDetailsInt;
+import it.pagopa.pn.deliverypush.dto.timeline.details.NotificationCancelledDetailsInt;
 import it.pagopa.pn.deliverypush.dto.timeline.details.PrepareAnalogDomicileFailureDetailsInt;
 import it.pagopa.pn.deliverypush.generated.openapi.server.v1.dto.TimelineElementDetailsV20;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.PropertyMap;
 import org.modelmapper.convention.MatchingStrategies;
 
 public class SmartMapper {
     private static ModelMapper modelMapper;
+
+
+    private static BiFunction postMappingTransformer;
 
     private SmartMapper (){}
 
@@ -34,15 +41,32 @@ public class SmartMapper {
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         modelMapper.addMappings(addressDetailPropertyMap);
         modelMapper.addMappings(prepareAnalogDomicileFailureDetailsInt);
+
+        List<BiFunction> postMappingTransformers = new ArrayList<>();
+        postMappingTransformers.add( (source, result)-> {
+            if (!(source instanceof NotificationCancelledDetailsInt) && result instanceof TimelineElementDetailsV20){
+                ((TimelineElementDetailsV20) result).setNotRefinedRecipientIndexes(null);
+            }
+            return result;
+        });
+
+        postMappingTransformer =  postMappingTransformers.stream()
+            .reduce((f, g) -> (i, s) -> f.apply(i, g.apply(i, s)))
+            .get();
     }
 
     public static  <S,T> T mapToClass(S source, Class<T> destinationClass ){
-        T result = null;
+        T result;
         if( source != null) {
             result = modelMapper.map(source, destinationClass );
+
+            result = (T) postMappingTransformer.apply(source, result);
+        } else {
+            result = null;
         }
         return result;
     }
+
 
 
 }
