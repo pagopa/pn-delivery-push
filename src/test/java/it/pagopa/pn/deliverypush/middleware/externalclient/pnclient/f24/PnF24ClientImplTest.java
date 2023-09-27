6bcd74846d5f55820f884ef9edf93b7502ed9798
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2023-09-27 12:35:16 +0200

    PN-7398 Fix PnF24ClientImplTest

--- src/test/java/it/pagopa/pn/deliverypush/middleware/externalclient/pnclient/f24/PnF24ClientImplTest.java
+++ src/test/java/it/pagopa/pn/deliverypush/middleware/externalclient/pnclient/f24/PnF24ClientImplTest.java
@@ -2,8 +2,6 @@ package it.pagopa.pn.deliverypush.middleware.externalclient.pnclient.f24;
 
 import it.pagopa.pn.deliverypush.generated.openapi.msclient.f24.api.F24ControllerApi;
 import it.pagopa.pn.deliverypush.generated.openapi.msclient.f24.model.RequestAccepted;
-import it.pagopa.pn.deliverypush.generated.openapi.msclient.f24.model.ValidateF24Request;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,6 +10,9 @@ import org.mockito.Mockito;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.mockito.ArgumentMatchers.any;
 
 @TestPropertySource(properties = {"pn.delivery-push.pagopa-cx-id=pn-delivery"})
 class PnF24ClientImplTest {
@@ -26,20 +27,15 @@ class PnF24ClientImplTest {
     @ExtendWith(SpringExtension.class)
     void validate(){
 
-        ValidateF24Request validateF24Request = new ValidateF24Request();
-        validateF24Request.setSetId("setId");
-
         RequestAccepted response = new RequestAccepted();
         response.setDescription("description");
         response.setStatus("status");
 
-        Mockito.when(f24ControllerApi.validateMetadata("pn-delivery", validateF24Request.getSetId(), validateF24Request)).thenReturn(Mono.just(response));
+        Mockito.when(f24ControllerApi.validateMetadata(any(), any(), any())).thenReturn(Mono.just(response));
 
-        Mono<RequestAccepted> responseMono = client.validate("setId");
-
-        Assertions.assertNotNull(responseMono);
-        RequestAccepted responseExpected = responseMono.block();
-        Assertions.assertEquals(response, responseExpected);
+        StepVerifier.create(client.validate("setId"))
+                .expectNext(response)
+                .verifyComplete();
     }
 
 }
