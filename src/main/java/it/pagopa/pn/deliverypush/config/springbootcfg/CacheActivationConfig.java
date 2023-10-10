6c729e931ebf8ebaddeb6cd06f7737d2606c3053
EmanueleBonofiglio <emanuele.bonofiglio@grupposcai.it>
2023-10-10 23:07:13 +0200

    PN-8052 new cache config

--- src/main/java/it/pagopa/pn/deliverypush/config/springbootcfg/CacheActivationConfig.java
+++ src/main/java/it/pagopa/pn/deliverypush/config/springbootcfg/CacheActivationConfig.java
@@ -1,11 +1,10 @@
 package it.pagopa.pn.deliverypush.config.springbootcfg;
 
-import it.pagopa.pn.commons.configs.cache.CacheConfigs;
+
 import it.pagopa.pn.commons.configs.cache.PnCacheConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+
 
 @Configuration
-@Import({PnCacheConfiguration.class})
-public class CacheActivationConfig extends CacheConfigs {
+public class CacheActivationConfig extends PnCacheConfiguration {
 }
