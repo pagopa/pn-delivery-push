1d8644668e46bed25431d92af1f93bdba306f2bc
Marco Iannaccone <marco.iannaccone@pagopa.it>
2023-10-11 12:53:27 +0200

    Feature/PN-8151 (#710)
    * PN-8151: first commit
    
    * PN-8151: mapping preparation + utils tests
    
    * PN-8151: index test
    
    * PN-8151: eventHandler tests
    
    * PN-8151: mapping from timeline to SQS
    
    * PN-8151: basic mapping tests
    
    * PN-8151: complete mapping tests
    
    * PN-8151: almost complete infrastructure definition for lambda
    
    * PN-8151: complete infrastructure definition for lambda + better logging

---
+++ functions/paperEventsCostUpdate/src/app/lib/eventMapper.js
@@ -0,0 +1,125 @@
+const { parseKinesisObjToJsonObj } = require("./utils");
+const crypto = require("crypto");
+
+const TABLES = {
+  TIMELINES: "pn-Timelines",
+};
+
+const allowedTimelineCategories = [
+  "SEND_SIMPLE_REGISTERED_LETTER",
+  "SEND_ANALOG_DOMICILE",
+];
+
+function updateCostPhaseForSendAnalogDomicile(timelineObj) {
+  // timelineObj.timelineElementId must exist and be a string
+  if (
+    !timelineObj.timelineElementId ||
+    typeof timelineObj.timelineElementId !== "string"
+  ) {
+    return null;
+  }
+
+  if (timelineObj.timelineElementId.indexOf("ATTEMPT_0") >= 0) {
+    return "SEND_ANALOG_DOMICILE_ATTEMPT_0";
+  } else if (timelineObj.timelineElementId.indexOf("ATTEMPT_1") >= 0) {
+    return "SEND_ANALOG_DOMICILE_ATTEMPT_1";
+  } else {
+    return null;
+  }
+}
+
+exports.mapEvents = async (events) => {
+  const filteredEvents = events.filter((e) => {
+    // INSERT events from TIMELINES table, with the allowed categories
+    return (
+      e.eventName == "INSERT" &&
+      e.tableName == TABLES.TIMELINES &&
+      e.dynamodb.NewImage.category &&
+      allowedTimelineCategories.indexOf(e.dynamodb.NewImage.category.S) >= 0
+    );
+  });
+
+  let result = [];
+
+  for (const filteredEvent of filteredEvents) {
+    const date = new Date();
+
+    const timelineObj = parseKinesisObjToJsonObj(
+      filteredEvent.dynamodb.NewImage
+    );
+
+    const resultElementBody = {
+      // common to all events
+      iun: timelineObj.iun,
+      // common to all handled paper events
+      recIndex: timelineObj.details?.recIndex ?? undefined,
+      notificationStepCost: timelineObj.details?.analogCost ?? undefined,
+      eventTimestamp: timelineObj.timestamp,
+      eventStorageTimestamp: timelineObj.timestamp,
+    };
+
+    let messageAttributes = {
+      publisher: {
+        DataType: "String",
+        StringValue: "deliveryPush",
+      },
+      iun: {
+        DataType: "String",
+        StringValue: resultElementBody.iun,
+      },
+      eventId: {
+        DataType: "String",
+        StringValue: crypto.randomUUID(),
+      },
+      createdAt: {
+        DataType: "String",
+        StringValue: date.toISOString(),
+      },
+      eventType: {
+        DataType: "String",
+        StringValue: "UPDATE_COST_PHASE_EVENT",
+      },
+    };
+
+    const category = timelineObj.category;
+
+    switch (category) {
+      case "SEND_SIMPLE_REGISTERED_LETTER":
+        resultElementBody.updateCostPhase = "SEND_SIMPLE_REGISTERED_LETTER";
+        break;
+
+      case "SEND_ANALOG_DOMICILE":
+        const costPhase = updateCostPhaseForSendAnalogDomicile(timelineObj);
+        if (costPhase) {
+          resultElementBody.updateCostPhase = costPhase;
+        }
+        break;
+
+      //default:
+      // we previously filtered out the events with not allowed categories
+      //  break;
+    }
+
+    let resultElement = {
+      Id: filteredEvent.kinesisSeqNumber,
+      MessageBody: JSON.stringify(resultElementBody),
+      MessageAttributes: messageAttributes,
+    };
+
+    // we want to send the element to the queue only if fields are valid,
+    // without sending them back to the stream, or we'd end up in an loop
+    if (
+      resultElementBody.updateCostPhase &&
+      resultElementBody.recIndex &&
+      resultElementBody.notificationStepCost
+    ) {
+      result.push(resultElement);
+    } else {
+      console.error(
+        "Error in parsing timelineObj (analogCost or recIndex missing or incomplete timelineElementId): ",
+        timelineObj
+      );
+    }
+  }
+  return result;
+};
---
+++ functions/paperEventsCostUpdate/src/test/eventMapper.test.js
@@ -0,0 +1,212 @@
+const { expect } = require("chai");
+const fs = require("fs");
+
+const { mapEvents } = require("../app/lib/eventMapper");
+
+describe("event mapper tests", function () {
+  const iun = "VWKQ-WQNT-VJZG-202308-K-1";
+  const eventType = "UPDATE_COST_PHASE_EVENT";
+
+  it("test SEND_ANALOG_DOMICILE ATTEMPT_0 mapping", async () => {
+    const eventJSON = fs.readFileSync(
+      "./src/test/events/eventMapper.send_analog_domicile.json"
+    );
+    let event = JSON.parse(eventJSON);
+
+    const events = [event];
+
+    const res = await mapEvents(events);
+
+    expect(res).length(1);
+
+    let body = JSON.parse(res[0].MessageBody);
+    expect(body.iun).equal(iun);
+    expect(body.recIndex).equal("0");
+    expect(body.notificationStepCost).equal("926");
+    expect(body.eventTimestamp).equal("2023-08-09T01:23:56.803455499Z");
+    expect(body.eventStorageTimestamp).equal("2023-08-09T01:23:56.803455499Z");
+    expect(body.updateCostPhase).equal("SEND_ANALOG_DOMICILE_ATTEMPT_0");
+
+    expect(res[0].MessageAttributes.publisher.StringValue).equal(
+      "deliveryPush"
+    );
+    expect(res[0].MessageAttributes.iun.StringValue).equal(iun);
+    expect(res[0].MessageAttributes.eventType.StringValue).equal(eventType);
+  });
+
+  it("test SEND_ANALOG_DOMICILE ATTEMPT_1 and different recIndex mapping", async () => {
+    const eventJSON = fs.readFileSync(
+      "./src/test/events/eventMapper.send_analog_domicile.json"
+    );
+    let event = JSON.parse(eventJSON);
+
+    // change ATTEMP to 1
+    event.dynamodb.NewImage.timelineElementId.S =
+      event.dynamodb.NewImage.timelineElementId.S.replace(
+        "ATTEMPT_0",
+        "ATTEMPT_1"
+      );
+
+    // change recIndex to 1
+    event.dynamodb.NewImage.details.M.recIndex.N = "1";
+
+    const events = [event];
+
+    const res = await mapEvents(events);
+
+    expect(res).length(1);
+
+    let body = JSON.parse(res[0].MessageBody);
+    expect(body.iun).equal(iun);
+    expect(body.recIndex).equal("1");
+    expect(body.notificationStepCost).equal("926");
+    expect(body.eventTimestamp).equal("2023-08-09T01:23:56.803455499Z");
+    expect(body.eventStorageTimestamp).equal("2023-08-09T01:23:56.803455499Z");
+    expect(body.updateCostPhase).equal("SEND_ANALOG_DOMICILE_ATTEMPT_1");
+
+    expect(res[0].MessageAttributes.publisher.StringValue).equal(
+      "deliveryPush"
+    );
+    expect(res[0].MessageAttributes.iun.StringValue).equal(iun);
+    expect(res[0].MessageAttributes.eventType.StringValue).equal(eventType);
+  });
+
+  it("test SEND_SIMPLE_REGISTERED_LETTER mapping", async () => {
+    const eventJSON = fs.readFileSync(
+      "./src/test/events/eventMapper.send_analog_domicile.json"
+    );
+    let event = JSON.parse(eventJSON);
+
+    // change category to SEND_SIMPLE_REGISTERED_LETTER
+    event.dynamodb.NewImage.category.S = "SEND_SIMPLE_REGISTERED_LETTER";
+
+    // change the timelineElementId
+    event.dynamodb.NewImage.timelineElementId.S =
+      "SEND_SIMPLE_REGISTERED_LETTER.IUN_" + iun + ".RECINDEX_0";
+
+    const events = [event];
+
+    const res = await mapEvents(events);
+
+    expect(res).length(1);
+
+    let body = JSON.parse(res[0].MessageBody);
+    expect(body.iun).equal(iun);
+    expect(body.recIndex).equal("0");
+    expect(body.notificationStepCost).equal("926");
+    expect(body.eventTimestamp).equal("2023-08-09T01:23:56.803455499Z");
+    expect(body.eventStorageTimestamp).equal("2023-08-09T01:23:56.803455499Z");
+    expect(body.updateCostPhase).equal("SEND_SIMPLE_REGISTERED_LETTER");
+
+    expect(res[0].MessageAttributes.publisher.StringValue).equal(
+      "deliveryPush"
+    );
+    expect(res[0].MessageAttributes.iun.StringValue).equal(iun);
+    expect(res[0].MessageAttributes.eventType.StringValue).equal(eventType);
+  });
+
+  it("test unmapped event", async () => {
+    const eventJSON = fs.readFileSync(
+      "./src/test/events/eventMapper.send_analog_domicile.json"
+    );
+    let event = JSON.parse(eventJSON);
+
+    // specify an unsupported category
+    event.dynamodb.NewImage.category.S = "UNSUPPORTED_CATEGORY";
+
+    const events = [event];
+
+    const res = await mapEvents(events);
+
+    expect(res).length(0);
+  });
+
+  it("test missing fields event", async () => {
+    const eventJSON = fs.readFileSync(
+      "./src/test/events/eventMapper.send_analog_domicile.json"
+    );
+    let event = JSON.parse(eventJSON);
+
+    // remove one of the required fields
+    let saved = event.dynamodb.NewImage.details.M.recIndex;
+    delete event.dynamodb.NewImage.details.M.recIndex;
+
+    let events = [event];
+
+    let res = await mapEvents(events);
+
+    expect(res).length(0);
+
+    // restore the field and remove a different one
+    event.dynamodb.NewImage.details.M.recIndex = saved;
+    saved = event.dynamodb.NewImage.details.M.analogCost;
+    delete event.dynamodb.NewImage.details.M.analogCost;
+
+    events = [event];
+
+    res = await mapEvents(events);
+
+    expect(res).length(0);
+
+    // restore the field and test ok
+    event.dynamodb.NewImage.details.M.analogCost = saved;
+
+    events = [event];
+
+    res = await mapEvents(events);
+
+    expect(res).length(1);
+  });
+
+  it("test wrong type or missing timelineElementId", async () => {
+    const eventJSON = fs.readFileSync(
+      "./src/test/events/eventMapper.send_analog_domicile.json"
+    );
+    let event = JSON.parse(eventJSON);
+
+    // change timelineElementId to a wrong type after saving the original value
+    let saved = event.dynamodb.NewImage.timelineElementId.S;
+    event.dynamodb.NewImage.timelineElementId.S = 1234;
+
+    let events = [event];
+
+    let res = await mapEvents(events);
+
+    expect(res).length(0);
+
+    // restore and retry
+    event.dynamodb.NewImage.timelineElementId.S = saved;
+
+    events = [event];
+
+    res = await mapEvents(events);
+
+    expect(res).length(1);
+
+    // remove timelineElementId
+    delete event.dynamodb.NewImage.timelineElementId;
+
+    events = [event];
+
+    res = await mapEvents(events);
+
+    expect(res).length(0);
+  });
+
+  it("test SEND_ANALOG_DOMICILE missing ATTEMPT", async () => {
+    const eventJSON = fs.readFileSync(
+      "./src/test/events/eventMapper.send_analog_domicile.json"
+    );
+    let event = JSON.parse(eventJSON);
+
+    // remove ATTEMPT_0 from timelineElementId
+    event.dynamodb.NewImage.timelineElementId.S =
+      event.dynamodb.NewImage.timelineElementId.S.replace("ATTEMPT_0", "");
+
+    const events = [event];
+
+    const res = await mapEvents(events);
+
+    expect(res).length(0);
+  });
+});
