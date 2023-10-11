5e02f6fe0b0ac2b456e60b515006df3f1c70080c
Simone Rondinella <simone.rondinella@pagopa.it>
2023-10-11 15:57:41 +0200

    Modificato name DTO timeline

--- src/test/java/it/pagopa/pn/deliverypush/generated/openapi/server/v1/dto/RequestRefusedDetailsTest.java
+++ src/test/java/it/pagopa/pn/deliverypush/generated/openapi/server/v1/dto/RequestRefusedDetailsTest.java
@@ -13,15 +13,15 @@ class RequestRefusedDetailsTest {
     @BeforeEach
     void setUp() {
         details = new RequestRefusedDetails();
-        details.setRefusalReasons(List.of(NotificationRefusedError.builder().errorCode("FILE_NOTFOUND").detail("Allegati non trovati").build()));
+        details.setRefusalReasons(List.of(NotificationRefusedErrorV20.builder().errorCode("FILE_NOTFOUND").detail("Allegati non trovati").build()));
     }
 
     @Test
     void errors() {
         RequestRefusedDetails tmp = RequestRefusedDetails.builder()
-                .refusalReasons(List.of(NotificationRefusedError.builder().errorCode("FILE_NOTFOUND").detail("Allegati non trovati").build()))
+                .refusalReasons(List.of(NotificationRefusedErrorV20.builder().errorCode("FILE_NOTFOUND").detail("Allegati non trovati").build()))
                 .build();
-        Assertions.assertEquals(tmp, details.refusalReasons(List.of(NotificationRefusedError.builder().errorCode("FILE_NOTFOUND").detail("Allegati non trovati").build())
+        Assertions.assertEquals(tmp, details.refusalReasons(List.of(NotificationRefusedErrorV20.builder().errorCode("FILE_NOTFOUND").detail("Allegati non trovati").build())
         ));
     }
 
@@ -29,20 +29,20 @@ class RequestRefusedDetailsTest {
     void addErrorsItem() {
         RequestRefusedDetails tmp = new RequestRefusedDetails();
         tmp.addRefusalReasonsItem(
-                NotificationRefusedError.builder().errorCode("FILE_NOTFOUND").detail("Allegati non trovati").build()
+                NotificationRefusedErrorV20.builder().errorCode("FILE_NOTFOUND").detail("Allegati non trovati").build()
         );
         Assertions.assertEquals(details, tmp);
     }
 
     @Test
     void getErrors() {
-        Assertions.assertEquals(List.of(NotificationRefusedError.builder().errorCode("FILE_NOTFOUND").detail("Allegati non trovati").build()), details.getRefusalReasons());
+        Assertions.assertEquals(List.of(NotificationRefusedErrorV20.builder().errorCode("FILE_NOTFOUND").detail("Allegati non trovati").build()), details.getRefusalReasons());
     }
 
     @Test
     void testEquals() {
         RequestRefusedDetails tmp = RequestRefusedDetails.builder()
-                .refusalReasons(List.of(NotificationRefusedError.builder().errorCode("FILE_NOTFOUND").detail("Allegati non trovati").build()))
+                .refusalReasons(List.of(NotificationRefusedErrorV20.builder().errorCode("FILE_NOTFOUND").detail("Allegati non trovati").build()))
                 .build();
         Assertions.assertEquals(Boolean.TRUE, tmp.equals(details));
     }
