e4fcd8ea5246b398dfd5e604d9f0161835b90f66
Simone Rondinella <simone.rondinella@pagopa.it>
2023-10-16 12:55:28 +0200

    PN-8149 Modifiche per annullamento

--- src/main/java/it/pagopa/pn/deliverypush/action/utils/PaymentUtils.java
+++ src/main/java/it/pagopa/pn/deliverypush/action/utils/PaymentUtils.java
@@ -1,8 +1,10 @@
 package it.pagopa.pn.deliverypush.action.utils;
 
 import it.pagopa.pn.deliverypush.dto.cost.PaymentsInfoForRecipientInt;
+import it.pagopa.pn.deliverypush.dto.cost.UpdateNotificationCostResponseInt;
 import it.pagopa.pn.deliverypush.dto.ext.delivery.notification.NotificationInt;
 import it.pagopa.pn.deliverypush.dto.ext.delivery.notification.PagoPaInt;
+import it.pagopa.pn.deliverypush.exceptions.PnPaymentUpdateRetryException;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 
@@ -37,5 +39,39 @@ public class PaymentUtils {
         });
         return paymentsInfoForRecipients;
     }
+
+    public static void handleResponse(NotificationInt notification, UpdateNotificationCostResponseInt updateNotificationCostResponse) {
+        log.debug("Start handle update cost response {}", updateNotificationCostResponse.getIun());
+
+        updateNotificationCostResponse.getUpdateResults().forEach(response -> {
+            PaymentsInfoForRecipientInt paymentsInfo = response.getPaymentsInfoForRecipient();
+
+            log.debug("Start handle update response for iun={} recIndex={} creditorTaxId={} noticeCode={}",
+                    notification.getIun(), paymentsInfo.getRecIndex(), paymentsInfo.getCreditorTaxId(), paymentsInfo.getNoticeCode());
+
+            switch (response.getResult()) {
+                case OK -> log.debug("Update cost OK for iun={} recIndex={} creditorTaxId={} noticeCode={}",
+                        notification.getIun(), paymentsInfo.getRecIndex(), paymentsInfo.getCreditorTaxId(), paymentsInfo.getNoticeCode());
+                case KO -> log.error("Payment information is not valid. Can't update notification cost" +
+                        " - creditorTaxId={} noticeCode={}", paymentsInfo.getCreditorTaxId(), paymentsInfo.getNoticeCode());
+                case RETRY -> {
+                    final String errorDetail = String.format("Update notification fee error, can't have response from service. iun=%s recIndex=%s creditorTaxId=%s noticeCode=%s",
+                            notification.getIun(), paymentsInfo.getRecIndex(), paymentsInfo.getCreditorTaxId(), paymentsInfo.getNoticeCode());
+                    handleRetryError(errorDetail);
+                }
+                default -> {
+                    final String errorDetail = String.format("Update notification fee error. Response received is not handled for iun=%s recIndex=%s creditorTaxId=%s noticeCode=%s",
+                            notification.getIun(), paymentsInfo.getRecIndex(), paymentsInfo.getCreditorTaxId(), paymentsInfo.getNoticeCode());
+                    handleRetryError(errorDetail);
+                }
+            }
+        });
+    }
+
+    private static void handleRetryError(String errorDetail) {
+        log.info(errorDetail);
+        throw new PnPaymentUpdateRetryException(errorDetail);
+    }
+
 
 }
--- src/test/java/it/pagopa/pn/deliverypush/service/impl/NotificationCancellationServiceImplTest.java
+++ src/test/java/it/pagopa/pn/deliverypush/service/impl/NotificationCancellationServiceImplTest.java
@@ -7,24 +7,26 @@ import it.pagopa.pn.deliverypush.action.it.utils.NotificationTestBuilder;
 import it.pagopa.pn.deliverypush.action.utils.TimelineUtils;
 import it.pagopa.pn.deliverypush.dto.address.PhysicalAddressInt;
 import it.pagopa.pn.deliverypush.dto.cancellation.StatusDetailInt;
-import it.pagopa.pn.deliverypush.dto.ext.delivery.notification.NotificationInt;
-import it.pagopa.pn.deliverypush.dto.ext.delivery.notification.NotificationRecipientInt;
-import it.pagopa.pn.deliverypush.dto.ext.delivery.notification.NotificationSenderInt;
+import it.pagopa.pn.deliverypush.dto.cost.PaymentsInfoForRecipientInt;
+import it.pagopa.pn.deliverypush.dto.cost.UpdateCostPhaseInt;
+import it.pagopa.pn.deliverypush.dto.cost.UpdateNotificationCostResponseInt;
+import it.pagopa.pn.deliverypush.dto.cost.UpdateNotificationCostResultInt;
+import it.pagopa.pn.deliverypush.dto.ext.delivery.notification.*;
 import it.pagopa.pn.deliverypush.dto.ext.delivery.notification.status.NotificationStatusInt;
 import it.pagopa.pn.deliverypush.dto.timeline.TimelineElementInternal;
 import it.pagopa.pn.deliverypush.dto.timeline.details.NotificationCancelledDetailsInt;
 import it.pagopa.pn.deliverypush.exceptions.PnNotFoundException;
+import it.pagopa.pn.deliverypush.exceptions.PnPaymentUpdateRetryException;
 import it.pagopa.pn.deliverypush.generated.openapi.server.v1.dto.CxTypeAuthFleet;
-import it.pagopa.pn.deliverypush.service.AuditLogService;
-import it.pagopa.pn.deliverypush.service.NotificationCancellationService;
-import it.pagopa.pn.deliverypush.service.NotificationService;
-import it.pagopa.pn.deliverypush.service.TimelineService;
+import it.pagopa.pn.deliverypush.generated.openapi.server.v1.dto.NotificationFeePolicy;
+import it.pagopa.pn.deliverypush.service.*;
 import it.pagopa.pn.deliverypush.utils.AuthUtils;
 import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -33,8 +35,10 @@ import reactor.core.publisher.Mono;
 
 import java.time.Instant;
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
+import static it.pagopa.pn.deliverypush.action.it.utils.TestUtils.verifyPaymentInfo;
 import static it.pagopa.pn.deliverypush.exceptions.PnDeliveryPushExceptionCodes.ERROR_CODE_DELIVERYPUSH_NOTFOUND;
 import static it.pagopa.pn.deliverypush.service.impl.NotificationCancellationServiceImpl.NOTIFICATION_ALREADY_CANCELLED;
 import static it.pagopa.pn.deliverypush.service.impl.NotificationCancellationServiceImpl.NOTIFICATION_CANCELLATION_ACCEPTED;
@@ -51,12 +55,14 @@ class NotificationCancellationServiceImplTest {
     private TimelineUtils timelineUtils;
     @Mock
     private AuditLogService auditLogService;
-    
+    @Mock
+    private NotificationProcessCostService notificationProcessCostService;
+
     private NotificationCancellationService notificationCancellationService;
     
     @BeforeEach
     public void init(){
-        notificationCancellationService = new NotificationCancellationServiceImpl(notificationService,authUtils, timelineService, timelineUtils, auditLogService);
+        notificationCancellationService = new NotificationCancellationServiceImpl(notificationService,authUtils, timelineService, timelineUtils, auditLogService, notificationProcessCostService);
     }
     
     @Test
@@ -185,14 +191,195 @@ class NotificationCancellationServiceImplTest {
 
     @Test
     @ExtendWith(SpringExtension.class)
-    void cancelNotification() {
+    void cancelNotificationForPagoPaAsync() {
         //Given
-        NotificationRecipientInt recipient = NotificationRecipientTestBuilder.builder().build();
+        final String creditorTaxId = "cred";
+        final String noticeCode = "notice";
+        NotificationRecipientInt recipient = NotificationRecipientTestBuilder.builder()
+                .withPayments(Collections.singletonList(
+                        NotificationPaymentInfoInt.builder()
+                                .pagoPA(PagoPaInt.builder()
+                                        .creditorTaxId(creditorTaxId)
+                                        .noticeCode(noticeCode)
+                                        .applyCost(true)
+                                        .build()
+                                )
+                                .build()
+                ))
+                .build();
         NotificationInt notification = NotificationTestBuilder.builder()
                 .withIun("iun")
                 .withNotificationRecipient(recipient)
+                .withNotificationFeePolicy(NotificationFeePolicy.DELIVERY_MODE)
+                .withPagoPaIntMode(PagoPaIntMode.ASYNC)
+                .withPaFee(100)
+                .build();
+        
+        final TimelineElementInternal timelineElement = TimelineElementInternal.builder()
+                .details(NotificationCancelledDetailsInt.builder()
+                        .build())
+                .timestamp(Instant.now())
+                .build();
+        Mockito.when(timelineUtils.buildCancelledTimelineElement(notification)).thenReturn(timelineElement);
+        Mockito.when(timelineService.addTimelineElement(Mockito.any(), Mockito.any())).thenReturn(false);
+        Mockito.when(notificationService.removeAllNotificationCostsByIun(notification.getIun())).thenReturn(Mono.empty());
+        Mockito.when(notificationService.getNotificationByIun(notification.getIun())).thenReturn(notification);
+        Mockito.when(notificationService.updateStatus(notification.getIun(), NotificationStatusInt.CANCELLED, timelineElement.getTimestamp())).thenReturn(Mono.empty());
+        PnAuditLogEvent auditLogEvent = Mockito.mock(PnAuditLogEvent.class);
+        Mockito.when(auditLogService.buildAuditLogEvent(eq(notification.getIun()), eq(PnAuditLogEventType.AUD_NT_CANCELLED), anyString(), eq(2)))
+                .thenReturn(auditLogEvent);
+        Mockito.when(auditLogEvent.generateSuccess()).thenReturn(auditLogEvent);
+
+        final int recIndex = 0;
+        UpdateNotificationCostResponseInt response = UpdateNotificationCostResponseInt.builder()
+                .iun(notification.getIun())
+                .updateResults(Collections.singletonList(UpdateNotificationCostResultInt.builder()
+                        .result(UpdateNotificationCostResultInt.ResultEnum.KO)
+                        .paymentsInfoForRecipient(PaymentsInfoForRecipientInt.builder()
+                                .creditorTaxId(creditorTaxId)
+                                .noticeCode(noticeCode)
+                                .recIndex(recIndex)
+                                .build())
+                        .build()
+                )).build();
+
+        Mockito.when(notificationProcessCostService.setNotificationStepCost(
+                Mockito.anyInt(),
+                Mockito.anyString(),
+                Mockito.any(),
+                Mockito.any(Instant.class),
+                Mockito.any(Instant.class),
+                Mockito.any(UpdateCostPhaseInt.class)
+        )).thenReturn(Mono.just(response));
+        
+        //WHEN
+        notificationCancellationService.completeCancellationProcess(notification.getIun());
+
+        //THEN
+        Mockito.verify(timelineService).addTimelineElement(timelineElement, notification);
+        Mockito.verify(notificationService).removeAllNotificationCostsByIun(notification.getIun());
+        Mockito.verify(auditLogEvent).generateSuccess();
+
+        ArgumentCaptor<List<PaymentsInfoForRecipientInt>> paymentForRecipientListCaptor = ArgumentCaptor.forClass(List.class);
+
+        Mockito.verify(notificationProcessCostService).setNotificationStepCost(
+                Mockito.eq(NotificationCancellationServiceImpl.NOTIFICATION_CANCELLED_COST),
+                Mockito.eq(notification.getIun()),
+                paymentForRecipientListCaptor.capture(),
+                Mockito.any(Instant.class),
+                Mockito.any(Instant.class),
+                Mockito.eq(UpdateCostPhaseInt.NOTIFICATION_CANCELLED)
+        );
+
+        List<PaymentsInfoForRecipientInt> paymentsInfoForRecipientsCaptured = paymentForRecipientListCaptor.getValue();
+        verifyPaymentInfo(notification, recIndex, paymentsInfoForRecipientsCaptured);
+    }
+
+    @Test
+    @ExtendWith(SpringExtension.class)
+    void cancelNotificationForPagoPaAsyncError() {
+        //Given
+        final String creditorTaxId = "cred";
+        final String noticeCode = "notice";
+        NotificationRecipientInt recipient = NotificationRecipientTestBuilder.builder()
+                .withPayments(Collections.singletonList(
+                        NotificationPaymentInfoInt.builder()
+                                .pagoPA(PagoPaInt.builder()
+                                        .creditorTaxId(creditorTaxId)
+                                        .noticeCode(noticeCode)
+                                        .applyCost(true)
+                                        .build()
+                                )
+                                .build()
+                ))
+                .build();
+        NotificationInt notification = NotificationTestBuilder.builder()
+                .withIun("iun")
+                .withNotificationRecipient(recipient)
+                .withNotificationFeePolicy(NotificationFeePolicy.DELIVERY_MODE)
+                .withPagoPaIntMode(PagoPaIntMode.ASYNC)
+                .withPaFee(100)
                 .build();
 
+        final String iun = notification.getIun();
+        Mockito.when(notificationService.removeAllNotificationCostsByIun(iun)).thenReturn(Mono.empty());
+        Mockito.when(notificationService.getNotificationByIun(iun)).thenReturn(notification);
+        PnAuditLogEvent auditLogEvent = Mockito.mock(PnAuditLogEvent.class);
+        Mockito.when(auditLogService.buildAuditLogEvent(eq(iun), eq(PnAuditLogEventType.AUD_NT_CANCELLED), anyString(), eq(2)))
+                .thenReturn(auditLogEvent);
+        Mockito.when(auditLogEvent.generateFailure(Mockito.anyString(), Mockito.any(), Mockito.any())).thenReturn(auditLogEvent);
+
+        final int recIndex = 0;
+        UpdateNotificationCostResponseInt response = UpdateNotificationCostResponseInt.builder()
+                .iun(iun)
+                .updateResults(Collections.singletonList(UpdateNotificationCostResultInt.builder()
+                        .result(UpdateNotificationCostResultInt.ResultEnum.RETRY)
+                        .paymentsInfoForRecipient(PaymentsInfoForRecipientInt.builder()
+                                .creditorTaxId(creditorTaxId)
+                                .noticeCode(noticeCode)
+                                .recIndex(recIndex)
+                                .build())
+                        .build()
+                )).build();
+
+        Mockito.when(notificationProcessCostService.setNotificationStepCost(
+                Mockito.anyInt(),
+                Mockito.anyString(),
+                Mockito.any(),
+                Mockito.any(Instant.class),
+                Mockito.any(Instant.class),
+                Mockito.any(UpdateCostPhaseInt.class)
+        )).thenReturn(Mono.just(response));
+
+        //WHEN
+        Assertions.assertThrows(PnPaymentUpdateRetryException.class,
+                () -> notificationCancellationService.completeCancellationProcess(iun));
+        
+        //THEN
+        Mockito.verify(notificationService).removeAllNotificationCostsByIun(iun);
+        Mockito.verify(timelineService, Mockito.never()).addTimelineElement(Mockito.any(), Mockito.eq(notification));
+        Mockito.verify(auditLogEvent).generateFailure(Mockito.anyString(), Mockito.any(), Mockito.any());
+
+        ArgumentCaptor<List<PaymentsInfoForRecipientInt>> paymentForRecipientListCaptor = ArgumentCaptor.forClass(List.class);
+
+        Mockito.verify(notificationProcessCostService).setNotificationStepCost(
+                Mockito.eq(NotificationCancellationServiceImpl.NOTIFICATION_CANCELLED_COST),
+                Mockito.eq(iun),
+                paymentForRecipientListCaptor.capture(),
+                Mockito.any(Instant.class),
+                Mockito.any(Instant.class),
+                Mockito.eq(UpdateCostPhaseInt.NOTIFICATION_CANCELLED)
+        );
+
+        List<PaymentsInfoForRecipientInt> paymentsInfoForRecipientsCaptured = paymentForRecipientListCaptor.getValue();
+        verifyPaymentInfo(notification, recIndex, paymentsInfoForRecipientsCaptured);
+    }
+    
+    @Test
+    @ExtendWith(SpringExtension.class)
+    void cancelNotificationNotPagoPaAsync() {
+        //Given
+        final String creditorTaxId = "cred";
+        final String noticeCode = "notice";
+        NotificationRecipientInt recipient = NotificationRecipientTestBuilder.builder()
+                .withPayments(Collections.singletonList(
+                        NotificationPaymentInfoInt.builder()
+                                .pagoPA(PagoPaInt.builder()
+                                        .creditorTaxId(creditorTaxId)
+                                        .noticeCode(noticeCode)
+                                        .applyCost(true)
+                                        .build()
+                                )
+                                .build()
+                ))
+                .build();
+        NotificationInt notification = NotificationTestBuilder.builder()
+                .withIun("iun")
+                .withNotificationRecipient(recipient)
+                .withNotificationFeePolicy(NotificationFeePolicy.DELIVERY_MODE)
+                .withPagoPaIntMode(PagoPaIntMode.NONE)
+                .withPaFee(100)
+                .build();
 
         final TimelineElementInternal timelineElement = TimelineElementInternal.builder()
                 .details(NotificationCancelledDetailsInt.builder()
@@ -216,6 +403,15 @@ class NotificationCancellationServiceImplTest {
         Mockito.verify(timelineService).addTimelineElement(timelineElement, notification);
         Mockito.verify(notificationService).removeAllNotificationCostsByIun(notification.getIun());
         Mockito.verify(auditLogEvent).generateSuccess();
+
+        Mockito.verify(notificationProcessCostService, Mockito.never()).setNotificationStepCost(
+                Mockito.eq(NotificationCancellationServiceImpl.NOTIFICATION_CANCELLED_COST),
+                Mockito.eq(notification.getIun()), 
+                Mockito.any(),
+                Mockito.any(Instant.class),
+                Mockito.any(Instant.class),
+                Mockito.eq(UpdateCostPhaseInt.NOTIFICATION_CANCELLED)
+        );
     }
 
 
@@ -258,8 +454,8 @@ class NotificationCancellationServiceImplTest {
         Mockito.verify(notificationService).removeAllNotificationCostsByIun(notification.getIun());
         Mockito.verify(auditLogEvent).generateSuccess();
     }
-
-
+    
+    
     @Test
     @ExtendWith(SpringExtension.class)
     void cancelNotificationException() {
