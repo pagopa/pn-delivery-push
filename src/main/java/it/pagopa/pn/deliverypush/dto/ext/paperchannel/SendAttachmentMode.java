714c9ce150a2a2a0e61a722a422e94458402961b
fabriziolana <100559407+fabriziolana@users.noreply.github.com>
2023-10-16 15:04:53 +0200

    fix enum attachmentmode (#724)

--- src/main/java/it/pagopa/pn/deliverypush/dto/ext/paperchannel/SendAttachmentMode.java
+++ src/main/java/it/pagopa/pn/deliverypush/dto/ext/paperchannel/SendAttachmentMode.java
@@ -3,18 +3,34 @@ package it.pagopa.pn.deliverypush.dto.ext.paperchannel;
 import org.springframework.util.StringUtils;
 
 public enum SendAttachmentMode {
-    AAR,
-    AAR_DOCUMENTS,
-    AAR_DOCUMENTS_PAYMENTS;
+    AAR("AAR"),
+    AAR_DOCUMENTS("AAR-DOCUMENTS"),
+    AAR_DOCUMENTS_PAYMENTS("AAR-DOCUMENTS-PAYMENTS");
 
     public static SendAttachmentMode fromValue(String sendAnalogNotificationAttachments) {
         if(StringUtils.hasText(sendAnalogNotificationAttachments)) {
             try {
-                return SendAttachmentMode.valueOf(sendAnalogNotificationAttachments);
+                // gestisce le varianti con il "-" come separatore, o ritorna direttamente il valore dell'enum
+                return "AAR-DOCUMENTS-PAYMENTS".equals(sendAnalogNotificationAttachments)?AAR_DOCUMENTS_PAYMENTS:("AAR-DOCUMENTS".equals(sendAnalogNotificationAttachments)?AAR_DOCUMENTS:SendAttachmentMode.valueOf(sendAnalogNotificationAttachments));
             }catch (Exception e){
                 return AAR;
             }
         }
         return AAR;
+    }
+
+    private final String value;
+
+    SendAttachmentMode(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
     }
 }
