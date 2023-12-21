9ab5fd2a340255609d2a6de02b1b1c7e0d55fe57
EmanueleBonofiglio <emanuele.bonofiglio@grupposcai.it>
2023-12-21 16:27:50 +0100

    fix end analog workflow business date

--- src/main/java/it/pagopa/pn/deliverypush/service/mapper/SmartMapper.java
+++ src/main/java/it/pagopa/pn/deliverypush/service/mapper/SmartMapper.java
@@ -1,11 +1,13 @@
 package it.pagopa.pn.deliverypush.service.mapper;
 
+import it.pagopa.pn.commons.exceptions.PnExceptionsCodes;
 import it.pagopa.pn.commons.exceptions.PnInternalException;
 import it.pagopa.pn.deliverypush.dto.timeline.TimelineElementInternal;
 import it.pagopa.pn.deliverypush.dto.timeline.details.*;
 import it.pagopa.pn.deliverypush.exceptions.PnDeliveryPushExceptionCodes;
 import it.pagopa.pn.deliverypush.generated.openapi.server.v1.dto.TimelineElementDetailsV20;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -102,6 +104,10 @@ public class SmartMapper {
             .get();
     }
 
+    /*
+        Mapping effettuato per la modifica dei timestamp per gli
+        elementi di timeline che implementano l'interfaccia ElementTimestampTimelineElementDetails
+     */
     public static  <S,T> T mapToClass(S source, Class<T> destinationClass ){
         T result;
         if( source != null) {
@@ -114,6 +120,10 @@ public class SmartMapper {
         return result;
     }
 
+    /*
+        Mapping effettuato per la modifica dei timestamp per gli
+        elementi di timeline che implementano l'interfaccia ElementTimestampTimelineElementDetails
+     */
     private static  TimelineElementInternal mapTimelineInternal(TimelineElementInternal source ){
         TimelineElementInternal result;
         if( source != null) {
@@ -126,7 +136,10 @@ public class SmartMapper {
         return result;
     }
 
-
+    /**
+        Remapping per gli elementi di timeline per il workflow analogico (workaround per PN-9059)
+        I restanti remapping vengono gestiti tramite il typeMap e l'interfaccia ElementTimestampTimelineElementDetails
+     */
     public static TimelineElementInternal mapTimelineInternal(TimelineElementInternal source, Set<TimelineElementInternal> timelineElementInternalSet) {
         TimelineElementInternal result = mapTimelineInternal(source);
         if(result != null) {
@@ -141,18 +154,18 @@ public class SmartMapper {
                     log.debug("MAP TIMESTAMP: elem category {}, elem previous timestamp {}, elem new timestamp {}  ", result.getCategory(), result.getTimestamp(), details.getNotificationDate());
                     result.setTimestamp(details.getNotificationDate());
                 }
-                case SCHEDULE_REFINEMENT ->{
-                    SendAnalogFeedbackDetailsInt details = findLastSendAnalogFeedbackDetails((RecipientRelatedTimelineElementDetails)result.getDetails(), timelineElementInternalSet);
-                    if(details != null){
-                        log.debug("MAP TIMESTAMP: elem category {}, elem previous timestamp {}, elem new timestamp {} ", result.getCategory(), result.getTimestamp(), details.getNotificationDate());
-                        result.setTimestamp(details.getNotificationDate());
+                case SCHEDULE_REFINEMENT -> {
+                    Instant endAnalogWorkflowBusinessDate =  computeEndAnalogWorkflowBusinessData((RecipientRelatedTimelineElementDetails)result.getDetails(), timelineElementInternalSet, result.getIun());
+                    if(endAnalogWorkflowBusinessDate != null){
+                        log.debug("MAP TIMESTAMP: elem category {}, elem previous timestamp {}, elem new timestamp {} ", result.getCategory(), result.getTimestamp(), endAnalogWorkflowBusinessDate);
+                        result.setTimestamp(endAnalogWorkflowBusinessDate);
                     }
                 }
                 case ANALOG_SUCCESS_WORKFLOW, ANALOG_FAILURE_WORKFLOW, COMPLETELY_UNREACHABLE_CREATION_REQUEST, COMPLETELY_UNREACHABLE -> {
-                    SendAnalogFeedbackDetailsInt details = findLastSendAnalogFeedbackDetails((RecipientRelatedTimelineElementDetails)result.getDetails(), timelineElementInternalSet);
-                    if(details != null){
-                        log.debug("MAP TIMESTAMP: elem category {}, elem previous timestamp {}, elem new timestamp {} ", result.getCategory(), result.getTimestamp(), details.getNotificationDate());
-                        result.setTimestamp(details.getNotificationDate());
+                    Instant endAnalogWorkflowBusinessDate = computeEndAnalogWorkflowBusinessData((RecipientRelatedTimelineElementDetails)result.getDetails(), timelineElementInternalSet, result.getIun());
+                    if(endAnalogWorkflowBusinessDate != null){
+                        log.debug("MAP TIMESTAMP: elem category {}, elem previous timestamp {}, elem new timestamp {} ", result.getCategory(), result.getTimestamp(), endAnalogWorkflowBusinessDate);
+                        result.setTimestamp(endAnalogWorkflowBusinessDate);
                     }else{
                         log.error("SEARCH LAST SEND_ANALOG_FEEDBACK DETAILS NULL element {}",result);
                         throw new PnInternalException("SEND_ANALOG_FEEDBACK NOT PRESENT, ERROR IN MAPPING", PnDeliveryPushExceptionCodes.ERROR_CODE_DELIVERYPUSH_TIMELINE_ELEMENT_NOT_PRESENT);
@@ -174,6 +187,7 @@ public class SmartMapper {
         return result;
     }
 
+
     private static ScheduleRefinementDetailsInt findScheduleRefinementDetails(RecipientRelatedTimelineElementDetails elementDetails, Set<TimelineElementInternal> timelineElementInternalSet) {
         if(elementDetails == null){
             throw new PnInternalException("ELEMENT DETAILS NULL", PnDeliveryPushExceptionCodes.ERROR_CODE_DELIVERYPUSH_TIMELINE_ELEMENT_NOT_PRESENT);
@@ -189,27 +203,41 @@ public class SmartMapper {
         return (ScheduleRefinementDetailsInt) scheduleRefinementTimelineElment.getDetails();
     }
 
-    private static SendAnalogFeedbackDetailsInt findLastSendAnalogFeedbackDetails(RecipientRelatedTimelineElementDetails elementDetails, Set<TimelineElementInternal> timelineElementInternalSet) {
+    private static Instant computeEndAnalogWorkflowBusinessData(RecipientRelatedTimelineElementDetails elementDetails, Set<TimelineElementInternal> timelineElementInternalSet, String iun) {
         if(elementDetails == null){
             throw new PnInternalException("ELEMENT DETAILS NULL", PnDeliveryPushExceptionCodes.ERROR_CODE_DELIVERYPUSH_TIMELINE_ELEMENT_NOT_PRESENT);
         }
         int recIndex = elementDetails.getRecIndex();
 
-        TimelineElementInternal lastSendAnalogFeedback = timelineElementInternalSet.stream().filter(e ->
-                e.getCategory() == TimelineElementCategoryInt.SEND_ANALOG_FEEDBACK &&
-                        e.getDetails() instanceof SendAnalogFeedbackDetailsInt sendAnalogFeedbackDetails &&
-                        sendAnalogFeedbackDetails.getRecIndex() == recIndex
-        ).max((sendFeedback1, sendFeedback2) -> {
-            SendAnalogFeedbackDetailsInt sendFeedback1Details = (SendAnalogFeedbackDetailsInt) sendFeedback1.getDetails();
-            SendAnalogFeedbackDetailsInt sendFeedback2Details = (SendAnalogFeedbackDetailsInt) sendFeedback2.getDetails();
-            return sendFeedback1Details.getNotificationDate().compareTo(sendFeedback2Details.getNotificationDate());
-        }).orElse(null);
+        Instant endAnalogWorkflowBusinessDate = timelineElementInternalSet.stream().filter(elem ->
+                isElementAffectingEndAnalogWorkflowBusinessData(elem,recIndex)
+        ).map(SmartMapper::extractBusinessDate).max(Instant::compareTo).orElse(null);
 
-        if(lastSendAnalogFeedback != null){
-            return (SendAnalogFeedbackDetailsInt)lastSendAnalogFeedback.getDetails();
+        log.debug("Business end analog workflow for iun {} is {} ", iun, endAnalogWorkflowBusinessDate);
+        return endAnalogWorkflowBusinessDate;
+    }
+
+    private static Instant extractBusinessDate(TimelineElementInternal timelineElementInternal){
+        if(timelineElementInternal.getDetails() instanceof SendAnalogFeedbackDetailsInt sendAnalogFeedbackDetails){
+            return sendAnalogFeedbackDetails.getNotificationDate();
+        }else if(timelineElementInternal.getDetails() instanceof PrepareAnalogDomicileFailureDetailsInt){
+            return timelineElementInternal.getTimestamp();
+        }else{
+            throw new PnInternalException("Illegal state: iun "+timelineElementInternal.getIun(), PnExceptionsCodes.ERROR_CODE_PN_GENERIC_ERROR);
+        }
+    }
+
+    private static boolean isElementAffectingEndAnalogWorkflowBusinessData(TimelineElementInternal elementInternal, Integer recIndex){
+        boolean isValidCategory = (elementInternal.getCategory() == TimelineElementCategoryInt.SEND_ANALOG_FEEDBACK ||
+                elementInternal.getCategory() == TimelineElementCategoryInt.PREPARE_ANALOG_DOMICILE_FAILURE);
+
+        boolean isValidRecIndex = false;
+
+        if(isValidCategory && elementInternal.getDetails() instanceof RecipientRelatedTimelineElementDetails details){
+            isValidRecIndex = details.getRecIndex() == recIndex;
         }
 
-        return null;
+        return isValidRecIndex;
     }
 
 }
