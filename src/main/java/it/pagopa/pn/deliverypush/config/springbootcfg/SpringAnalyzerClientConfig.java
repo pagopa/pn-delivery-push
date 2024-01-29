199da65d02ffe9065576a10ee574cc98a5c21e64
Vincenzo.Veniero <vincenzo.veniero@pagopa.it>
2024-01-29 11:00:45 +0100

    PN-9462: Spring Analyzer Added

---
+++ src/main/java/it/pagopa/pn/deliverypush/config/springbootcfg/SpringAnalyzerClientConfig.java
@@ -0,0 +1,7 @@
+package it.pagopa.pn.deliverypush.config.springbootcfg;
+
+import it.pagopa.pn.commons.configs.SpringAnalyzerConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringAnalyzerClientConfig extends SpringAnalyzerConfiguration {}
