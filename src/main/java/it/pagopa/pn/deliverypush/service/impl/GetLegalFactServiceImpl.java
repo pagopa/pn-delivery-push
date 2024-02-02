996b5f8bb432a742c1dc674fa0c5a4bf1ea3329d
empassaro <emanuele.passaro@emeal.nttdata.com>
2024-02-02 09:30:43 +0100

    PN-9771: Updated getLegalFactPrivate API to retrieve contentType

--- src/main/java/it/pagopa/pn/deliverypush/service/impl/GetLegalFactServiceImpl.java
+++ src/main/java/it/pagopa/pn/deliverypush/service/impl/GetLegalFactServiceImpl.java
@@ -11,10 +11,7 @@ import it.pagopa.pn.deliverypush.dto.ext.safestorage.FileDownloadResponseInt;
 import it.pagopa.pn.deliverypush.dto.timeline.TimelineElementInternal;
 import it.pagopa.pn.deliverypush.dto.timeline.details.RecipientRelatedTimelineElementDetails;
 import it.pagopa.pn.deliverypush.dto.timeline.details.TimelineElementDetailsInt;
-import it.pagopa.pn.deliverypush.generated.openapi.server.v1.dto.CxTypeAuthFleet;
-import it.pagopa.pn.deliverypush.generated.openapi.server.v1.dto.LegalFactCategory;
-import it.pagopa.pn.deliverypush.generated.openapi.server.v1.dto.LegalFactDownloadMetadataResponse;
-import it.pagopa.pn.deliverypush.generated.openapi.server.v1.dto.LegalFactListElement;
+import it.pagopa.pn.deliverypush.generated.openapi.server.v1.dto.*;
 import it.pagopa.pn.deliverypush.service.GetLegalFactService;
 import it.pagopa.pn.deliverypush.service.NotificationService;
 import it.pagopa.pn.deliverypush.service.SafeStorageService;
@@ -28,8 +25,10 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.math.BigDecimal;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 @Service
@@ -41,11 +40,11 @@ public class GetLegalFactServiceImpl implements GetLegalFactService {
     private final NotificationService notificationService;
     private final NotificationUtils notificationUtils;
     private final AuthUtils authUtils;
-    
-    public GetLegalFactServiceImpl(TimelineService timelineService, 
-                                   SafeStorageService safeStorageService, 
-                                   NotificationService notificationService, 
-                                   NotificationUtils notificationUtils, 
+
+    public GetLegalFactServiceImpl(TimelineService timelineService,
+                                   SafeStorageService safeStorageService,
+                                   NotificationService notificationService,
+                                   NotificationUtils notificationUtils,
                                    AuthUtils authUtils) {
         this.timelineService = timelineService;
         this.safeStorageService = safeStorageService;
@@ -60,11 +59,31 @@ public class GetLegalFactServiceImpl implements GetLegalFactService {
                                                                         String legalfactId,
                                                                         String senderReceiverId,
                                                                         String mandateId,
-                                                                  CxTypeAuthFleet cxType,
-                                                                  List<String> cxGroups) {
+                                                                        CxTypeAuthFleet cxType,
+                                                                        List<String> cxGroups) {
+        return downloadLegalFact(iun, legalFactType, legalfactId, senderReceiverId, mandateId, cxType, cxGroups)
+                .map(fileDownloadResponse -> generateResponse(iun, legalFactType, legalfactId, fileDownloadResponse));
+    }
+    @Override
+    public Mono<LegalFactDownloadMetadataWithContentTypeResponse> getLegalFactMetadataWithContentType(String iun,
+                                                                                                      LegalFactCategory legalFactType,
+                                                                                                      String legalfactId,
+                                                                                                      String senderReceiverId,
+                                                                                                      String mandateId,
+                                                                                                      CxTypeAuthFleet cxType,
+                                                                                                      List<String> cxGroups) {
+        return downloadLegalFact(iun, legalFactType, legalfactId, senderReceiverId, mandateId, cxType, cxGroups)
+                .map(fileDownloadResponse -> generateResponseWithContentType(iun, legalFactType, legalfactId, fileDownloadResponse));
 
-        log.debug( "GetLegalFactMetadata iun={} legalFactId={} senderReceiverId={}", iun, legalfactId, senderReceiverId );
+    }
 
+    private Mono<FileDownloadResponseInt> downloadLegalFact(String iun,
+                                                            LegalFactCategory legalFactType,
+                                                            String legalfactId,
+                                                            String senderReceiverId,
+                                                            String mandateId,
+                                                            CxTypeAuthFleet cxType,
+                                                            List<String> cxGroups) {
         return Mono.fromCallable(() -> notificationService.getNotificationByIun(iun))
                 .flatMap(notification -> {
                             Mono.fromRunnable(() -> authUtils.checkUserPaAndMandateAuthorization(notification, senderReceiverId, mandateId, cxType, cxGroups));
@@ -76,29 +95,21 @@ public class GetLegalFactServiceImpl implements GetLegalFactService {
                     logEvent.log();
                     return logEvent;
                 })
-                .flatMap( logEvent ->
-                    // la key è la legalfactid
-                    safeStorageService.getFile(legalfactId, false)
-                            .onErrorResume( exc -> {
-                                    logEvent.generateFailure("Exception in getLegalFactMetadata", exc).log();
-                                        return Mono.error(exc);
-                                    }
-                            )
-                            .map( fileDownloadResponse -> {
-                                LegalFactDownloadMetadataResponse response = generateResponse(iun, legalFactType, legalfactId, fileDownloadResponse);
-                                generateSuccessAuditLog(iun, legalfactId, senderReceiverId, logEvent, response);
-                                return response;
-                            })
+                .flatMap(logEvent ->
+                        // la key è la legalfactid
+                        safeStorageService.getFile(legalfactId, false)
+                                .onErrorResume(exc -> {
+                                            logEvent.generateFailure("Exception in getLegalFactMetadata", exc).log();
+                                            return Mono.error(exc);
+                                        }
+                                )
+                                .doOnNext(fileDownloadResponse -> {
+                                    String filename = FileUtils.buildFileName(iun, legalFactType == null ? null : legalFactType.getValue(), legalfactId, fileDownloadResponse.getContentType());
+                                    String url = Objects.nonNull(fileDownloadResponse.getDownload()) ? fileDownloadResponse.getDownload().getUrl() : null;
+                                    BigDecimal retryAfter = Objects.nonNull(fileDownloadResponse.getDownload()) ? fileDownloadResponse.getDownload().getRetryAfter() : null;
+                                    generateSuccessAuditLog(iun, legalfactId, senderReceiverId, logEvent, filename, url, retryAfter);
+                                })
                 );
-    }
-
-    private void generateSuccessAuditLog(String iun, String legalfactId, String senderReceiverId, PnAuditLogEvent logEvent, LegalFactDownloadMetadataResponse response) {
-        String fileName = response.getFilename();
-        String url = response.getUrl();
-        String retryAfter = String.valueOf( response.getRetryAfter() );
-        String message = LogUtils.createAuditLogMessageForDownloadDocument( fileName, url, retryAfter );
-        logEvent.generateSuccess("getLegalFactMetadata iun={} legalFactId={} senderReceiverId={} {}",
-                iun, legalfactId, senderReceiverId, message).log();
     }
 
     private PnAuditLogEvent getAuditLog(String iun, String legalfactId, String senderReceiverId, String mandateId, NotificationInt notification) {
@@ -113,19 +124,36 @@ public class GetLegalFactServiceImpl implements GetLegalFactService {
     @NotNull
     private LegalFactDownloadMetadataResponse generateResponse(String iun, LegalFactCategory legalFactType, String legalfactId, FileDownloadResponseInt fileDownloadResponse) {
         LegalFactDownloadMetadataResponse response = new LegalFactDownloadMetadataResponse();
-        response.setFilename( FileUtils.buildFileName(iun, legalFactType==null?null:legalFactType.getValue(), legalfactId, fileDownloadResponse.getContentType()));
+        response.setFilename(FileUtils.buildFileName(iun, legalFactType == null ? null : legalFactType.getValue(), legalfactId, fileDownloadResponse.getContentType()));
         response.setContentLength(fileDownloadResponse.getContentLength());
         response.setRetryAfter(fileDownloadResponse.getDownload() != null ? fileDownloadResponse.getDownload().getRetryAfter() : null);
         response.setUrl(fileDownloadResponse.getDownload().getUrl());
         return response;
     }
+    @NotNull
+    private LegalFactDownloadMetadataWithContentTypeResponse generateResponseWithContentType(String iun, LegalFactCategory legalFactType, String legalfactId, FileDownloadResponseInt fileDownloadResponse) {
+        LegalFactDownloadMetadataWithContentTypeResponse response = new LegalFactDownloadMetadataWithContentTypeResponse();
+        response.setFilename(FileUtils.buildFileName(iun, legalFactType == null ? null : legalFactType.getValue(), legalfactId, fileDownloadResponse.getContentType()));
+        response.setContentLength(fileDownloadResponse.getContentLength());
+        response.setContentType(fileDownloadResponse.getContentType());
+        response.setRetryAfter(fileDownloadResponse.getDownload() != null ? fileDownloadResponse.getDownload().getRetryAfter() : null);
+        response.setUrl(fileDownloadResponse.getDownload().getUrl());
+        return response;
+    }
+
+    private void generateSuccessAuditLog(String iun, String legalfactId, String senderReceiverId, PnAuditLogEvent logEvent, String fileName, String url, BigDecimal retryAfter) {
+        String retryAfterStr = String.valueOf(retryAfter);
+        String message = LogUtils.createAuditLogMessageForDownloadDocument(fileName, url, retryAfterStr);
+        logEvent.generateSuccess("getLegalFactMetadata iun={} legalFactId={} senderReceiverId={} {}",
+                iun, legalfactId, senderReceiverId, message).log();
+    }
 
     @Override
     @NotNull
     public List<LegalFactListElement> getLegalFacts(String iun, String senderReceiverId, String mandateId, CxTypeAuthFleet cxType, List<String> cxGroups) {
-        log.debug( "Retrieve timeline elements for iun={}", iun );
+        log.debug("Retrieve timeline elements for iun={}", iun);
         Set<TimelineElementInternal> timelineElements = timelineService.getTimeline(iun, true);
-        
+
         NotificationInt notification = notificationService.getNotificationByIun(iun);
 
         String recipientId = authUtils.checkUserPaAndMandateAuthorizationAndRetrieveRealRecipientId(notification, senderReceiverId, mandateId, cxType, cxGroups);
@@ -133,7 +161,7 @@ public class GetLegalFactServiceImpl implements GetLegalFactService {
 
         PnAuditLogBuilder auditLogBuilder = new PnAuditLogBuilder();
         PnAuditLogEvent logEvent = auditLogBuilder
-                .before(eventType, "GetLegalFacts iun={} senderReceiverId={} mandateId={} resolvedRecipientId={}", iun, senderReceiverId, mandateId,recipientId)
+                .before(eventType, "GetLegalFacts iun={} senderReceiverId={} mandateId={} resolvedRecipientId={}", iun, senderReceiverId, mandateId, recipientId)
                 .iun(iun)
                 .build();
         logEvent.log();
@@ -141,19 +169,19 @@ public class GetLegalFactServiceImpl implements GetLegalFactService {
         try {
             List<LegalFactListElement> legalFacts = timelineElements
                     .stream()
-                    .filter( timeEl -> timeEl.getLegalFactsIds() != null )
-                    .filter( timeEl -> checkIfLegalFactCanBeViewed(notification, recipientId, cxType, timeEl))
-                    .sorted( Comparator.comparing(TimelineElementInternal::getTimestamp))
-                    .flatMap( timeEl -> timeEl.getLegalFactsIds().stream().map(
+                    .filter(timeEl -> timeEl.getLegalFactsIds() != null)
+                    .filter(timeEl -> checkIfLegalFactCanBeViewed(notification, recipientId, cxType, timeEl))
+                    .sorted(Comparator.comparing(TimelineElementInternal::getTimestamp))
+                    .flatMap(timeEl -> timeEl.getLegalFactsIds().stream().map(
                             lfId -> LegalFactListElement.builder()
-                                    .taxId( readRecipientId( timeEl, notification ) )
-                                    .iun( iun )
-                                    .legalFactsId( LegalFactIdMapper.internalToExternal(lfId) )
+                                    .taxId(readRecipientId(timeEl, notification))
+                                    .iun(iun)
+                                    .legalFactsId(LegalFactIdMapper.internalToExternal(lfId))
                                     .build()
                     ))
                     .toList();
 
-            log.debug("legalFacts List={}" ,legalFacts );
+            log.debug("legalFacts List={}", legalFacts);
 
             logEvent.generateSuccess().log();
 
@@ -173,12 +201,12 @@ public class GetLegalFactServiceImpl implements GetLegalFactService {
             return true;
     }
 
-    private String readRecipientId( TimelineElementInternal timelineElement, NotificationInt notification ) {
+    private String readRecipientId(TimelineElementInternal timelineElement, NotificationInt notification) {
         String recipientId = null;
 
         if (timelineElement != null) {
             TimelineElementDetailsInt details = timelineElement.getDetails();
-            if ( details instanceof RecipientRelatedTimelineElementDetails recipientRelatedTimelineElementDetails) {
+            if (details instanceof RecipientRelatedTimelineElementDetails recipientRelatedTimelineElementDetails) {
 
                 int recIndex = recipientRelatedTimelineElementDetails.getRecIndex();
                 NotificationRecipientInt recipient = notificationUtils.getRecipientFromIndex(notification, recIndex);
