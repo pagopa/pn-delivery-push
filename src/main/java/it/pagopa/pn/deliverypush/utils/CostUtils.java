64281a5f431aeb9ca35d79fe22731a371600d259
Simone Rondinella <simone.rondinella@pagopa.it>
2024-02-02 12:20:39 +0100

    Modifica a CostUtils

--- src/main/java/it/pagopa/pn/deliverypush/utils/CostUtils.java
+++ src/main/java/it/pagopa/pn/deliverypush/utils/CostUtils.java
@@ -6,9 +6,7 @@ public class CostUtils {
     public static Integer getCostWithVat(Integer vat, Integer cost) {
         Integer costWithVat = null;
         if(vat != null){
-            double doubleVat = vat.doubleValue();
-            double doubleCost = cost.doubleValue();
-            double completeCostWithVat = doubleCost + (doubleCost * doubleVat / 100);
+            double completeCostWithVat = cost.doubleValue() + (cost.doubleValue() * vat.doubleValue() / 100);
             costWithVat = Math.toIntExact(Math.round(completeCostWithVat));
         }
         return costWithVat;
