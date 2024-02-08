a331e05a8cfcfae7fc6d3881214677be15cc0cc9
ivanrosinapa <127737322+ivanrosinapa@users.noreply.github.com>
2024-02-08 17:44:53 +0100

    feature/PN-9813 (#837)

--- src/test/java/it/pagopa/pn/deliverypush/generated/openapi/server/webhook/v1/dto/StreamCreationRequestTest.java
+++ src/test/java/it/pagopa/pn/deliverypush/generated/openapi/server/webhook/v1/dto/StreamCreationRequestTest.java
@@ -1,77 +1,69 @@
 package it.pagopa.pn.deliverypush.generated.openapi.server.webhook.v1.dto;
 
-import org.junit.Ignore;
-
-@Ignore //IVAN
+import java.util.Collections;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class StreamCreationRequestTest {
-//
-//    private StreamCreationRequestV23 request;
-//
-//    @BeforeEach
-//    void setUp() {
-//        request = new StreamCreationRequestV23();
-//        request.setEventType(StreamCreationRequestV23.EventTypeEnum.STATUS);
-//        request.setFilterValues(Collections.singletonList("001"));
-//        request.setTitle("001");
-//    }
-//
-//    @Ignore //IVAN
-//    @Test
-//    void title() {
-//        StreamCreationRequestV23 expected = StreamCreationRequestV23.builder()
-//                .title("001")
-//                .eventType(StreamCreationRequestV23.EventTypeEnum.STATUS)
-//                .filterValues(Collections.singletonList("001"))
-//                .build();
-//        Assertions.assertEquals(expected, request.title("001"));
-//    }
-//
-//    @Test
-//    void getTitle() {
-//        Assertions.assertEquals("001", request.getTitle());
-//    }
-//
-//    @Ignore //IVAN
-//    @Test
-//    void eventType() {
-//        StreamCreationRequestV23 expected = StreamCreationRequestV23.builder()
-//                .title("001")
-//                .eventType(StreamCreationRequestV23.EventTypeEnum.STATUS)
-//                .filterValues(Collections.singletonList("001"))
-//                .build();
-//        Assertions.assertEquals(expected, request.eventType(StreamCreationRequestV23.EventTypeEnum.STATUS));
-//    }
-//
-//    @Test
-//    void getEventType() {
-//        Assertions.assertEquals(StreamCreationRequestV23.EventTypeEnum.STATUS, request.getEventType());
-//    }
-//
-//    @Ignore //IVAN
-//    @Test
-//    void filterValues() {
-//        StreamCreationRequestV23 expected = StreamCreationRequestV23.builder()
-//                .title("001")
-//                .eventType(StreamCreationRequestV23.EventTypeEnum.STATUS)
-//                .filterValues(Collections.singletonList("001"))
-//                .build();
-//        Assertions.assertEquals(expected, request.filterValues(Collections.singletonList("001")));
-//    }
-//
-//    @Test
-//    void getFilterValues() {
-//        Assertions.assertEquals(Collections.singletonList("001"), request.getFilterValues());
-//    }
-//
-//    @Ignore //IVAN
-//    @Test
-//    void testEquals() {
-//        StreamCreationRequestV23 expected = StreamCreationRequestV23.builder()
-//                .title("001")
-//                .eventType(StreamCreationRequestV23.EventTypeEnum.STATUS)
-//                .filterValues(Collections.singletonList("001"))
-//                .build();
-//        Assertions.assertEquals(Boolean.TRUE, expected.equals(request));
-//    }
+
+    private StreamCreationRequestV23 request;
+
+    @BeforeEach
+    void setUp() {
+        request = new StreamCreationRequestV23();
+        request.setEventType(StreamCreationRequestV23.EventTypeEnum.STATUS);
+        request.setFilterValues(Collections.singletonList("001"));
+        request.setTitle("001");
+    }
+
+    @Test
+    void title() {
+        StreamCreationRequestV23 expected = StreamCreationRequestV23.builder()
+                .title("001")
+                .eventType(StreamCreationRequestV23.EventTypeEnum.STATUS)
+                .filterValues(Collections.singletonList("001"))
+                .groups(Collections.emptyList())
+                .build();
+        Assertions.assertEquals(expected, request.title("001"));
+    }
+
+    @Test
+    void getTitle() {
+        Assertions.assertEquals("001", request.getTitle());
+    }
+
+    @Test
+    void eventType() {
+        StreamCreationRequestV23 expected = StreamCreationRequestV23.builder()
+                .title("001")
+                .eventType(StreamCreationRequestV23.EventTypeEnum.STATUS)
+                .filterValues(Collections.singletonList("001"))
+                .groups(Collections.emptyList())
+                .build();
+        Assertions.assertEquals(expected, request.eventType(StreamCreationRequestV23.EventTypeEnum.STATUS));
+    }
+
+    @Test
+    void getEventType() {
+        Assertions.assertEquals(StreamCreationRequestV23.EventTypeEnum.STATUS, request.getEventType());
+    }
+
+    @Test
+    void filterValues() {
+        StreamCreationRequestV23 expected = StreamCreationRequestV23.builder()
+                .title("001")
+                .eventType(StreamCreationRequestV23.EventTypeEnum.STATUS)
+                .filterValues(Collections.singletonList("001"))
+                .groups(Collections.emptyList())
+                .build();
+        Assertions.assertEquals(expected, request.filterValues(Collections.singletonList("001")));
+    }
+
+    @Test
+    void getFilterValues() {
+        Assertions.assertEquals(Collections.singletonList("001"), request.getFilterValues());
+    }
+
+
 }
