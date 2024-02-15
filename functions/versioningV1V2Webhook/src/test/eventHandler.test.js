421f8d90716386522f6ff8e7c446fdadb29bdb6e
fabriziolana <100559407+fabriziolana@users.noreply.github.com>
2024-02-15 09:05:49 +0100

    Feature/pn 9706 (#847)
    * PN-9706: creazione lambda per traduzione eventi v1-v2

---
+++ functions/versioningV1V2Webhook/src/test/eventHandler.test.js
@@ -0,0 +1,138 @@
+const { expect } = require('chai');
+const { eventHandler } = require("../app/eventHandler.js")
+const EventHandler = require("../app/handlers/baseHandler");
+const proxyquire = require("proxyquire").noPreserveCache();
+
+describe('handleEvent', function () {
+
+    it("call consumeEventStreamHandler when checkOwnership returns true", async () => {
+
+        const lambda = proxyquire("../app/eventHandler.js", {
+            "./handlers/consumeEventStreamHandler.js":
+                class MockConsumeEventStreamHandler {
+                        checkOwnership = () => true;
+                        handlerEvent = () => ({
+                            statusCode: 200,
+                            body: 'A'
+                        });
+                },
+            "../app/handlers/createEventStreamHandler":
+                class MockCreateEventStreamHandler {
+                    checkOwnership = () => false;
+                    handlerEvent = () => ({
+                        statusCode: 500,
+                        body: 'B'
+                    });
+                }
+        });
+
+        const result = await lambda.eventHandler({}, {});
+
+        expect(result.statusCode).to.equal(200);
+        expect(result.body).to.equal('A');
+
+    });
+
+    it("call createEventStreamHandler when checkOwnership returns true", async () => {
+
+        const lambda = proxyquire("../app/eventHandler.js", {
+            "./handlers/consumeEventStreamHandler.js":
+                class MockConsumeEventStreamHandler {
+                    checkOwnership = () => false;
+                    handlerEvent = () => ({
+                        statusCode: 500,
+                        body: 'A'
+                    });
+                },
+            "../app/handlers/createEventStreamHandler":
+                class MockCreateEventStreamHandler {
+                    checkOwnership = () => true;
+                    handlerEvent = () => ({
+                        statusCode: 200,
+                        body: 'B'
+                    });
+                }
+        });
+
+        const result = await lambda.eventHandler({}, {});
+
+        expect(result.statusCode).to.equal(200);
+        expect(result.body).to.equal('B');
+
+    });
+
+    it("catch and handle error in handlerEvent", async () => {
+
+        const lambda = proxyquire("../app/eventHandler.js", {
+            "../app/handlers/createEventStreamHandler":
+                class MockCreateEventStreamHandler {
+                    checkOwnership = () => true;
+                    handlerEvent = () => {
+                        throw new Error("Errore Simulato");
+                    };
+
+                }
+        });
+
+        const result = await lambda.eventHandler({}, {});
+
+        expect(result.statusCode).to.equal(500);
+        expect(result.body).to.match(/(?:PN_GENERIC_ERROR)/);
+    });
+
+    it("statusCode 403", async () => {
+
+        const lambda = proxyquire("../app/eventHandler.js", {
+            "../app/handlers/consumeEventStreamHandler":
+                class MockConsumeEventStreamHandler {
+                    checkOwnership = () => true;
+                    handlerEvent = () => {
+                        let error = new Error();
+                        error.response = {
+                            status: 403,
+                        };
+                        throw error;
+                    };
+                }
+        });
+
+        const result = await lambda.eventHandler({}, {});
+
+        expect(result.statusCode).to.equal(400);
+    });
+
+    it("statusCode 404", async () => {
+
+        const lambda = proxyquire("../app/eventHandler.js", {
+            "../app/handlers/consumeEventStreamHandler":
+                class MockConsumeEventStreamHandler {
+                    checkOwnership = () => true;
+                    handlerEvent = () => {
+                        let error = new Error();
+                        error.response = {
+                            status: 404
+                        };
+                        throw error;
+                    };
+                }
+        });
+
+        const result = await lambda.eventHandler({}, {});
+
+        expect(result.statusCode).to.equal(400);
+    });
+
+    it("error endpoint", async () => {
+
+        const event = {
+            path: "/delivery-progresses",
+            httpMethod: "GET"
+        }
+
+        const result = await eventHandler(event, {});
+
+        expect(result.statusCode).to.equal(500);
+        expect(result.body).to.match(/(?:ENDPOINT ERRATO)/);
+    });
+
+});
---
+++ functions/versioningV1V2Webhook/src/test/handlers/updateEventStreamHandler.test.js
@@ -0,0 +1,114 @@
+const { expect } = require("chai");
+const axios = require("axios");
+const UpdateEventStreamHandler  = require("../../app/handlers/updateEventStreamHandler.js");
+
+let MockAdapter = require("axios-mock-adapter")
+let mock = new MockAdapter(axios);
+
+describe("UpdateEventStreamHandler", () => {
+
+    let updateEventStreamHandler;
+
+    beforeEach(() => {
+        updateEventStreamHandler = new UpdateEventStreamHandler();
+        mock = new MockAdapter(axios);
+    });
+
+    afterEach(() => {
+        mock.restore();
+    });
+
+    describe("checkOwnership", () => {
+        it("valid ownership", () => {
+            const streamId = "12345";
+            const event = {
+                path: "/delivery-progresses/streams",
+                pathParameters : { streamId: streamId },
+                httpMethod: "PUT" };
+            const result = updateEventStreamHandler.checkOwnership(event, {});
+            expect(result).to.be.true;
+        });
+
+        it("invalid ownership - case 1", () => {
+            const streamId = "12345";
+            const event = {
+                path: "/delivery-progresses/streams",
+                pathParameters : { streamId: streamId },
+                httpMethod: "GET" };
+            const result = updateEventStreamHandler.checkOwnership(event, {});
+            expect(result).to.be.false;
+        });
+
+        it("invalid ownership - case 2", () => {
+            const event = {
+                path: "/delivery-progresses/streams",
+                httpMethod: "PUT" };
+            const result = updateEventStreamHandler.checkOwnership(event, {});
+            expect(result).to.be.false;
+        });
+    });
+
+    describe("handlerEvent", () => {
+
+        process.env = Object.assign(process.env, {
+            PN_WEBHOOK_URL: "https://api.dev.notifichedigitali.it/delivery-progresses/v2.3",
+        });
+
+        it("successful request", async () => {
+            const streamId = "12345";
+            const event = {
+                path: "/delivery-progresses/streams",
+                pathParameters : { streamId: streamId },
+                httpMethod: "PUT",
+                headers: {},
+                requestContext: {
+                    authorizer: {},
+                },
+                body: {
+                    title: "stream name",
+                    eventType: "STATUS",
+                    filterValues: ["status_1", "status_2"]
+                }
+            };
+
+            let url = `${process.env.PN_WEBHOOK_URL}/streams/${streamId}`;
+
+            const responseBodyV23 = {
+                title: "stream name",
+                eventType: "STATUS",
+                groups: [{
+                    groupId: "group1",
+                    groupName: "Group One"
+                },
+                    {
+                        groupId: "group2",
+                        groupName: "Group Two"
+                    }],
+                filterValues: ["status_1", "status_2"],
+                streamId: "12345678-90ab-cdef-ghij-klmnopqrstuv",
+                activationDate: "2024-02-01T12:00:00Z",
+                disabledDate: "2024-02-02T12:00:00Z",
+                version: "v10"
+            }
+
+            const responseBodyV10 = {
+                title: "stream name",
+                eventType: "STATUS",
+                filterValues: ["status_1", "status_2"],
+                streamId: "12345678-90ab-cdef-ghij-klmnopqrstuv",
+                activationDate: "2024-02-01T12:00:00Z"
+            }
+
+            mock.onPut(url).reply(200, responseBodyV23);
+
+            const context = {};
+            const response = await updateEventStreamHandler.handlerEvent(event, context);
+
+            expect(response.statusCode).to.equal(200);
+            expect(response.body).to.equal(JSON.stringify(responseBodyV10));
+
+            expect(mock.history.put.length).to.equal(1);
+        });
+    });
+
+});
