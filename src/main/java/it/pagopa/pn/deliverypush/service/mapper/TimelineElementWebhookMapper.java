4ede37e92bee65381f876414efdc3c4ec0c1f567
pasqualino-cristaudo-acn <pasqualino.cristaudo@accenture.com>
2024-02-23 17:58:36 +0100

    Reimplemented TimelineElementWebhookMapper to execute delivery push's logic mapper

--- src/main/java/it/pagopa/pn/deliverypush/service/mapper/TimelineElementWebhookMapper.java
+++ src/main/java/it/pagopa/pn/deliverypush/service/mapper/TimelineElementWebhookMapper.java
@@ -1,7 +1,6 @@
 package it.pagopa.pn.deliverypush.service.mapper;
 
 import it.pagopa.pn.deliverypush.dto.timeline.TimelineElementInternal;
-
 import it.pagopa.pn.deliverypush.generated.openapi.server.webhook.v1.dto.*;
 
 
@@ -9,23 +8,8 @@ public class TimelineElementWebhookMapper {
     private TimelineElementWebhookMapper(){}
 
     public static TimelineElementV23 internalToExternal(TimelineElementInternal internalDto) {
-        var builder = TimelineElementV23.builder()
-                .category(internalDto.getCategory() != null ? TimelineElementCategoryV23.fromValue( internalDto.getCategory().getValue() ) : null)
-                .elementId(internalDto.getElementId())
-                .timestamp(internalDto.getTimestamp())
-                .details( SmartMapper.mapToClass(internalDto.getDetails(), TimelineElementDetailsV23.class) );
-
-        if(internalDto.getLegalFactsIds() != null){
-            builder.legalFactsIds(
-                    internalDto.getLegalFactsIds().stream()
-                            .map(legalFact ->  LegalFactsId.builder()
-                                        .key(legalFact.getKey())
-                                        .category( legalFact.getCategory() != null ? LegalFactCategory.fromValue(legalFact.getCategory().getValue()): null)
-                                        .build()
-                            ).toList()
-            );
-        }
-
-        return builder.build();
+        // passo da TimelineElementMapper.internalToExternal(internalDto) in modo da replicare gli stessi controlli già presenti per il mapper di delivery push
+        it.pagopa.pn.deliverypush.generated.openapi.server.v1.dto.TimelineElementV23 timelineElementV23 = TimelineElementMapper.internalToExternal(internalDto);
+        return SmartMapper.mapToClass(timelineElementV23, TimelineElementV23.class);
     }
 }
