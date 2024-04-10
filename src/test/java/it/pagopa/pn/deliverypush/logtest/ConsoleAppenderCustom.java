66dc266aed6423414e89d95e8331007b56ae93fd
ivanrosinapa <127737322+ivanrosinapa@users.noreply.github.com>
2024-04-10 09:36:06 +0200

    Feature/pn 9494 (#901)
    * PN-9494
    
    * PN-9494
    
    * PN-9494

--- src/test/java/it/pagopa/pn/deliverypush/logtest/ConsoleAppenderCustom.java
+++ src/test/java/it/pagopa/pn/deliverypush/logtest/ConsoleAppenderCustom.java
@@ -3,16 +3,17 @@ package it.pagopa.pn.deliverypush.logtest;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.ConsoleAppender;
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.ArrayList;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ConsoleAppenderCustom extends ConsoleAppender<ILoggingEvent> {
 
     private static List<LogEvent> eventList
             = new ArrayList<>();
+    private static List<LogEvent> eventListWarning
+        = new ArrayList<>();
 
     @Override
     protected void append(ILoggingEvent event) {
@@ -20,6 +21,13 @@ public class ConsoleAppenderCustom extends ConsoleAppender<ILoggingEvent> {
         if(Level.ERROR.equals(event.getLevel())){
             
             eventList.add(LogEvent.builder()
+                    .classPath(event.getLoggerName())
+                    .message(event.getMessage())
+                    .logLevel(LogLevel.valueOf(event.getLevel().levelStr))
+                    .build());
+        } else if (Level.WARN.equals(event.getLevel())){
+
+                eventListWarning.add(LogEvent.builder()
                     .classPath(event.getLoggerName())
                     .message(event.getMessage())
                     .logLevel(LogLevel.valueOf(event.getLevel().levelStr))
@@ -33,13 +41,20 @@ public class ConsoleAppenderCustom extends ConsoleAppender<ILoggingEvent> {
     }
 
     public static void checkLogs(String acceptedError){
-        if( eventList != null && ! eventList.isEmpty() ){
+        checkLogs(eventList, acceptedError);
+    }
+
+    public static void checkWarningLogs(String acceptedError){
+        checkLogs(eventListWarning, acceptedError);
+    }
+
+    private static void checkLogs(List<LogEvent> list, String acceptedError){
+        if( list != null && ! list.isEmpty() ){
             boolean throwException = true;
             if (acceptedError != null)
             {
                 throwException = false;
-                for (LogEvent le :
-                        eventList) {
+                for (LogEvent le : list) {
                     if (!le.getMessage().startsWith(acceptedError))
                     {
                         throwException = true;
@@ -48,8 +63,8 @@ public class ConsoleAppenderCustom extends ConsoleAppender<ILoggingEvent> {
                 }
             }
             if (throwException) {
-                log.warn("[TEST] There are log not excpeted. Log list {}", eventList);
-                NotExpectedLogException expectedLogExceptionTest = new NotExpectedLogException("There are problem ", eventList);
+                log.warn("[TEST] There are log not excpeted. Log list {}", list);
+                NotExpectedLogException expectedLogExceptionTest = new NotExpectedLogException("There are problem ", list);
                 initializeLog();
                 throw expectedLogExceptionTest;
             }
@@ -58,5 +73,6 @@ public class ConsoleAppenderCustom extends ConsoleAppender<ILoggingEvent> {
 
     public static void initializeLog() {
         eventList = new ArrayList<>();
+        eventListWarning = new ArrayList<>();
     }
 }
