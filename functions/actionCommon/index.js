b937c032f8239dd19946b3b3e2a98c13ce47f4b1
Alessandro Masci <alessandro.masci@pagopa.it>
2024-05-08 15:10:48 +0200

    PN-11120: add action common get parameter store from layer

---
+++ functions/actionCommon/index.js
@@ -0,0 +1,9 @@
+const AllowedIssuerDao = require('./app/modules/dao/AllowedIssuerDao')
+const UrlDownloader = require('./app/modules/http/UrlDownloader')
+const JwtAttributesDao = require('./app/modules/dao/JwtAttributesDao')
+
+module.exports = {
+    AllowedIssuerDao,
+    UrlDownloader,
+    JwtAttributesDao
+}
