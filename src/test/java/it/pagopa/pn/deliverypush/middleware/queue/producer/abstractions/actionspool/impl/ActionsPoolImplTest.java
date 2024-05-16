77eefbd245cf99142e971d40d289f6a577eeddef
Alessandro Masci <alessandro.masci@pagopa.it>
2024-05-16 16:03:45 +0200

    PN-10995: sonar trigger

--- src/test/java/it/pagopa/pn/deliverypush/middleware/queue/producer/abstractions/actionspool/impl/ActionsPoolImplTest.java
+++ src/test/java/it/pagopa/pn/deliverypush/middleware/queue/producer/abstractions/actionspool/impl/ActionsPoolImplTest.java
@@ -207,7 +207,6 @@ class ActionsPoolImplTest {
                 .type(ActionType.ANALOG_WORKFLOW)
                 .build();
 
-
         // performance improvement enables
         Mockito.when( configs.getPerformanceImprovementStartDate() ).thenReturn( now.minus(Duration.ofSeconds(100)).toString());
         Mockito.when( configs.getPerformanceImprovementEndDate() ).thenReturn( now.plus(Duration.ofSeconds(200)).toString() );
