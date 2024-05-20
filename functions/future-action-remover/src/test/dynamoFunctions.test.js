aaf3a9705aa7eb13539421f39eddfe17608b281a
abertu75 <122233952+abertu75@users.noreply.github.com>
2024-05-20 11:57:56 +0200

    Feature/10992 (#936)
    * feat: added lambda action-remover to delivery-push
    
    * test: added tests mock
    
    * test: missing dependency
    
    * test: added eventHandler first release
    
    * config: added feature flat config
    
    * test: update
    
    * test: eventHandler completed
    
    * test:added batchDelete tests
    
    * test: added unprocessed items test
    
    * test: fixed error
    
    * fix: after testing in infra
    
    * test: fix error on tests
    
    * PN-11120: add map queueName;queueUrl mngt
    
    * fix pn send mode
    
    * PN-10784: cambio parametro PnSendMode
    
    * PN-10784: fix openapi
    
    * PN-10784: fix openapi
    
    * PN-10784: fix openapi
    
    * PN-10784: fix openapi
    
    * PN-10784: fix openapi
    
    * PN-10784: fix openapi
    
    * PN-10784: modifica openapi
    
    * PN-10784: modifica openapi
    
    * Feature/pn 8812 (#924)
    
    * [2.6.0-RC.1] Modifica versione nel pom.xml
    
    * [2.6.0-RC.2] Modifica versione nel pom.xml
    
    * PN-10875 Fix gestione validazione notifica senza pagamenti ma con DELIVERY_MODE e ASYNC
    
    * PN-10875 Modifica logging
    
    * cambiata versione
    
    * [2.5.6-RC.1] Modifica versione nel pom.xml
    
    * PN-10875 Gestione casistica notifica senza pagamenti anche in caso di notifica rifiutata o cancellata
    
    * [2.5.6-RC.2] Modifica versione nel pom.xml
    
    * [2.5.6] Create Release Version
    
    * [2.6.0-RC.3] Modifica versione nel pom.xml
    
    * [2.6.0] Create Release Version
    
    * increment minor version
    
    * PN-8812
    
    * PN-8812
    
    ---------
    
    Co-authored-by: GitHub Actions Bot <>
    Co-authored-by: Simone Rondinella <simone.rondinella@pagopa.it>
    Co-authored-by: simonerondinella <92715398+simonerondinella@users.noreply.github.com>
    Co-authored-by: vincenzoracca <vincenzo.racca@pagopa.it>
    Co-authored-by: Vincenzo Racca <113343826+vincenzo-racca-pa@users.noreply.github.com>
    
    * PN-10784: modifica openapi con ulteriori maxLength specificati
    
    * PN-10784: modifica openapi con ulteriori maxLength specificati
    
    * PN-10784: modifica openapi con ulteriori maxLength specificati
    
    * PN-10784: modifica openapi con ulteriori maxLength specificati
    
    * PN-10784: modifica descrizioni openapi
    
    * PN-10784: modifica descrizioni openapi
    
    * Revert "PN-10784: modifica descrizioni openapi"
    
    This reverts commit 9066cd0dce34244393c86ff59ceb28d2dae2bcdd.
    
    * PN-11140 (#932)
    
    * PN-10811: fix statemap
    
    * PN-10991 Disabilitati test che falliscono in maniera randomica
    
    * chore: updated sonarque projectkey
    
    * fix: projectname in sonarqube
    
    * test: disabled problematic class
    
    ---------
    
    Co-authored-by: Alessandro Masci <alessandro.masci@pagopa.it>
    Co-authored-by: EmanueleBonofiglio <emanuele.bonofiglio@pagopa.it>
    Co-authored-by: fabriziolana <fabrizio.lana@necsy.it>
    Co-authored-by: ivanrosinapa <127737322+ivanrosinapa@users.noreply.github.com>
    Co-authored-by: Simone Rondinella <simone.rondinella@pagopa.it>
    Co-authored-by: simonerondinella <92715398+simonerondinella@users.noreply.github.com>
    Co-authored-by: vincenzoracca <vincenzo.racca@pagopa.it>
    Co-authored-by: Vincenzo Racca <113343826+vincenzo-racca-pa@users.noreply.github.com>

---
+++ functions/future-action-remover/src/test/dynamoFunctions.test.js
@@ -0,0 +1,322 @@
+/* eslint-disable no-unused-vars */
+const { expect, assert } = require("chai");
+const { describe, it, before, after, afterEach } = require("mocha");
+const sinon = require("sinon");
+const proxyquire = require("proxyquire").noPreserveCache();
+
+const {
+  DynamoDBDocumentClient,
+  GetCommand,
+  PutCommand,
+  QueryCommand,
+} = require("@aws-sdk/lib-dynamodb");
+
+const { mockClient } = require("aws-sdk-client-mock");
+
+const { DateTime } = require("luxon");
+
+const {
+  getLastTimeSlotWorked,
+  setLastTimeSlotWorked,
+  getActionsByTimeSlot,
+} = require("../app/dynamoFunctions.js");
+
+const config = require("config");
+const LAST_POLL_TABLE_NAME = config.get("LAST_POLL_TABLE_NAME");
+const FUTURE_TABLE_NAME = config.get("FUTURE_TABLE_NAME");
+
+describe("dynamoFunctions tests", function () {
+  let ddbMock;
+
+  before(() => {
+    ddbMock = mockClient(DynamoDBDocumentClient);
+  });
+
+  afterEach(() => {
+    ddbMock.reset();
+  });
+
+  after(() => {
+    ddbMock.restore();
+  });
+
+  it("test getLastTimeSlotWorked: item found", async () => {
+    const keyValue = "1";
+    const timeValue = "12345";
+
+    const params = {
+      TableName: LAST_POLL_TABLE_NAME,
+      Key: {
+        lastPoolKey: keyValue,
+      },
+    };
+
+    ddbMock.on(GetCommand, params).resolves({
+      Item: { lastPoolKey: keyValue, lastPollExecuted: timeValue },
+    });
+
+    const item = await getLastTimeSlotWorked(params.TableName, keyValue);
+
+    expect(item).to.be.equal(timeValue);
+  });
+
+  it("test getLastTimeSlotWorked: item not found", async () => {
+    const keyValue = "1";
+
+    const params = {
+      TableName: LAST_POLL_TABLE_NAME,
+      Key: {
+        lastPoolKey: keyValue,
+      },
+    };
+    ddbMock.on(GetCommand, params).resolves({ Item: null });
+
+    try {
+      await getLastTimeSlotWorked(params.TableName, keyValue);
+    } catch (error) {
+      expect(error.name).to.be.eq("ItemNotFoundException");
+      return;
+    }
+
+    assert(false);
+  });
+
+  it("test getLastTimeSlotWorked: item found but wrong format", async () => {
+    const keyValue = "1";
+    const timeValue = "12345";
+
+    const params = {
+      TableName: LAST_POLL_TABLE_NAME,
+      Key: {
+        lastPoolKey: keyValue,
+      },
+    };
+
+    ddbMock.on(GetCommand, params).resolves({
+      Item: { lastPoolKey: keyValue, lastPollExecuted: timeValue },
+    });
+
+    const item = await getLastTimeSlotWorked(params.TableName, keyValue);
+
+    expect(item).to.be.equal(timeValue);
+  });
+
+  it("test setLastTimeSlotWorked: add item", async () => {
+    const keyValue = "1";
+
+    const params = {
+      TableName: LAST_POLL_TABLE_NAME,
+      Key: {
+        lastPoolKey: keyValue,
+      },
+    };
+
+    ddbMock.on(PutCommand, params).resolves();
+    await setLastTimeSlotWorked(
+      params.TableName,
+      keyValue,
+      DateTime.local()
+        .toUTC()
+        .toISO({ suppressMilliseconds: true, includeOffset: true })
+    );
+  });
+
+  it("test getActionsByTimeSlot not found", async () => {
+    const keyValue = "2024-05-20T12:00";
+    const params = {
+      TableName: FUTURE_TABLE_NAME,
+      KeyConditionExpression: "timeSlot = :ts",
+      ExpressionAttributeValues: {
+        ":ts": keyValue,
+        ":tS": keyValue,
+        ":tE": keyValue,
+      },
+      FilterExpression: "notBefore >= :tS AND notBefore <= :tE",
+    };
+
+    const resultItems = [];
+
+    ddbMock.on(QueryCommand, params).resolves({
+      Items: [],
+    });
+    const result = await getActionsByTimeSlot(params.TableName, {
+      timeSlot: keyValue,
+      startTime: keyValue,
+      endTime: keyValue,
+    });
+    expect(result).to.deep.equal({
+      timeSlot: keyValue,
+      items: resultItems,
+      lastEvaluatedKey: undefined,
+    });
+  });
+
+  it("test getActionsByTimeSlot not found", async () => {
+    const keyValue = "2024-05-20T12:00";
+
+    const params = {
+      TableName: FUTURE_TABLE_NAME,
+      KeyConditionExpression: "timeSlot = :ts",
+      ExpressionAttributeValues: {
+        ":ts": keyValue,
+        ":tS": keyValue,
+        ":tE": keyValue,
+      },
+      FilterExpression: "notBefore >= :tS AND notBefore <= :tE",
+    };
+    const resultItems = [{ iun: "iunTest" }];
+
+    ddbMock.on(QueryCommand, params).resolves({
+      Items: resultItems,
+    });
+    const result = await getActionsByTimeSlot(params.TableName, {
+      timeSlot: keyValue,
+      startTime: keyValue,
+      endTime: keyValue,
+    });
+
+    expect(result).to.deep.equal({
+      timeSlot: keyValue,
+      items: resultItems,
+      lastEvaluatedKey: undefined,
+    });
+  });
+
+  it("test BatchDelete: item less than batch", async () => {
+    let mockResponse = {
+      UnprocessedItems: {},
+    };
+
+    // Stubbing DynamoDBClient and DynamoDBDocumentClient
+    const DynamoDBClientStub = sinon.stub();
+    const batchWriteStub = sinon.stub();
+
+    // Assegna la funzione di stubbing al metodo batchWrite di DynamoDBDocumentClient
+    DynamoDBClientStub.prototype.batchWrite = batchWriteStub;
+    batchWriteStub.returns(mockResponse);
+    let count = 0;
+    const lambda = proxyquire.noCallThru().load("../app/dynamoFunctions.js", {
+      "@aws-sdk/lib-dynamodb": {
+        DynamoDBClient: DynamoDBClientStub,
+        BatchWriteCommand: sinon.stub(),
+        DynamoDBDocumentClient: {
+          from: () => ({
+            batchWrite: batchWriteStub,
+            send: (params) => {
+              count++;
+
+              return mockResponse;
+            },
+          }),
+        },
+      },
+    });
+
+    const result = await lambda.batchDelete(
+      "placeholder",
+      require("./testData/deleteTest-lessBatchSize/dataset.json"),
+      () => false
+    );
+    expect(count).to.be.equal(1);
+    expect(result).to.be.true;
+  });
+
+  it("test BatchDelete: item more than  a batch", async () => {
+    let mockResponse = {
+      UnprocessedItems: {},
+    };
+
+    // Stubbing DynamoDBClient and DynamoDBDocumentClient
+    const DynamoDBClientStub = sinon.stub();
+    const batchWriteStub = sinon.stub();
+
+    // Assegna la funzione di stubbing al metodo batchWrite di DynamoDBDocumentClient
+    DynamoDBClientStub.prototype.batchWrite = batchWriteStub;
+    batchWriteStub.returns(mockResponse);
+    let count = 0;
+    const lambda = proxyquire.noCallThru().load("../app/dynamoFunctions.js", {
+      "@aws-sdk/lib-dynamodb": {
+        DynamoDBClient: DynamoDBClientStub,
+        BatchWriteCommand: sinon.stub(),
+        DynamoDBDocumentClient: {
+          from: () => ({
+            batchWrite: batchWriteStub,
+            send: (params) => {
+              count++;
+
+              return mockResponse;
+            },
+          }),
+        },
+      },
+    });
+
+    const result = await lambda.batchDelete(
+      "placeholder",
+      require("./testData/deleteTest-moreBatchSize/dataset.json"),
+      () => false
+    );
+    expect(result).to.be.true;
+    expect(count).to.be.equal(2);
+  });
+
+  it("test BatchDelete: batch operation Error", async () => {
+    // Stubbing DynamoDBClient and DynamoDBDocumentClient
+
+    const testData = require("./testData/deleteTest-moreBatchSize/dataset.json");
+
+    let mockResponse = {
+      UnprocessedItems: {
+        "pn-FutureAction": [
+          {
+            DeleteRequest: {
+              Item: testData[0],
+            },
+          },
+        ],
+      },
+    };
+
+    const DynamoDBClientStub = sinon.stub();
+    const batchWriteStub = sinon.stub();
+
+    // Assegna la funzione di stubbing al metodo batchWrite di DynamoDBDocumentClient
+    DynamoDBClientStub.prototype.batchWrite = batchWriteStub;
+    batchWriteStub.returns(mockResponse);
+    let count = 0;
+    const lambda = proxyquire.noCallThru().load("../app/dynamoFunctions.js", {
+      "@aws-sdk/lib-dynamodb": {
+        DynamoDBClient: DynamoDBClientStub,
+        BatchWriteCommand: sinon.stub(),
+        DynamoDBDocumentClient: {
+          from: () => ({
+            batchWrite: batchWriteStub,
+            send: (params) => {
+              count++;
+
+              return mockResponse;
+            },
+          }),
+        },
+      },
+    });
+
+    let retryNo = 0;
+    const MAX_RETRY_NO = 5;
+
+    const isTimeToLeave = () => {
+      retryNo++;
+      if (retryNo < MAX_RETRY_NO) return false;
+
+      return true;
+    };
+
+    const res = await lambda.batchDelete(
+      "placeholder",
+      testData,
+      isTimeToLeave
+    );
+
+    expect(res).to.be.false;
+  });
+});
