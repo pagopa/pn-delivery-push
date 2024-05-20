2b713e1fbe9737ef7b561af154e0387b419ac227
Andrea Bertucci <andrea.bertucci@pagopa.it>
2024-05-20 10:56:03 +0200

    chore: removed unused files

--- functions/action-enqueuer/src/index.js
+++
@@ -1,8 +0,0 @@
-const { handleEvent } = require("./src/app/eventHandler.js");
-
-async function handler(event, context) {
-  console.info("New Execution scheduled ", event);
-  return handleEvent(event, context);
-}
-
-exports.handler = handler;
--- functions/action-enqueuer/src/test.js
+++
@@ -1,4 +0,0 @@
-const config = require("config");
-//return handleEvent(event, context);
-
-console.log("AAAA!", config.get("MAX_SQS_BATCH_SIZE"));
