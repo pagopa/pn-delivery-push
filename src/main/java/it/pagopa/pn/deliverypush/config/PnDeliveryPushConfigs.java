2e0982209e7abf71ec90b98f028062cb94475e45
Sarah Donvito <sarah.donvito@pagopa.it>
2024-05-21 12:28:23 +0200

    fix(PN-11124): update AAR_RADDalt template with phone number for assistance

--- src/main/java/it/pagopa/pn/deliverypush/config/PnDeliveryPushConfigs.java
+++ src/main/java/it/pagopa/pn/deliverypush/config/PnDeliveryPushConfigs.java
@@ -15,6 +15,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.util.unit.DataSize;
+import javax.annotation.PostConstruct;
 
 @Configuration
 @ConfigurationProperties( prefix = "pn.delivery-push")
@@ -213,6 +214,7 @@ public class PnDeliveryPushConfigs {
         private String quickAccessUrlAarDetailSuffix;
         private String landingUrl;
         private Map<String, String> additional;
+        private String raddPhoneNumber;
    }
 
     @Data
@@ -264,5 +266,10 @@ public class PnDeliveryPushConfigs {
     }
 
     private boolean safeStorageFileNotFoundRetry;
+
+    @PostConstruct
+    public void init() {
+        System.out.println(this);
+    }
 
 }
