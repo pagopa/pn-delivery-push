c9e605ccc421f954faa7afbf1d0478370a5af689
Andrea Bertucci <andrea.bertucci@pagopa.it>
2024-06-04 16:11:57 +0200

    feat: added CheckRADDExperimentation Stub

--- src/main/java/it/pagopa/pn/deliverypush/utils/CheckRADDExperimentation.java
+++ src/main/java/it/pagopa/pn/deliverypush/utils/CheckRADDExperimentation.java
@@ -1,2 +1,12 @@
-package it.pagopa.pn.deliverypush.utils;public class CheckRADDExperimentation {
+
+package it.pagopa.pn.deliverypush.utils;
+
+import it.pagopa.pn.deliverypush.dto.address.PhysicalAddressInt;
+
+public class CheckRADDExperimentation {
+    public static boolean checkAddress(PhysicalAddressInt toCheck){
+        return true;
+
+    }
+
 }
