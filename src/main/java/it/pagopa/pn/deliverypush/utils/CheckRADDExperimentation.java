896c8e8b613814ec7254aa9cd7d301cec1fbbd94
Andrea Bertucci <andrea.bertucci@pagopa.it>
2024-06-04 17:16:04 +0200

    feat: filled methods with logic from parameter store

--- src/main/java/it/pagopa/pn/deliverypush/utils/CheckRADDExperimentation.java
+++ src/main/java/it/pagopa/pn/deliverypush/utils/CheckRADDExperimentation.java
@@ -1,67 +1,64 @@
 package it.pagopa.pn.deliverypush.utils;
 
+import it.pagopa.pn.commons.abstractions.ParameterConsumer;
 import it.pagopa.pn.deliverypush.dto.address.PhysicalAddressInt;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
-
-
-import it.pagopa.pn.commons.abstractions.ParameterConsumer;
-
-import lombok.extern.slf4j.Slf4j;
 
 
 @Slf4j
 public class CheckRADDExperimentation {
-    private static final String[] experimentalCountries = ["it", "italia", "italy"];
+    private static final String[] EXPERIMENTAL_COUNTRIES = {"it", "italia", "italy"};
     private static final String[] PARAMETER_STORES_MAP_ZIP_EXPERIMENTATION_LIST =
-        ["RADD_EXPER", "italia", "italy"];
-
+            {"radd-experimentation-zip-1",
+                    "radd-experimentation-zip-2",
+                    "radd-experimentation-zip-3",
+                    "radd-experimentation-zip-4",
+                    "radd-experimentation-zip-5"
+            };
     private final ParameterConsumer parameterConsumer;
-
-    private static final String PARAMETER_STORE_MAP_TAX_ID_WHITE_LIST_NAME = "MapTaxIdWhiteList";
 
     public CheckRADDExperimentation(ParameterConsumer parameterConsumer) {
         this.parameterConsumer = parameterConsumer;
     }
 
-    public static boolean checkAddress(PhysicalAddressInt toCheck){
-        // countTry in admitted countries
-        if (isAnExperimentalCountry(toCheck.getForeignState()))
+    private static boolean isAnExperimentalCountry(final String countryToCheck) {
+        if (StringUtils.isBlank(countryToCheck)) return true;
 
-            // TOOD zip in paramater store
-            return true;
-        //  toCheck.zip
-        return true;
-
+        for (String currentCountry :CheckRADDExperimentation.EXPERIMENTAL_COUNTRIES) {
+            if (StringUtils.equalsIgnoreCase(currentCountry, countryToCheck))
+                return true;
+        }
+        return false;
     }
 
+    public boolean checkAddress(PhysicalAddressInt toCheck) {
 
-    public Boolean isInStore(String zipCode, String storeName ) {
-        log.debug( "Looking for zip code={}", zipCode);
+        if (CheckRADDExperimentation.isAnExperimentalCountry(toCheck.getForeignState())) {
+            // country in admitted countries
+            for (String currentStore : CheckRADDExperimentation.PARAMETER_STORES_MAP_ZIP_EXPERIMENTATION_LIST) {
+                if (isInStore(toCheck.getZip(), currentStore))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isInStore(String zipCode, String storeName) {
+        log.debug("Looking for zip code={}", zipCode);
         Optional<String[]> zipLists = parameterConsumer.getParameterValue(storeName, String[].class);
         if (zipLists.isPresent()) {
             String[] experimentalZipList = zipLists.get();
             for (String currentZip : experimentalZipList) {
-                if ( currentZip.equals( zipCode ) ) {
-                    log.debug("zipCode={} is in experimental list", zipCode );
+                if (currentZip.equals(zipCode)) {
+                    log.debug("zipCode={} is in experimental list", zipCode);
                     return true;
                 }
             }
         }
         log.debug("zipCode={} not found in experimental list", zipCode);
-        return false;
-    }
-
-    private static boolean isAnExperimentalCountry(final String countryToCheck){
-        if  (StringUtils.isBlank(countryToCheck)) return true;
-
-        for (String currentCountry: experimentalCountries){
-            if (StringUtils.equalsIgnoreCase(currentCountry, countryToCheck))
-                return true;
-        }
         return false;
     }
 }
