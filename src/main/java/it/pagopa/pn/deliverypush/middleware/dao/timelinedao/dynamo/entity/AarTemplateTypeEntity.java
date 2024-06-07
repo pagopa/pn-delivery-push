d1b87c3d4e832fa99a31364c83badba1a0b34c65
Simone Rondinella <simone.rondinella@pagopa.it>
2024-06-07 16:45:10 +0200

    PN-11447 Modificato Enum

--- src/main/java/it/pagopa/pn/deliverypush/middleware/dao/timelinedao/dynamo/entity/AarTemplateTypeEntity.java
+++ src/main/java/it/pagopa/pn/deliverypush/middleware/dao/timelinedao/dynamo/entity/AarTemplateTypeEntity.java
@@ -1,16 +1,17 @@
 package it.pagopa.pn.deliverypush.middleware.dao.timelinedao.dynamo.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
-
-@Getter
-@AllArgsConstructor
-@ToString
 public enum AarTemplateTypeEntity {
     AAR_NOTIFICATION("AAR_NOTIFICATION"),
     AAR_NOTIFICATION_RADD("AAR_NOTIFICATION_RADD"),
     AAR_NOTIFICATION_RADD_ALT("AAR_NOTIFICATION_RADD_ALT");
 
     private final String value;
+
+    AarTemplateTypeEntity(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
