32944a1b9640f9ee166f830701800b3152cc5399
Ivan Rosina <ivan.rosina@cinetix-team.it>
2024-06-20 07:53:18 +0200

    PN-11674

--- src/main/java/it/pagopa/pn/deliverypush/service/impl/WebhookStreamsServiceImpl.java
+++ src/main/java/it/pagopa/pn/deliverypush/service/impl/WebhookStreamsServiceImpl.java
@@ -182,7 +182,7 @@ public class WebhookStreamsServiceImpl extends WebhookServiceImpl implements Web
                 return false;
             }
 
-            if (request.getGroups() != null && !request.getGroups().containsAll(getGroups(r))){
+            if (!request.getGroups().containsAll(getGroups(r))){
                 return false;
             }
 
