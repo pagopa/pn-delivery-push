06d60cbb23434e18d41cf5a67ba910e95a1ad8df
Andrea Bertucci <andrea.bertucci@pagopa.it>
2024-06-26 18:13:10 +0200

    feat: added DLQ for timeouted messages

--- functions/action-enqueuer/src/app/eventHandler.js
+++ functions/action-enqueuer/src/app/eventHandler.js
@@ -20,14 +20,9 @@ const isRecordToSend = (record) => record.eventName === "REMOVE";
 
 function mapMessageFromKinesisToAction(record) {
   let action = record.dynamodb.OldImage;
-  //console.debug("[ACTION_ENQUEUER]", "Action", action);
   const regularAction = unmarshall(action);
-  //console.debug("[ACTION_ENQUEUER]", "regularAction", regularAction);
   return regularAction;
 }
-
-// const getRecordCreation = (record) =>
-//   new Date(record.dynamodb.ApproximateCreationDateTime).toISOString();
 
 const sendMessages = async (destinationEndpoint, actions, timeoutFn) => {
   const notSendedResult = {
@@ -46,7 +41,11 @@ const sendMessages = async (destinationEndpoint, actions, timeoutFn) => {
     return notSendedResult;
   }
 
-  let sqsParams = { endpoint: destinationEndpoint };
+  let sqsParams = { endpoint: destinationEndpoint, timeoutEndpoint: config.get("TIMEOUT_DLQ")};
+  console.debug(
+    "[ACTION_ENQUEUER]",
+    `Parameters: ${sqsParams}`
+  );
   console.debug(
     "[ACTION_ENQUEUER]",
     `Sending ${actions.length} actions to SQS queue ${sqsParams.endpoint}`
@@ -57,10 +56,28 @@ const sendMessages = async (destinationEndpoint, actions, timeoutFn) => {
     notSended.forEach((element) =>
       notSendedResult.batchItemFailures.push({ itemIdentifier: element.seqNo })
     );
-    console.error("[ACTION_ENQUEUER]", "NOT SENDED", notSendedResult);
+    console.warn("[ACTION_ENQUEUER]", "NOT SENDED", notSendedResult);
   }
   return notSendedResult;
 };
+
+
+  // ////////
+  // x ogni record kinesis
+  //   - verifico se è un record da inviare
+  //   caso "non è un record da inviare" -> continua (prossimo record)
+  //   caso "è un record da inviare"
+  //     - verifico  la destinazione
+  //     caso "la destinazione è cambiata o è il primo record"
+  //       faccio invio dei record in lista usando la destinazione precedente
+  //       caso "invio non ha avuto successo": restituisco la lista dei record non inviati ed esco
+  //       caso "invio  ha avuto successo": azzero la lista
+  //     in ogni caso:
+  //       metto in lista di invio l'item attuale
+  // caso "la lista dei record da inviare è vuota": esco
+  // caso "la lista dei record da inviare non è vuota":
+  //   - faccio invio dei record in lista usando l'ultima destinazione trovata
+  //   - caso "invio non ha avuto successo": restituisco la lista dei record non inviati ed esco
 
 async function handleEvent(event, context) {
   const emptyResult = {
@@ -147,23 +164,6 @@ async function handleEvent(event, context) {
 
   console.log("[ACTION_ENQUEUER]", "No more Records to process");
   return emptyResult;
-
-  // ////////
-  // x ogni record kinesis
-  //   - verifico se è un record da inviare
-  //   caso "non è un record da inviare" -> continua (prossimo record)
-  //   caso "è un record da inviare"
-  //     - verifico  la destinazione
-  //     caso "la destinazione è cambiata o è il primo record"
-  //       faccio invio dei record in lista usando la destinazione precedente
-  //       caso "invio non ha avuto successo": restituisco la lista dei record non inviati ed esco
-  //       caso "invio  ha avuto successo": azzero la lista
-  //     in ogni caso:
-  //       metto in lista di invio l'item attuale
-  // caso "la lista dei record da inviare è vuota": esco
-  // caso "la lista dei record da inviare non è vuota":
-  //   - faccio invio dei record in lista usando l'ultima destinazione trovata
-  //   - caso "invio non ha avuto successo": restituisco la lista dei record non inviati ed esco
 }
 
 module.exports = { handleEvent };
