936293c9798deccb75ab3c8d00f9bd91dcfc9b41
Andrea Bertucci <andrea.bertucci@pagopa.it>
2024-07-09 17:13:35 +0200

    test: enabled disabled tests

--- functions/future-action-remover/src/test/eventHandler.test.js
+++ functions/future-action-remover/src/test/eventHandler.test.js
@@ -168,7 +168,7 @@ describe("eventHandler tests", () => {
     expect(deletion[SLOT_2]).to.be.equal(1);
   });
 
-  it.only("two slot with some elements to discard", async () => {
+  it("two slot with some elements to discard", async () => {
     let lastWorkedTimeslot = "2024-05-22T11:57";
     let currSlot;
     let deletion = {};
@@ -183,7 +183,6 @@ describe("eventHandler tests", () => {
       currSlot
     );
     opsOverride["./dynamoFunctions.js"].batchDelete = async (_unused1, items, _unused3) => {
-      console.log(items, "ITEMS");
       deletion[items[0].timeslot] != undefined
         ? deletion[items[0].timeslot]++
         : (deletion[items[0].timeslot] = 1);
@@ -201,7 +200,6 @@ describe("eventHandler tests", () => {
       getRemainingTimeInMillis: () => 10000000000,
     });
     expect(result).to.not.be.null;
-    console.log("RESULT", result)
     expect(result.statusCode).to.be.eq(500);
     expect(result).to.have.property("body");
     let errorResult = JSON.parse(result.body);
@@ -212,7 +210,6 @@ describe("eventHandler tests", () => {
     expect(errorResult.error.name).to.be.eq(bOEx.name);
     expect(read[SLOT_2]).to.be.undefined;
 
-    console.log(deletion, "DELETION");
     expect(read[SLOT_1]).to.be.equal(5);
     expect(deletion[SLOT_1]).to.be.equal(5);
     expect(deletion[SLOT_2]).to.be.undefined;
@@ -234,7 +231,6 @@ describe("eventHandler tests", () => {
       currSlot
     );
     opsOverride["./dynamoFunctions.js"].batchDelete = async (_unused1, items, _unused3) => {
-      console.log(items, "ITEMS");
       deletion[items[0].timeslot] != undefined
         ? deletion[items[0].timeslot]++
         : (deletion[items[0].timeslot] = 1);
@@ -252,7 +248,6 @@ describe("eventHandler tests", () => {
       getRemainingTimeInMillis: () => 10000000000,
     });
     expect(result).to.not.be.null;
-    console.log("RESULT", result)
     expect(result.statusCode).to.be.eq(500);
     expect(result).to.have.property("body");
     let errorResult = JSON.parse(result.body);
@@ -263,7 +258,6 @@ describe("eventHandler tests", () => {
     expect(errorResult.error.name).to.be.eq(bOEx.name);
     expect(read[SLOT_2]).to.be.undefined;
 
-    console.log(deletion, "DELETION");
     expect(read[SLOT_1]).to.be.equal(5);
     expect(deletion[SLOT_1]).to.be.equal(5);
     expect(deletion[SLOT_2]).to.be.undefined;
