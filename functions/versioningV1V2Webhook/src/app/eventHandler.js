08282784d7812fa04cb93bc0d386182648f507c5
Simone Rondinella <simone.rondinella@pagopa.it>
2024-09-26 17:19:26 +0200

    PN-12679 Aggiunta gestione lambda disabilitazione stream in lambda versionig v23

--- functions/versioningV1V2Webhook/src/app/eventHandler.js
+++ functions/versioningV1V2Webhook/src/app/eventHandler.js
@@ -5,6 +5,7 @@ const InformOnExternalEventHandler = require("./handlers/informOnExternalEventHa
 const ListEventStreamsHandler = require("./handlers/listEventStreamsHandler.js");
 const DeleteEventStreamHandler = require("./handlers/deleteEventStreamHandler.js");
 const ConsumeEventStreamHandler  = require("./handlers/consumeEventStreamHandler.js");
+const DisableEventStreamHandler = require("./handlers/disableEventStreamHandler.js");
 
 const AWSXRay = require("aws-xray-sdk-core");
 
@@ -25,7 +26,9 @@ exports.eventHandler = async (event, context) => {
         handlers.push(new GetEventStreamHandler());
         handlers.push(new ListEventStreamsHandler());
         handlers.push(new DeleteEventStreamHandler());
+        handlers.push(new DisableEventStreamHandler());
         handlers.push(new InformOnExternalEventHandler());
+        
         for( let i = 0; i<handlers.length; i++){
             if (handlers[i].checkOwnership(event, context)) {
                     let result = await handlers[i].handlerEvent(event, context);
---
+++ functions/versioningV1V2Webhook/src/app/handlers/disableEventStreamHandler.js
@@ -0,0 +1,71 @@
+const axios = require("axios");
+const axiosRetry = require("axios-retry").default;
+const EventHandler  = require('./baseHandler.js');
+const { createStreamMetadataResponseV10 } = require("./mapper/transformStreamMetadataResponseFromV23ToV10.js");
+const { createStreamRequestV22 } = require("./mapper/transformStreamRequestFromV10ToV23")
+class DisableEventStreamHandler extends EventHandler {
+    constructor() {
+        super();
+    }
+
+    checkOwnership(event, context){
+        const {path, httpMethod} = event;
+        return path.includes('/streams') && path.includes('/action/disable') && event["pathParameters"] !== undefined && event["pathParameters"] != null && httpMethod.toUpperCase() === 'POST';
+    }
+
+    async handlerEvent(event, context) {
+        console.log("Versioning_V1-V2.x_DisableEventStream_Lambda function started");
+
+        // HEADERS
+        let version = this.getVersion(event);
+        
+        if(version == 10){
+            console.log("Disable operation not implemented for version ", version);
+            throw new Error("NOT IMPLEMENTED")
+        }
+
+        const headers = this.prepareHeaders(event, version);
+
+        // REQUEST BODY
+        const requestBodyV24 = {};
+
+        const streamId = event["pathParameters"]["streamId"];
+        const url = `${this.baseUrl}/streams/${streamId}/action/disable`;
+        axiosRetry(axios, {
+            retries: this.numRetry,
+            shouldResetTimeout: true ,
+            retryCondition: (error) => {
+              return axiosRetry.isNetworkOrIdempotentRequestError(error) || error.code === 'ECONNABORTED';
+            },
+            onRetry: (retryCount, error, requestConfig) => {
+                console.warn(`Retry num ${retryCount} - error:${error.message}`);
+            },
+            onMaxRetryTimesExceeded: (error, retryCount) => {
+                console.warn(`Retries exceeded: ${retryCount} - error:${error.message}`);
+            }
+        });
+
+        console.log('calling ', url);
+        let response = await axios.post(url, requestBodyV24, {headers: headers, timeout: this.attemptTimeout});
+
+        // RESPONSE BODY
+        let transformedObject;
+        
+        switch(version) {
+            case 23:
+                transformedObject = response.data;
+            break;
+            default:
+                console.error('Invalid version ', version)
+            break;
+        }
+
+        const ret = {
+            statusCode: response.status,
+            body: JSON.stringify(transformedObject),
+        }
+        return ret;
+    }
+}
+
+module.exports = DisableEventStreamHandler;
