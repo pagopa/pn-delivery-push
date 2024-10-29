96376077938dfb008abb042a24583be9441eda62
Sarah Donvito <sarah.donvito@pagopa.it>
2024-10-29 16:20:23 +0100

    feat(PN-12551): remove useless properties

--- src/main/java/it/pagopa/pn/deliverypush/legalfacts/AarTemplateType.java
+++ src/main/java/it/pagopa/pn/deliverypush/legalfacts/AarTemplateType.java
@@ -8,7 +8,7 @@ import lombok.ToString;
 public enum AarTemplateType {
     AAR_NOTIFICATION(false, DocumentComposition.TemplateType.AAR_NOTIFICATION),
     AAR_NOTIFICATION_RADD(true, DocumentComposition.TemplateType.AAR_NOTIFICATION_RADD),
-    AAR_NOTIFICATION_RADD_ALT(true, DocumentComposition.TemplateType.AAR_NOTIFICATION_RADD_ALT),
+    AAR_NOTIFICATION_RADD_ALT(true, DocumentComposition.TemplateType.AAR_NOTIFICATION_RADD_ALT);
 
     private final boolean aarRADD;
     private final DocumentComposition.TemplateType templateType;
--- src/main/java/it/pagopa/pn/deliverypush/middleware/dao/timelinedao/dynamo/entity/AarTemplateTypeEntity.java
+++ src/main/java/it/pagopa/pn/deliverypush/middleware/dao/timelinedao/dynamo/entity/AarTemplateTypeEntity.java
@@ -3,7 +3,7 @@ package it.pagopa.pn.deliverypush.middleware.dao.timelinedao.dynamo.entity;
 public enum AarTemplateTypeEntity {
     AAR_NOTIFICATION("AAR_NOTIFICATION"),
     AAR_NOTIFICATION_RADD("AAR_NOTIFICATION_RADD"),
-    AAR_NOTIFICATION_RADD_ALT("AAR_NOTIFICATION_RADD_ALT"),
+    AAR_NOTIFICATION_RADD_ALT("AAR_NOTIFICATION_RADD_ALT");
 
     private final String value;
 
