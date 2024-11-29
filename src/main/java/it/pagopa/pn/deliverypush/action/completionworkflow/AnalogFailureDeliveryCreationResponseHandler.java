0bf0082a3770c683afa90351dace46c2b1b8d5a4
Eugenio Giuliana <eugenio.giuliana@nttdata.com>
2024-11-29 12:53:06 +0100

    [PN-13366] - Changed schedule refinement date and added if condition on it

--- src/main/java/it/pagopa/pn/deliverypush/action/completionworkflow/AnalogFailureDeliveryCreationResponseHandler.java
+++ src/main/java/it/pagopa/pn/deliverypush/action/completionworkflow/AnalogFailureDeliveryCreationResponseHandler.java
@@ -5,6 +5,7 @@ import it.pagopa.pn.commons.log.PnAuditLogBuilder;
 import it.pagopa.pn.commons.log.PnAuditLogEvent;
 import it.pagopa.pn.commons.log.PnAuditLogEventType;
 import it.pagopa.pn.deliverypush.action.details.DocumentCreationResponseActionDetails;
+import it.pagopa.pn.deliverypush.config.PnDeliveryPushConfigs;
 import it.pagopa.pn.deliverypush.dto.ext.delivery.notification.NotificationInt;
 import it.pagopa.pn.deliverypush.dto.legalfacts.LegalFactCategoryInt;
 import it.pagopa.pn.deliverypush.dto.timeline.TimelineElementInternal;
@@ -16,6 +17,7 @@ import lombok.AllArgsConstructor;
 import lombok.CustomLog;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
 import java.util.Optional;
 
 import static it.pagopa.pn.deliverypush.exceptions.PnDeliveryPushExceptionCodes.ERROR_CODE_DELIVERYPUSH_STATUSNOTFOUND;
@@ -28,23 +30,30 @@ public class AnalogFailureDeliveryCreationResponseHandler {
     private final NotificationService notificationService;
     private final TimelineService timelineService;
     private final RefinementScheduler refinementScheduler;
+    private final PnDeliveryPushConfigs pnDeliveryPushConfigs;
 
     public void handleAnalogFailureDeliveryCreationResponse(String iun, int recIndex, DocumentCreationResponseActionDetails actionDetails) {
         log.info("Start handleAnalogFailureDeliveryCreationResponse - iun={} recIndex={} aarKey={}", iun, recIndex, actionDetails.getKey());
         NotificationInt notification = notificationService.getNotificationByIun(iun);
+        Instant featureUnreachableRefinementPostAARStartDate = pnDeliveryPushConfigs.getFeatureUnreachableRefinementPostAARStartDate();
 
         PnAuditLogEvent logEvent = createAuditLog(notification, recIndex, actionDetails.getKey());
         logEvent.log();
         
-        Optional<CompletelyUnreachableCreationRequestDetails> analogFailureWorkflowCreationRequestDetailsIntOpt = timelineService.getTimelineElementDetails(iun, actionDetails.getTimelineId(), CompletelyUnreachableCreationRequestDetails.class);
+        Optional<CompletelyUnreachableCreationRequestDetails> completelyUnreachableCreationRequestDetailsOpt = timelineService.getTimelineElementDetails(iun, actionDetails.getTimelineId(), CompletelyUnreachableCreationRequestDetails.class);
 
-        if (analogFailureWorkflowCreationRequestDetailsIntOpt.isPresent()) {
-            CompletelyUnreachableCreationRequestDetails timelineDetails = analogFailureWorkflowCreationRequestDetailsIntOpt.get();
+        if (completelyUnreachableCreationRequestDetailsOpt.isPresent()) {
+            CompletelyUnreachableCreationRequestDetails completelyUnreachableCreationRequestDetails = completelyUnreachableCreationRequestDetailsOpt.get();
             // recupero la data di generazione del DEPOSITO AAR, per poterla inserire nell'atto opponibile
             TimelineElementInternal analogFailureWorkflowTimelineElement = retrieveAnalogFailureWorkflowTimelineElement(notification.getIun(), recIndex);
 
-            completelyUnreachableUtils.handleCompletelyUnreachable(notification, recIndex, timelineDetails.getLegalFactId(), analogFailureWorkflowTimelineElement.getTimestamp());
-            refinementScheduler.scheduleAnalogRefinement(notification, recIndex, timelineDetails.getCompletionWorkflowDate(), timelineDetails.getEndWorkflowStatus());
+            Instant scheduleRefinementDate = completelyUnreachableCreationRequestDetails.getCompletionWorkflowDate(); //Data business evento di feedback irreperibile
+            if(notification.getSentAt() != null && notification.getSentAt().isAfter(featureUnreachableRefinementPostAARStartDate)) {
+                scheduleRefinementDate = analogFailureWorkflowTimelineElement.getTimestamp();
+            }
+
+            completelyUnreachableUtils.handleCompletelyUnreachable(notification, recIndex, completelyUnreachableCreationRequestDetails.getLegalFactId(), analogFailureWorkflowTimelineElement.getTimestamp());
+            refinementScheduler.scheduleAnalogRefinement(notification, recIndex, scheduleRefinementDate, completelyUnreachableCreationRequestDetails.getEndWorkflowStatus());
 
         } else {
             logEvent.generateFailure("Error in handleAnalogFailureDeliveryCreationResponse for timelineId={} and key={} - iun={} recIndex={}", actionDetails.getTimelineId(), actionDetails.getKey(), notification.getIun(), recIndex).log();
