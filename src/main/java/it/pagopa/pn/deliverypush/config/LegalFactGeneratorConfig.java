d23fb29707bf37126edf8439bb6a18e0aa47daaa
adrian.borta <adrian.borta@accenture.com>
2024-12-11 14:12:22 +0100

    fix(PN-13127): comments LegalFactGeneratorConfig

--- src/main/java/it/pagopa/pn/deliverypush/config/LegalFactGeneratorConfig.java
+++ src/main/java/it/pagopa/pn/deliverypush/config/LegalFactGeneratorConfig.java
@@ -9,6 +9,27 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Classe di configurazione per impostare i bean di Template Generator in base alle proprietà dell'applicazione.
+ * <p>
+ * Questa configurazione determina quale implementazione dell'interfaccia {@link LegalFactGenerator}
+ * deve essere istanziata a seconda del valore della proprietà
+ * {@code pn.user-attributes.enableTemplatesEngine}.
+ * </p>
+ *
+ * <p>
+ * Quando la proprietà è impostata su {@code true} (o non è definita), viene utilizzata
+ * l'implementazione {@link LegalFactGeneratorTemplates}
+ * Quando la proprietà è impostata su {@code false}, viene utilizzata l'implementazione
+ * {@link LegalFactGeneratorDocComposition}.
+ * </p>
+ *
+ * <p>
+ * **Nota:** Quando l'implementazione {@link LegalFactGeneratorDocComposition} non sarà più necessaria,
+ * si dovrà eliminare la classe presente  {@code LegalFactGeneratorConfig} e qualsiasi riferimento,
+ * la properties pn.delivery-push.enableTemplatesEngine.
+ * </p>
+ */
 @Configuration
 @AllArgsConstructor
 public class LegalFactGeneratorConfig {
