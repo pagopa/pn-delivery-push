3ce5936417f90dbbc20d195dc36ef481439a9038
empassaro <emanuele.passaro@emeal.nttdata.com>
2025-04-17 13:50:54 +0200

    [PN-14258] fix: junit

--- src/test/java/it/pagopa/pn/deliverypush/dto/timeline/details/RequestRefusedDetailsIntTest.java
+++ src/test/java/it/pagopa/pn/deliverypush/dto/timeline/details/RequestRefusedDetailsIntTest.java
@@ -32,7 +32,7 @@ class RequestRefusedDetailsIntTest {
     @Test
     void toLog() {
         String log = request.toLog();
-        Assertions.assertEquals("errors=[NotificationRefusedErrorInt(errorCode=FILE_NOTFOUND, detail=details)]", log);
+        Assertions.assertEquals("errors=[NotificationRefusedErrorInt(errorCode=FILE_NOTFOUND, detail=details, recIndex=null)]", log);
     }
 
     @Test
