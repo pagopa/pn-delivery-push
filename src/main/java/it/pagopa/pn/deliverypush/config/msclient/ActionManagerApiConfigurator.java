693403f34c883281154bc339616abffafc0d15b0
empassaro <emanuele.passaro@emeal.nttdata.com>
2025-06-06 11:14:53 +0200

    [PN-14960] fix: generated bean config

--- src/main/java/it/pagopa/pn/deliverypush/config/msclient/ActionManagerApiConfigurator.java
+++ src/main/java/it/pagopa/pn/deliverypush/config/msclient/ActionManagerApiConfigurator.java
@@ -8,12 +8,14 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ActionManagerApiConfigurator extends CommonBaseClient {
 
     @Bean
+    @Primary
     public ActionApi actionApi(@Qualifier("withTracing") RestTemplate restTemplate, PnDeliveryPushConfigs cfg) {
         return new ActionApi(getNewApiClient(restTemplate, cfg));
     }
