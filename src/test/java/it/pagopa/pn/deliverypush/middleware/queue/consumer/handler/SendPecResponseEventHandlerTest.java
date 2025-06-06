7234a8ae22d3e43c72c2a62fde2009ba335157ee
empassaro <emanuele.passaro@emeal.nttdata.com>
2025-06-06 09:21:50 +0200

    [PN-15218] feat: introduced handlers for external channels events

---
+++ src/test/java/it/pagopa/pn/deliverypush/middleware/queue/consumer/handler/SendPecResponseEventHandlerTest.java
@@ -0,0 +1,56 @@
+package it.pagopa.pn.deliverypush.middleware.queue.consumer.handler;
+
+import it.pagopa.pn.deliverypush.generated.openapi.msclient.externalchannel.model.SingleStatusUpdate;
+import it.pagopa.pn.deliverypush.middleware.queue.consumer.router.SupportedEventType;
+import it.pagopa.pn.deliverypush.middleware.responsehandler.ExternalChannelResponseHandler;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.messaging.MessageHeaders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+class SendPecResponseEventHandlerTest {
+    @Mock
+    private ExternalChannelResponseHandler externalChannelResponseHandler;
+
+    @Mock
+    private MessageHeaders headers;
+
+    @InjectMocks
+    private SendPecResponseEventHandler handler;
+
+    private static final SingleStatusUpdate PAYLOAD = new SingleStatusUpdate();
+
+    @Test
+    void getSupportedEventTypeReturnsCorrectType() {
+        assertEquals(SupportedEventType.SEND_PEC_RESPONSE, handler.getSupportedEventType());
+    }
+
+    @Test
+    void getPayloadTypeReturnsCorrectType() {
+        assertEquals(SingleStatusUpdate.class, handler.getPayloadType());
+    }
+
+    @Test
+    void handleExecutes() {
+        handler.handle(PAYLOAD, headers);
+
+        Mockito.verify(externalChannelResponseHandler).extChannelResponseReceiver(PAYLOAD);
+    }
+
+    @Test
+    void handleLogsAndThrowsExceptionOnError() {
+        Mockito.doThrow(new RuntimeException("Validation error")).when(externalChannelResponseHandler).extChannelResponseReceiver(PAYLOAD);
+
+        assertThrows(RuntimeException.class, () -> handler.handle(PAYLOAD, headers));
+
+        Mockito.verify(externalChannelResponseHandler).extChannelResponseReceiver(PAYLOAD);
+    }
+
+}
