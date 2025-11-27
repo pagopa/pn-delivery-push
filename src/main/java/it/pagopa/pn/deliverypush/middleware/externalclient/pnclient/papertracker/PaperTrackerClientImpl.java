52371e1b0a7fc94b46b50387f43c94932fd7f01a
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2025-11-27 08:58:12 +0100

    add logInvokingAsyncExternalService in PaperTrackerClientImpl

--- src/main/java/it/pagopa/pn/deliverypush/middleware/externalclient/pnclient/papertracker/PaperTrackerClientImpl.java
+++ src/main/java/it/pagopa/pn/deliverypush/middleware/externalclient/pnclient/papertracker/PaperTrackerClientImpl.java
@@ -3,27 +3,32 @@ package it.pagopa.pn.deliverypush.middleware.externalclient.pnclient.papertracke
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.pagopa.pn.commons.exceptions.PnInternalException;
+import it.pagopa.pn.commons.log.PnLogger;
 import it.pagopa.pn.deliverypush.generated.openapi.msclient.papertracker.api.NotificationReworkApi;
 import it.pagopa.pn.deliverypush.generated.openapi.msclient.papertracker.model.SequenceResponse;
 import it.pagopa.pn.deliverypush.generated.openapi.server.v1.dto.Problem;
+import lombok.CustomLog;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpStatus;
-import org.apache.http.client.HttpResponseException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
 
+
 @Component
 @RequiredArgsConstructor
-@Slf4j
+@CustomLog
 public class PaperTrackerClientImpl implements PaperTrackerClient{
 
     private final NotificationReworkApi notificationReworkApi;
     private final ObjectMapper objectMapper;
 
+    private static final String CLIENT_NAME = PnLogger.EXTERNAL_SERVICES.PN_PAPER_TRACKER;
+    private static final String OPERATION_RETRIEVE_SEQUENCE_AND_FINAL_STATUS = "RETRIEVE_SEQUENCE_AND_FINAL_STATUS";
+
     @Override
     public Mono<SequenceResponse> retrieveSequenceAndFinalStatus(String statusCode, String deliveryFailureCause, String productType) {
+        log.logInvokingAsyncExternalService(CLIENT_NAME, OPERATION_RETRIEVE_SEQUENCE_AND_FINAL_STATUS, String.format("%s - %s - %s", statusCode, deliveryFailureCause, productType));
         return notificationReworkApi.retrieveSequenceAndFinalStatus(statusCode, productType, deliveryFailureCause)
                 .onErrorResume(WebClientResponseException.class, ex -> {
                     try {
