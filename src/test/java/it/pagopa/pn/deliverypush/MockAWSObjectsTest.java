4bbb9023a532a6b50080ce5bf8d8b87bd3ee4535
empassaro <emanuele.passaro@emeal.nttdata.com>
2025-12-01 18:59:54 +0100

    [PN-16319] feat: removed unused dependencies from pom.xml

--- src/test/java/it/pagopa/pn/deliverypush/MockAWSObjectsTest.java
+++ src/test/java/it/pagopa/pn/deliverypush/MockAWSObjectsTest.java
@@ -4,10 +4,9 @@ import com.amazonaws.services.sqs.AmazonSQSAsync;
 import io.awspring.cloud.autoconfigure.messaging.SqsAutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.cloud.function.context.config.ContextFunctionCatalogAutoConfiguration;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
-@EnableAutoConfiguration(exclude= {SqsAutoConfiguration.class, ContextFunctionCatalogAutoConfiguration.class})
+@EnableAutoConfiguration(exclude= {SqsAutoConfiguration.class})
 public abstract class MockAWSObjectsTest {
 
     @MockBean
