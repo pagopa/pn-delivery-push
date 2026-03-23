e083db86c283c04b00295eef7b2deeb533a7010a
Stefano Rughetti <stefano.rughetti@emeal.nttdata.com>
2026-03-23 10:36:36 +0100

    [PN-19093] Feat: add request type for rework creation

--- src/main/java/it/pagopa/pn/deliverypush/service/impl/NotificationReworkServiceImpl.java
+++ src/main/java/it/pagopa/pn/deliverypush/service/impl/NotificationReworkServiceImpl.java
@@ -17,6 +17,7 @@ import it.pagopa.pn.deliverypush.generated.openapi.server.v1.dto.ReworkItemsResp
 import it.pagopa.pn.deliverypush.generated.openapi.server.v1.dto.ReworkResponse;
 import it.pagopa.pn.deliverypush.middleware.dao.notificationreworkdao.NotificationReworkDao;
 import it.pagopa.pn.deliverypush.middleware.dao.notificationreworkdao.dynamo.entity.NotificationReworksEntity;
+import it.pagopa.pn.deliverypush.middleware.dao.notificationreworkdao.dynamo.entity.RequestTypeEnum;
 import it.pagopa.pn.deliverypush.middleware.dao.notificationreworkdao.dynamo.entity.ReworkRequestStatus;
 import it.pagopa.pn.deliverypush.middleware.dao.notificationreworkdao.dynamo.entity.StatusCodeEntity;
 import it.pagopa.pn.deliverypush.middleware.externalclient.pnclient.actionmanager.ActionManagerClient;
@@ -129,6 +130,7 @@ public class NotificationReworkServiceImpl implements NotificationReworkService 
         entity.setPcRetry(notificationReworkRequestDto.getPcRetry());
         entity.setRecIndex(notificationReworkRequestDto.getRecIndex());
         entity.setStatus(ReworkRequestStatus.CREATED);
+        entity.setRequestType(RequestTypeEnum.REWORK);
         return entity;
     }
 
